96b3df3615e535a090a2b90a2749e0317242b968
Davy Hellemans <davy@spoon-library.com>
2009-09-30 11:33:22 +0000

    * removed spoon
    git-svn-id: svn://gallium.openminds.be/opt/svn/fork2/trunk@63 869247f0-a1f3-4d5c-9449-4b73fc252fa6

--- library/globals.php
+++ library/globals.php
@@ -31,11 +31,11 @@ define('DB_TYPE', 'mysqli');
 // database name
 define('DB_DATABASE', 'forkng');
 // database host
-define('DB_HOSTNAME', '127.0.0.1');
+define('DB_HOSTNAME', 'mysql.local');
 // database username
-define('DB_USERNAME', 'root');
+define('DB_USERNAME', 'forkng');
 // datebase password
-define('DB_PASSWORD', 'zero#123');
+define('DB_PASSWORD', 'forkng');
 
 
 /**
@@ -57,7 +57,7 @@ define('SITE_MULTILANGUAGE', true);
  * Depends on the serverlayout. Openminds ftw!
  */
 // path to the website itself
-define('PATH_WWW', '/Users/tijs/Sites/forkng.local/default_www');
+define('PATH_WWW', '/Users/bauffman/Sites/forkng.local/default_www');
 // path to the Spoon library
 define('PATH_LIBRARY', dirname(__FILE__));
 
