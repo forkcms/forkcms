0d0fb794f2951bc9e7f125dda716d26bef87825b
Davy Hellemans <davy@spoon-library.com>
2010-01-06 12:42:08 +0000

    * updated defaults
    git-svn-id: svn://gallium.openminds.be/opt/svn/fork2/trunk@112 869247f0-a1f3-4d5c-9449-4b73fc252fa6

--- library/globals_backend.example.php
+++ library/globals_backend.example.php
@@ -1,14 +1,12 @@
 <?php
+
 /**
  * Global configuration options and constants specific for the backend
  *
  * @package	Backend
  *
- * @author	Bert Pattyn <bert@netlash.com>
  * @author	Davy Hellemans <davy@netlash.com>
  * @author	Tijs Verkoyen <tijs@netlash.com>
- * @author	Annelies Van Extergem <annelies@netlash.com>
- * @author	Matthias Mullie <matthias@netlash.com
  */
 
 ?>
