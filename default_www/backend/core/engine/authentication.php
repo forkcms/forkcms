0d749a7185e896ff92afad1b0ebe3e336d6b3969
Davy Hellemans <davy@spoon-library.com>
2010-01-12 07:45:20 +0000

    * schoonheidsfoutje
    git-svn-id: svn://gallium.openminds.be/opt/svn/fork2/trunk@129 869247f0-a1f3-4d5c-9449-4b73fc252fa6

--- default_www/backend/core/engine/authentication.php
+++ default_www/backend/core/engine/authentication.php
@@ -169,8 +169,8 @@ class BackendAuthentication
 			foreach($allowedModules as $row) self::$allowedModules[$row] = true;
 		}
 
-		// if the module isn't in our list, we return false, otherwise the cached value
-		return (bool) (!isset(self::$allowedModules[$module])) ? false : self::$allowedModules[$module];
+		// return result
+		return (!isset(self::$allowedModules[$module])) ? false : self::$allowedModules[$module]; // @todo tijs - wat doet die code?
 	}
 
 
