f3818f62f14f59eaa891678f262970ae91dcf47f
Davy Hellemans <davy@spoon-library.com>
2010-01-18 15:33:41 +0000

    * spaties weggehaald
    git-svn-id: svn://gallium.openminds.be/opt/svn/fork2/trunk@155 869247f0-a1f3-4d5c-9449-4b73fc252fa6

--- library/spoon/filter/filter.php
+++ library/spoon/filter/filter.php
@@ -814,7 +814,7 @@ class SpoonFilter
 	{
 		// define charset
 		$charset = ($charset !== null) ? self::getValue($charset, Spoon::getCharsets(), SPOON_CHARSET) : SPOON_CHARSET;
-
+		
 		// allowed characters
 		$aCharacters = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_', ' ');
 
