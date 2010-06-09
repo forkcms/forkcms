6608a9afab08b50870836398309e9fa0052ba1a0
Tijs Verkoyen <tijs@verkoyen.eu>
2010-06-09 15:48:44 +0000

    Fancy toolbars, only visible when the editor has focus
    git-svn-id: svn://gallium.openminds.be/opt/svn/fork2/trunk@945 869247f0-a1f3-4d5c-9449-4b73fc252fa6

--- default_www/backend/core/js/tiny_mce/config.js
+++ default_www/backend/core/js/tiny_mce/config.js
@@ -5,6 +5,7 @@ tinyMCE.init({
 	theme: 'advanced',	// @todo	should be based on user-setting
 	relative_urls: false,
 	entity_encoding: 'raw',
+	button_tile_map: true,
 	plugins: 'inlinepopups,paste,contextmenu,media,fullscreen,table,filemanager,imagemanager',
 
 	// layout options
@@ -17,7 +18,7 @@ tinyMCE.init({
 	language: '{$INTERFACE_LANGUAGE}',
 	
 	// theme options
-	theme_advanced_buttons1: 'bold,italic,strikethrough,|,undo,redo,|,bullist,numlist,blockquote,|,link,unlink,anchor,|,charmap,code',
+	theme_advanced_buttons1: 'bold,italic,strikethrough,|,undo,redo,|,bullist,numlist,blockquote,|,link,unlink,anchor,|,charmap,code,|,fullscreen',
 	theme_advanced_buttons2: 'table,|,image,|,formatselect',
 	theme_advanced_buttons3: '',
 	theme_advanced_resizing: true,
@@ -37,5 +38,18 @@ tinyMCE.init({
 	paste_auto_cleanup_on_paste : true,
 	paste_strip_class_attributes: 'mso',
 	paste_remove_spans: true,
-	paste_remove_styles: true
+	paste_remove_styles: true,
+	init_instance_callback: function(ed) {
+		// get element to hook the event on (ripped from AC)
+		var hookOn = ed.settings.content_editable ? ed.getBody() : (tinymce.isGecko ? ed.getDoc() : ed.getWin());
+	
+		// hook focus
+		tinyMCE.dom.Event.add(hookOn, 'focus', function(e) {
+			if(!$(ed.contentAreaContainer).parents('span.mceEditor').is('expanded')) $(ed.contentAreaContainer).parents('span.mceEditor').addClass('expanded'); 
+		});
+		// hook blur
+		tinyMCE.dom.Event.add(hookOn, 'blur', function(e) {
+			$(ed.contentAreaContainer).parents('span.mceEditor').removeClass('expanded');
+		});
+	}
 });
