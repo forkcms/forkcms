ea1656f7c1361e44e61b3fa9707128e00877cc7d
Davy Hellemans <davy@spoon-library.com>
2010-06-15 13:17:24 +0000

    * taal wordt nu correct meegegeven bij 'timeago' modifier
    git-svn-id: svn://gallium.openminds.be/opt/svn/fork2/trunk@962 869247f0-a1f3-4d5c-9449-4b73fc252fa6

--- default_www/frontend/core/engine/template.php
+++ default_www/frontend/core/engine/template.php
@@ -429,7 +429,7 @@ class FrontendTemplateModifiers
 		if($var == 0) return '';
 
 		// return
-		return '<abbr title="'. SpoonDate::getDate('d/m/Y H:i:s', $var, FRONTEND_LANGUAGE) .'">'. SpoonDate::getTimeAgo($var) .'</abbr>';
+		return '<abbr title="'. SpoonDate::getDate('d/m/Y H:i:s', $var, FRONTEND_LANGUAGE) .'">'. SpoonDate::getTimeAgo($var, FRONTEND_LANGUAGE) .'</abbr>';
 	}
 
 
