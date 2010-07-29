77b058ecc7ce32be62ee3b593bba50b262a42a15
Tijs Verkoyen <tijs@verkoyen.eu>
2010-07-29 09:47:30 +0000

    Removed obsolute code.
    git-svn-id: svn://gallium.openminds.be/opt/svn/fork2/trunk@1422 869247f0-a1f3-4d5c-9449-4b73fc252fa6

--- default_www/backend/modules/blog/js/blog.js
+++ default_www/backend/modules/blog/js/blog.js
@@ -14,18 +14,6 @@ jsBackend.blog = {
 
 jsBackend.blog.category = {
 	init: function() {
-		if($('#newCategory').length > 0) {
-			// set disabled state
-			$('#newCategoryButton').addClass('disabledButton');
-			
-			// hide errors
-			$('#newCategoryError').hide();
-			
-			// bind events
-			$('#newCategoryValue').bind('keyup', jsBackend.blog.category.keyPress);
-			$('#newCategoryButton').bind('click', jsBackend.blog.category.add);
-		}
-		
 		if($('.datagrid td.name').length > 0) {
 			// buil ajax-url
 			var url = '/backend/ajax.php?module=' + jsBackend.current.module + '&action=edit_category&language=' + jsBackend.current.language;
@@ -34,71 +22,6 @@ jsBackend.blog.category = {
 			$('.datagrid td.name').inlineTextEdit({ saveUrl: url, tooltip: '{$msgClickToEdit}' });
 		}
 		
-	},
-	// add new category
-	add: function(evt) {
-		// prevent default
-		evt.preventDefault();
-		
-		// validate
-		if($('#newCategoryValue').val().length == 0) return false;
-		
-		// buil ajax-url
-		var url = '/backend/ajax.php?module=' + jsBackend.current.module + '&action=add_category&language=' + jsBackend.current.language;
-
-		// init var
-		var name = $('#newCategoryValue').val();
-		
-		// make the call
-		$.ajax({ url: url,
-			data: 'category_name=' + name,
-			success: function(data, textStatus) {
-				if(data.code == 200) {
-					// existing categoyr
-					if(typeof data.data.id != 'undefined') var id = data.data.id;
-					
-					// doesn't exist
-					else
-					{
-						var id = data.data.new_id;
-						$('#categoryId').append('<option selected="selected" value="'+ id +'">'+ name +'</option>');
-					}
-
-					// unselect all
-					$('#categoryId option').attr('selected', '');
-					
-					// set selected
-					$('#categoryId').val(id);
-					
-					// clear
-					$('#newCategoryValue').val('');
-					$('#newCategoryButton').addClass('disabledButton');
-					$('#newCategory').slideUp();
-					
-					// show message
-					jsBackend.messages.add('success', "{$msgAddedCategory|addslashes}".replace('%1$s', name));
-				} else {
-					// show message
-					jsBackend.messages.add('error', textStatus);
-				}
-				
-				// alert the user
-				if(data.code != 200 && jsBackend.debug) { alert(data.message); }
-			}
-		});
-	},
-	// when a key is pressed
-	keyPress: function(evt) {
-		if(evt.which == 13) {
-			// stop the default action
-			evt.preventDefault();
-			evt.stopPropagation();
-			
-			// add the category
-			jsBackend.blog.category.add(evt); 
-		}
-		if($(this).val().length > 0) $('#newCategoryButton').removeClass('disabledButton');
-		else $('#newCategoryButton').addClass('disabledButton');
 	},
 	
 	// end
