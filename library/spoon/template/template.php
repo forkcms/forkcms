81739b57f5bebfb35bdb764b6a5309e5612da33a
Matthias Mullie <matthias@netlash.com>
2010-08-16 09:52:51 +0000

    git-svn-id: svn://gallium.openminds.be/opt/svn/fork2/trunk@1636 869247f0-a1f3-4d5c-9449-4b73fc252fa6

--- library/spoon/template/template.php
+++ library/spoon/template/template.php
@@ -229,7 +229,7 @@ class SpoonTemplate
 	public function compile($path, $template)
 	{
 		// redefine template
-		if(realpath($path .'/'. $template) !== false) $template = $path .'/'. $template;
+		if(realpath($template) === false) $template = $path .'/'. $template;
 
 		// create object
 		$compiler = new SpoonTemplateCompiler($template, $this->variables);
@@ -364,7 +364,7 @@ class SpoonTemplate
 	private function getCompileName($template, $path = null)
 	{
 		// redefine template
-		if($path !== null && realpath($path .'/'. $template) !== false) $template = $path .'/'. $template;
+		if($path !== null && realpath($template) === false) $template = $path .'/'. $template;
 
 		// return the correct full path
 		return md5(realpath($template)) .'_'. basename($template) .'.php';
