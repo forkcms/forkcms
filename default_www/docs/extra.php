ef9f1109ac033976b3700d35e5b1aaf1f712c16b
Tijs Verkoyen <tijs@verkoyen.eu>
2010-09-02 22:42:05 +0000

    http://projects.netlash.com/public/index.php/projects/369/tickets/556
    git-svn-id: svn://gallium.openminds.be/opt/svn/fork2/trunk@1785 869247f0-a1f3-4d5c-9449-4b73fc252fa6

--- default_www/docs/extra.php
+++ default_www/docs/extra.php
@@ -10,7 +10,7 @@
 		<div class="cols id1">
 			<div class="col col-8 content">
 				<div class="col-6">
-					
+
 					<p><span class="markedTodo">@todo sort this into relevant sections</span></p>
 
 					<h3>Pushing your site live</h3>
@@ -28,25 +28,36 @@
 					<p>The getByTag($tagId)-method should return an multidimensional array with the following keys for each row: url, name, module.</p>
 
 					<h3 id="debugMode">When is my site in debug mode?</h3>
-					
+
 					<p>Your site is in debug mode when the following option:</p>
-					
+
 					<pre class="brush: php;">
 						// should the debug information be shown
 						define('SPOON_DEBUG', true);
 					</pre>
 					<p>is set to true.</p>
-					
 
+
+					<h3 id="ignore">Paths that should be ignored by your versioning system</h3>
+					<ul>
+						<li><code>/default_www/backend/cache/*/*</code>: content of each subfolder in <code>/default_www/backend/cache/</code></li>
+						<li><code>/default_www/frontend/cache/*/*</code>: content of each subfolder in <code>/default_www/frontend/cache/</code></li>
+						<li><code>/default_www/frontend/files/*/*</code>: content of each subfolder in <code>/default_www/frontend/cache/</code></li>
+						<li><code>/default_www/frontend/files/userfiles/files/*</code></li>
+						<li><code>/default_www/frontend/files/userfiles/images/*</code></li>
+						<li><code>/default_www/install/cache/*</code></li>
+						<li><code>/default_www/install/installed.txt</code></li>
+						<li><code>/library/globals.php</code></li>
+						<li><code>/library/globals.*.php</code></li>
+					</ul>
 				</div>
-
 			</div>
 		</div>
 
 		<div class="hr"><hr /></div>
 
 </div>
-	
+
 	<script type="text/javascript">
 		SyntaxHighlighter.config.clipboardSwf = 'js/syntax/scripts/clipboard.swf';
 		SyntaxHighlighter.all();
