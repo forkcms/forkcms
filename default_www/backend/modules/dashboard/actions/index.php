e4fce57b5fda867e1bb758787f050ac2cca5e9c3
Bauffman <davy@spoon-library.com>
2010-12-04 20:22:46 +0100

    Bugfix: you no longer see dashboard widgets from modules you don't even have rights for.

--- default_www/backend/modules/dashboard/actions/index.php
+++ default_www/backend/modules/dashboard/actions/index.php
@@ -54,53 +54,57 @@ class BackendDashboardIndex extends BackendBaseActionIndex
 		// loop all modules
 		foreach($modules as $module)
 		{
-			// build pathName
-			$pathName = BACKEND_MODULES_PATH .'/'. $module;
-
-			// check if the folder exists
-			if(SpoonDirectory::exists($pathName .'/widgets'))
+			// you have sufficient rights?
+			if(BackendAuthentication::isAllowedModule($module))
 			{
-				// get widgets
-				$widgets = (array) SpoonFile::getList($pathName .'/widgets', '/(.*)\.php/i');
+				// build pathName
+				$pathName = BACKEND_MODULES_PATH .'/'. $module;
 
-				// loop widgets
-				foreach($widgets as $widget)
+				// check if the folder exists
+				if(SpoonDirectory::exists($pathName .'/widgets'))
 				{
-					// require the class
-					require_once $pathName .'/widgets/'. $widget;
+					// get widgets
+					$widgets = (array) SpoonFile::getList($pathName .'/widgets', '/(.*)\.php/i');
 
-					// build classname
-					$className = 'Backend'. SpoonFilter::toCamelCase($module) .'Widget'. SpoonFilter::toCamelCase(str_replace('.php', '', $widget));
-
-					// validate if the class exists
-					if(!class_exists($className)) throw new BackendException('The widgetfile is present, but the classname should be: '. $className .'.');
-
-					// check if model file exists
-					if(SpoonFile::exists($pathName .'/engine/model.php'))
+					// loop widgets
+					foreach($widgets as $widget)
 					{
-						// require model
-						require_once $pathName .'/engine/model.php';
+						// require the class
+						require_once $pathName .'/widgets/'. $widget;
+
+						// build classname
+						$className = 'Backend'. SpoonFilter::toCamelCase($module) .'Widget'. SpoonFilter::toCamelCase(str_replace('.php', '', $widget));
+
+						// validate if the class exists
+						if(!class_exists($className)) throw new BackendException('The widgetfile is present, but the classname should be: '. $className .'.');
+
+						// check if model file exists
+						if(SpoonFile::exists($pathName .'/engine/model.php'))
+						{
+							// require model
+							require_once $pathName .'/engine/model.php';
+						}
+
+						// create instance
+						$instance = new $className();
+
+						// execute instance
+						$instance->execute();
+
+						// add to correct column and position
+						$column = $instance->getColumn();
+						$position = $instance->getPosition();
+						$templatePath = $instance->getTemplatePath();
+
+						// reset template path
+						if($templatePath == null) $templatePath = BACKEND_PATH .'/modules/'. $module .'/layout/widgets/'. str_replace('.php', '.tpl', $widget);
+
+						// add on new position
+						if($position === null) $this->widgets[$column][] = array('template' => $templatePath);
+
+						// add on requested position
+						else $this->widgets[$column][$position] = array('template' => $templatePath);
 					}
-
-					// create instance
-					$instance = new $className();
-
-					// execute instance
-					$instance->execute();
-
-					// add to correct column and position
-					$column = $instance->getColumn();
-					$position = $instance->getPosition();
-					$templatePath = $instance->getTemplatePath();
-
-					// reset template path
-					if($templatePath == null) $templatePath = BACKEND_PATH .'/modules/'. $module .'/layout/widgets/'. str_replace('.php', '.tpl', $widget);
-
-					// add on new position
-					if($position === null) $this->widgets[$column][] = array('template' => $templatePath);
-
-					// add on requested position
-					else $this->widgets[$column][$position] = array('template' => $templatePath);
 				}
 			}
 		}
