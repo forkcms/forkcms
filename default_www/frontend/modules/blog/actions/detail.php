917c8ca25ef616878b129cf8beae37ad40d8c975
Dieterve <dieter@dieterve.be>
2011-01-17 09:23:28 +0100

    Bugfix: Blog RSS was added twice in blog detail

--- default_www/frontend/modules/blog/actions/detail.php
+++ default_www/frontend/modules/blog/actions/detail.php
@@ -203,9 +203,6 @@ class FrontendBlogDetail extends FrontendBaseBlock
 			$this->header->addMetaCustom($meta);
 		}
 
-		// add RSS-feed into the metaCustom
-		$this->header->addMetaCustom('<link rel="alternate" type="application/rss+xml" title="'. FrontendModel::getModuleSetting('blog', 'rss_title_'. FRONTEND_LANGUAGE) .'" href="'. $rssLink .'" />');
-
 		// add into breadcrumb
 		$this->breadcrumb->addElement($this->record['title']);
 
