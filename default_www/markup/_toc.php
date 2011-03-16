730198fa5d403efbedb9e59ed54606201ce39fcc
Thomas Deceuninck <thomasdeceuninck@netlash.com>
2011-03-16 16:47:46 +0100

    Started building backend markup library (introduction page, content blocks module and tags module)

---
+++ default_www/markup/_toc.php
@@ -0,0 +1,10 @@
+<div id="toc" class="col-3">
+	<div class="content">
+		<h4 class="secondaryContent">Table of Contents</h4>
+		<ol>
+			<li><a href="index.php">Introduction</a></li>
+			<li><a href="contentblocks.php">Content blocks</a></li>
+			<li><a href="tags.php">Tags</a></li>
+		</ol>
+	</div>
+</div>
