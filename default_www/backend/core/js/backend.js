a532705a219f65ee3ce7b6632da2511f05375eed
Tijs Verkoyen <tijs@verkoyen.eu>
2011-03-18 21:18:29 +0100

    Massactions shuld only operate on visible items

--- default_www/backend/core/js/backend.js
+++ default_www/backend/core/js/backend.js
@@ -440,7 +440,7 @@ jsBackend.controls =
 							$(this).dialog('close');
 
 							// submit the form
-							$($('*[data-message-id='+ $(this).attr('id') +']').parents('form')[0]).submit();
+							$($('*[data-message-id='+ $(this).attr('id') +']:visible').parents('form')[0]).submit();
 						},
 						'{$lblCancel|ucfirst}': function()
 						{
