78a3f8b4e9f44132e647eda35c165ffab8860153
dieterve <dieter@dieterve.be>
2011-09-13 09:53:07 +0200

    Checkboxes need a wrapper element for CSS positioning.

--- default_www/backend/modules/groups/actions/edit.php
+++ default_www/backend/modules/groups/actions/edit.php
@@ -8,6 +8,7 @@
  * @actiongroup	management	This is the all-around management action.
  *
  * @author		Jeroen Van den Bossche <jeroenvandenbossche@netlash.com>
+ * @author		Dieter Vanden Eynde <dieter@netlash.com>
  * @since		2.0
  */
 class BackendGroupsEdit extends BackendBaseActionEdit
@@ -404,7 +405,7 @@ class BackendGroupsEdit extends BackendBaseActionEdit
 					}
 
 					// add widget checkboxes
-					$widgetBoxes[$j]['checkbox'] = $this->frm->addCheckbox('widgets_' . $widget['label'], isset($selectedWidgets[$j]) ? $selectedWidgets[$j] : null)->parse();
+					$widgetBoxes[$j]['checkbox'] = '<span>' . $this->frm->addCheckbox('widgets_' . $widget['label'], isset($selectedWidgets[$j]) ? $selectedWidgets[$j] : null)->parse() . '</span>';
 					$widgetBoxes[$j]['widget'] = $widget['label'];
 					$widgetBoxes[$j]['description'] = $widget['description'];
 				}
@@ -436,7 +437,7 @@ class BackendGroupsEdit extends BackendBaseActionEdit
 					if(!in_array($action['group'], $addedBundles))
 					{
 						// assign bundled action boxes
-						$actionBoxes[$key]['actions'][$i]['checkbox'] = $this->frm->addCheckbox('actions_' . $module['label'] . '_' . 'Group_' . ucfirst($action['group']), in_array($action['value'], $selectedActions))->parse();
+						$actionBoxes[$key]['actions'][$i]['checkbox'] = '<span>' . $this->frm->addCheckbox('actions_' . $module['label'] . '_' . 'Group_' . ucfirst($action['group']), in_array($action['value'], $selectedActions))->parse() . '</span>';
 						$actionBoxes[$key]['actions'][$i]['action'] = ucfirst($action['group']);
 						$actionBoxes[$key]['actions'][$i]['description'] = $this->actionGroups[$action['group']];
 
@@ -449,7 +450,7 @@ class BackendGroupsEdit extends BackendBaseActionEdit
 				else
 				{
 					// assign action boxes
-					$actionBoxes[$key]['actions'][$i]['checkbox'] = $this->frm->addCheckbox('actions_' . $module['label'] . '_' . $action['label'], in_array($action['value'], $selectedActions))->parse();
+					$actionBoxes[$key]['actions'][$i]['checkbox'] = '<span>' . $this->frm->addCheckbox('actions_' . $module['label'] . '_' . $action['label'], in_array($action['value'], $selectedActions))->parse() . '</span>';
 					$actionBoxes[$key]['actions'][$i]['action'] = $action['label'];
 					$actionBoxes[$key]['actions'][$i]['description'] = $action['description'];
 				}
