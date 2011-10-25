7b53cfa8febc40c48afb37b442194aeb98281b2a
Bauffman <davy@spoon-library.com>
2011-10-25 16:14:04 +0200

    Cleaned up the type casting sniff.

--- tools/codesniffer/Fork/Sniffs/Styleguide/CastingSniff.php
+++ tools/codesniffer/Fork/Sniffs/Styleguide/CastingSniff.php
@@ -1,23 +1,20 @@
 <?php
 
 /**
- * Fork_Sniffs_Styleguide_CastingSniff
  * Checks if casting is as described in the styleguide
  *
  * @author	Tijs Verkoyen <tijs@sumocoders.be>
  */
 class Fork_Sniffs_Styleguide_CastingSniff implements PHP_CodeSniffer_Sniff
 {
-	public function register()
-	{
-		// register on (array), (bool), ...
-		return array(T_ARRAY_CAST, T_BOOL_CAST, T_DOUBLE_CAST, T_INT_CAST, T_OBJECT_CAST, T_STRING_CAST, T_UNSET_CAST);
-	}
-
-
+	/**
+	 * Processes the code.
+	 *
+	 * @param PHP_CodeSniffer_File $phpcsFile
+	 * @param int $stackPtr
+	 */
 	public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
 	{
-		// get the tokens
 		$tokens = $phpcsFile->getTokens();
 		$current = $tokens[$stackPtr];
 		$next = $tokens[$stackPtr + 1];
@@ -29,26 +26,44 @@ class Fork_Sniffs_Styleguide_CastingSniff implements PHP_CodeSniffer_Sniff
 			case T_BOOL_CAST:
 				if($current['content'] != '(bool)') $phpcsFile->addError('We use (bool) instead of (boolean)', $stackPtr);
 				if($next['content'] != ' ') $phpcsFile->addError('Space excpected after cast', $stackPtr);
-			break;
+				break;
 
 			case T_DOUBLE_CAST:
-				if($current['content'] != '(float)') $phpcsFile->addError('We use (float) instead of (double)', $stackPtr);
-				if($next['content'] != ' ') $phpcsFile->addError('Space excpected after cast', $stackPtr);
-			break;
+				if($current['content'] != '(float)')
+				{
+					$phpcsFile->addError('We use (float) instead of (double)', $stackPtr);
+				}
+
+				if($next['content'] != ' ')
+				{
+					$phpcsFile->addError('Space excpected after cast', $stackPtr);
+				}
+				break;
+
 			case T_ARRAY_CAST:
 			case T_INT_CAST:
 			case T_OBJECT_CAST:
 			case T_STRING_CAST:
 			case T_UNSET_CAST:
-				if($next['content'] != ' ') $phpcsFile->addError('Space excpected after cast', $stackPtr);
-			break;
+				if($next['content'] != ' ')
+				{
+					$phpcsFile->addError('Space excpected after cast', $stackPtr);
+				}
+				break;
 		}
 
-		// cleanup
 		unset($tokens);
 		unset($current);
 		unset($next);
 	}
-}
 
-?>
+	/**
+	 * Register on typecasting
+	 *
+	 * @return array
+	 */
+	public function register()
+	{
+		return array(T_ARRAY_CAST, T_BOOL_CAST, T_DOUBLE_CAST, T_INT_CAST, T_OBJECT_CAST, T_STRING_CAST, T_UNSET_CAST);
+	}
+}
