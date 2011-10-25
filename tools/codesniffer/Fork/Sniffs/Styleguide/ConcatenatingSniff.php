ed8516e4c8187c6e4d82dcbe3e5ad90c912f1a48
Bauffman <davy@spoon-library.com>
2011-10-25 16:32:32 +0200

    Cleaned up the concatenation sniff.

--- tools/codesniffer/Fork/Sniffs/Styleguide/ConcatenatingSniff.php
+++ tools/codesniffer/Fork/Sniffs/Styleguide/ConcatenatingSniff.php
@@ -1,20 +1,18 @@
 <?php
 
 /**
- * Fork_Sniffs_Styleguide_ConcatenatingSniff
  * Checks if contactenating is used like described in the styleguide
  *
- * @author	Davy Hellemans <davy@spoon-library.com>
+ * @author Davy Hellemans <davy@spoon-library.com>
  */
 class Fork_Sniffs_Styleguide_ConcatenatingSniff implements PHP_CodeSniffer_Sniff
 {
-	public function register()
-	{
-		// register on (array), (bool), ...
-		return array(T_STRING_CONCAT);
-	}
-
-
+	/**
+	 * Processes the code.
+	 *
+	 * @param PHP_CodeSniffer_File $phpcsFile
+	 * @param unknown_type $stackPtr
+	 */
 	public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
 	{
 		// get the tokens
@@ -24,14 +22,24 @@ class Fork_Sniffs_Styleguide_ConcatenatingSniff implements PHP_CodeSniffer_Sniff
 		$next = $tokens[$stackPtr + 1];
 
 		// space before and after
-		if($previous['content'] != ' ' || $next['content'] != ' ') $phpcsFile->addError('Concat operator must be surrounded by spaces', $stackPtr);
+		if($previous['content'] != ' ' || $next['content'] != ' ')
+		{
+			$phpcsFile->addError('Concat operator must be surrounded by spaces', $stackPtr);
+		}
 
-		// cleanup
 		unset($tokens);
 		unset($current);
 		unset($next);
 		unset($previous);
 	}
-}
 
-?>
+	/**
+	 * Registers on string concatenations (with strings)
+	 *
+	 * @return array
+	 */
+	public function register()
+	{
+		return array(T_STRING_CONCAT);
+	}
+}
