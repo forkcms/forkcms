4819d279c194c0242ea9747edc5c62b9225672de
Bauffman <davy@spoon-library.com>
2011-10-26 16:23:36 +0200

    Applied coding standards to some backend php files.

--- backend/core/installer/install.php
+++ backend/core/installer/install.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of Fork CMS.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
 /**
  * The base-class for the installer
  *
