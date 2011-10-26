86d67ba2fbcfb47b9a87f2d4d2b6e6c473b39c23
Bauffman <davy@spoon-library.com>
2011-10-26 10:08:33 +0200

    The codesniffer now throws an error for php closing tags.

--- tools/codesniffer/Fork/Sniffs/Styleguide/FilesSniff.php
+++ tools/codesniffer/Fork/Sniffs/Styleguide/FilesSniff.php
@@ -1,24 +1,25 @@
 <?php
 
 /**
- * Fork_Sniffs_Styleguide_FilesSniff
- * Checks if a file is formated as described in the styleguide
+ * Checks if a file is formatted as described in the styleguide
  *
- * @author	Tijs Verkoyen <tijs@sumocoders.be>
+ * @author Tijs Verkoyen <tijs@sumocoders.be>
  */
 class Fork_Sniffs_Styleguide_FilesSniff implements PHP_CodeSniffer_Sniff
 {
-	public function register()
-	{
-		// register on open en clsong PHP-tag
-		return array(T_OPEN_TAG, T_CLOSE_TAG);
-	}
-
-
+	/**
+	 * Processes the code.
+	 *
+	 * @param PHP_CodeSniffer_File $phpcsFile
+	 * @param int $stackPtr
+	 */
 	public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
 	{
 		// check line endings
-		if($phpcsFile->eolChar !== "\n") $phpcsFile->addError('We expect lines to end with "\n".', $stackPtr);
+		if($phpcsFile->eolChar !== "\n")
+		{
+			$phpcsFile->addError('We expect lines to end with "\n".', $stackPtr);
+		}
 
 		// get the tokens
 		$tokens = $phpcsFile->getTokens();
@@ -27,23 +28,29 @@ class Fork_Sniffs_Styleguide_FilesSniff implements PHP_CodeSniffer_Sniff
 		if(T_OPEN_TAG === $tokens[$stackPtr]['code'])
 		{
 			// check if next line is empty
-			if($tokens[$stackPtr + 1]['content'] != "\n") $phpcsFile->addError('After "<?php" we expect an empty line.', $stackPtr);
+			if($tokens[$stackPtr + 1]['content'] != "\n")
+			{
+				$phpcsFile->addError('After "<?php" we expect a blank line.', $stackPtr);
+			}
 		}
 
 		// check if there is a before after the closing tag
 		if(T_CLOSE_TAG === $tokens[$stackPtr]['code'])
 		{
-			// get all lines
-			$lines = file($phpcsFile->getFilename());
-
-			// check if line before tag is empty
-			if(isset($lines[$tokens[$stackPtr]['line'] - 2]) && $lines[$tokens[$stackPtr]['line'] - 2] != "\n") $phpcsFile->addError('Before "?>" we expect an empty line.', $stackPtr);
+			$phpcsFile->addError('We don\'t use the closing "?>" tag!', $stackPtr);
 		}
 
-		// cleanup
 		unset($tokens);
 		unset($lines);
 	}
-}
 
-?>
+	/**
+	 * Registers on php opening/closing tags
+	 *
+	 * @return array
+	 */
+	public function register()
+	{
+		return array(T_OPEN_TAG, T_CLOSE_TAG);
+	}
+}
