d609367e65644184c3b7799aebba27b43f168001
Tijs Verkoyen <tijs@verkoyen.eu>
2011-11-04 15:02:25 +0100

    Simplified and improved the Javascript for the users-module

--- backend/modules/users/js/users.js
+++ backend/modules/users/js/users.js
@@ -8,45 +8,44 @@ jsBackend.users =
 	// init, something like a constructor
 	init: function()
 	{
-		jsBackend.users.controls.init();
-	}
-}
-
-jsBackend.users.controls =
-{
-	// init, something like a constructor
-	init: function()
-	{
-		jsBackend.users.controls.nick();
+		jsBackend.users.nick();
 	},
 
 	// set nickname
 	nick: function()
 	{
+		$nickname = $('#nickname');
+		$name = $('#name');
+		$surname = $('#surname');
+
 		// are all elements available
-		if($('#nickname').length > 0 && $('#name').length > 0 && $('#surname').length > 0)
+		if($nickname.length > 0 && $name.length > 0 && $surname.length > 0)
 		{
 			var change = true;
 
 			// if the current value is the same as the one that would be generated then we bind the events
-			if($('#nickname').val() != jsBackend.users.controls.calculateNick()) { change = false; }
+			if($nickname.val() != jsBackend.users.calculateNick()) { change = false; }
 
 			// bind events
-			$('#name').keyup(function() { if(change) { $('#nickname').val(jsBackend.users.controls.calculateNick()); } });
-			$('#surname').keyup(function() { if(change) { $('#nickname').val(jsBackend.users.controls.calculateNick()); } });
+			$name.keyup(function() { if(change) { $nickname.val(jsBackend.users.calculateNick()); } });
+			$surname.keyup(function() { if(change) { $nickname.val(jsBackend.users.calculateNick()); } });
 
 			// unbind events
-			$('#nickname').keyup(function() { change = false; });
+			$nickname.keyup(function() { change = false; });
 		}
 	},
 
 	// calculate the nickname
 	calculateNick: function()
 	{
-		var maxLength = parseInt($('#nickname').attr('maxlength'));
+		$nickname = $('#nickname');
+		$name = $('#name');
+		$surname = $('#surname');
+
+		var maxLength = parseInt($nickname.attr('maxlength'));
 		if(maxLength == 0) maxLength = 255;
 
-		return utils.string.trim(utils.string.trim($('#name').val()) +' '+ utils.string.trim($('#surname').val())).substring(0, maxLength);
+		return utils.string.trim(utils.string.trim($name.val()) +' '+ utils.string.trim($surname.val())).substring(0, maxLength);
 	}
 }
 
