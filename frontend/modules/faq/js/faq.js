3c880ffd3357ad7239ee4d1c0d89e52477bfcba0
Thomas Deceuninck <thomas@fronto.be>
2011-11-08 19:53:35 +0100

    improved the JS and corrected some spelling mistakes

--- frontend/modules/faq/js/faq.js
+++ frontend/modules/faq/js/faq.js
@@ -1,10 +1,9 @@
-if(!jsFrontend) { var jsFrontend = new Object(); }
-
 /**
  * Interaction for the faq module
  *
  * @author	Annelies Van Extergem <annelies@netlash.com>
  * @author	Jelmer Snoeck <jelmer.snoeck@netlash.com>
+ * @author	Thomas Deceuninck <thomasdeceuninck@netlash.com>
  */
 jsFrontend.faq =
 {
@@ -20,17 +19,17 @@ jsFrontend.faq.feedback =
 {
 	init: function()
 	{
-		// usefull status has been changed
+		// useful status has been changed
 		$('#usefullY, #usefullN').on('click', function()
 		{
-			// get usefull status
-			var usefull = ($('#usefullY').attr('checked') ? true : false);
+			// get useful status
+			var useful = ($('#usefullY').attr('checked') ? true : false);
 
 			// show or hide the form
-			if(usefull) { $('form#feedback').submit(); }
+			if(useful) { $('form#feedback').submit(); }
 			else { $('#feedbackNoInfo').show(); }
 		});
 	}
 }
 
-$(document).ready(function() { jsFrontend.faq.init(); });
+$(jsFrontend.faq.init);
