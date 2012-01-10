7e3c9f78654ef38298cc8914d63e50f2f89dda25
Jelmer Snoeck <jelmer.snoeck@siphoc.com>
2012-01-10 11:20:30 +0100

    Cleaned the frontend pages model up

--- frontend/modules/pages/engine/model.php
+++ frontend/modules/pages/engine/model.php
@@ -28,7 +28,12 @@ class FrontendPagesModel implements FrontendTagsInterface
 			'SELECT i.id, i.title
 			 FROM pages AS i
 			 INNER JOIN meta AS m ON m.id = i.meta_id
-			 WHERE i.status = ? AND i.hidden = ? AND i.language = ? AND i.publish_on <= ? AND i.id IN (' . implode(',', $ids) . ')
+			 WHERE
+			 	i.status = ? AND
+			 	i.hidden = ? AND
+			 	i.language = ? AND
+			 	i.publish_on <= ? AND
+			 	i.id IN (' . implode(',', $ids) . ')
 			 ORDER BY i.title ASC',
 			array('active', 'N', FRONTEND_LANGUAGE, FrontendModel::getUTCDate('Y-m-d H:i') . ':00')
 		);
@@ -37,7 +42,10 @@ class FrontendPagesModel implements FrontendTagsInterface
 		if(!empty($items))
 		{
 			// reset url
-			foreach($items as &$row) $row['full_url'] = FrontendNavigation::getURL($row['id'], FRONTEND_LANGUAGE);
+			foreach($items as $key => $row)
+			{
+				$items[$key]['full_url'] = FrontendNavigation::getURL($row['id'], FRONTEND_LANGUAGE);
+			}
 		}
 
 		// return
@@ -72,13 +80,20 @@ class FrontendPagesModel implements FrontendTagsInterface
 			 WHERE i.parent_id = ? AND i.status = ? AND i.hidden = ?
 			 AND i.language = ? AND i.publish_on <= ?
 			 ORDER BY i.sequence ASC',
-			array((int) $id, 'active', 'N', FRONTEND_LANGUAGE, FrontendModel::getUTCDate('Y-m-d H:i') . ':00'));
+			array(
+				(int) $id, 'active', 'N', FRONTEND_LANGUAGE,
+				FrontendModel::getUTCDate('Y-m-d H:i') . ':00'
+			)
+		);
 
 		// has items
 		if(!empty($items))
 		{
 			// reset url
-			foreach($items as &$row) $row['full_url'] = FrontendNavigation::getURL($row['id'], FRONTEND_LANGUAGE);
+			foreach($items as $key => $row)
+			{
+				$items[$key]['full_url'] = FrontendNavigation::getURL($row['id'], FRONTEND_LANGUAGE);
+			}
 		}
 
 		// return
@@ -109,21 +124,26 @@ class FrontendPagesModel implements FrontendTagsInterface
 			 FROM pages AS p
 			 INNER JOIN meta AS m ON p.meta_id = m.id
 			 INNER JOIN themes_templates AS t ON p.template_id = t.id
-			 WHERE p.id IN (' . implode(', ', $ids) . ') AND p.id NOT IN (' . implode(', ', $ignore) . ') AND p.status = ? AND p.hidden = ? AND p.language = ?',
+			 WHERE
+			 	p.id IN (' . implode(', ', $ids) . ') AND
+			 	p.id NOT IN (' . implode(', ', $ignore) . ') AND
+			 	p.status = ? AND
+			 	p.hidden = ? AND
+			 	p.language = ?',
 			array('active', 'N', FRONTEND_LANGUAGE), 'id'
 		);
 
 		// prepare items for search
-		foreach($items as &$item)
+		foreach($items as $key => $item)
 		{
-			$item['text'] = implode(' ', (array) $db->getColumn(
+			$items[$key]['text'] = implode(' ', (array) $db->getColumn(
 				'SELECT pb.html
 				 FROM pages_blocks AS pb
 				 WHERE pb.revision_id = ?',
 				array($item['text']))
 			);
 
-			$item['full_url'] = FrontendNavigation::getURL($item['id']);
+			$items[$key]['full_url'] = FrontendNavigation::getURL($item['id']);
 		}
 
 		return $items;
