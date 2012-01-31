fb6797238abb45821b7ea5671850e060899b8ac0
Jeroen Desloovere <jeroendesloovere@gmail.com>
2012-01-31 11:09:52 +0100

    Added language

--- frontend/core/js/jquery/jquery.frontend.js
+++ frontend/core/js/jquery/jquery.frontend.js
@@ -214,6 +214,7 @@
 							html += '<li class="shareMenuTwitter">' +
 									'	<a href="http://twitter.com/share" class="twitter-share-button" data-url="' + link + '"';
 							if(title != '') html += ' data-text="' + title + '"';
+							html += ' data-lang="' + jsFrontend.current.language + '"';
 							html += 'data-count="none">' + options.twitter.label  + '</a>';
 							html += '</li>';
 						break;
