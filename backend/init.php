256e9f9468575f03422d2b6314231fe94a1f058f
dieterve <dieter@dieterve.be>
2012-02-17 13:55:48 +0100

    Rename base directory to common

--- backend/init.php
+++ backend/init.php
@@ -129,7 +129,7 @@ class BackendInit
 			{
 				$chunks = $parts;
 				array_shift($chunks);
-				$pathToLoad = PATH_LIBRARY . '/base/' . strtolower(implode('_', $chunks)) . '.php';
+				$pathToLoad = PATH_LIBRARY . '/common/' . strtolower(implode('_', $chunks)) . '.php';
 
 				if(SpoonFile::exists($pathToLoad)) require_once $pathToLoad;
 			}
