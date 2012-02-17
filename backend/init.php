e3f58e317771abcfe712b5745f180bef91f7d577
dieterve <dieter@dieterve.be>
2012-02-17 14:32:15 +0100

    Use a more dummyproof way of making the config optional

--- backend/init.php
+++ backend/init.php
@@ -342,13 +342,12 @@ class BackendInit
 		@include_once dirname(__FILE__) . '/cache/config/config.php';
 
 		// config doest not exist, use standard library location
-		if(!defined('INIT_PATH_LIBRARY')) $libraryPath = dirname(__FILE__) . '/../library';
-		else $libraryPath = INIT_PATH_LIBRARY;
+		if(!defined('INIT_PATH_LIBRARY')) define('INIT_PATH_LIBRARY', dirname(__FILE__) . '/../library');
 
 		// load the globals
-		$installed[] = @include_once $libraryPath . '/globals.php';
-		$installed[] = @include_once $libraryPath . '/globals_backend.php';
-		$installed[] = @include_once $libraryPath . '/globals_frontend.php';
+		$installed[] = @include_once INIT_PATH_LIBRARY . '/globals.php';
+		$installed[] = @include_once INIT_PATH_LIBRARY . '/globals_backend.php';
+		$installed[] = @include_once INIT_PATH_LIBRARY . '/globals_frontend.php';
 
 		// something could not be loaded
 		if(in_array(false, $installed))
