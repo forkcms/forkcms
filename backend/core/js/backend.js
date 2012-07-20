ba09dc61f02fd48d9c2b9ca6c625611c706498eb
Jan Moesen <github.com@moesen.nu>
2012-07-20 14:44:19 +0200

    backend.js: locale: split "if" oneliners
    They are needlessly illegible.

--- backend/core/js/backend.js
+++ backend/core/js/backend.js
@@ -1746,13 +1746,19 @@ jsBackend.locale =
 	get: function(type, key, module)
 	{
 		// initialize if needed
-		if(!jsBackend.locale.initialized) jsBackend.locale.init();
+		if(!jsBackend.locale.initialized)
+		{
+			jsBackend.locale.init();
+		}
 
 		// validate
 		if(typeof jsBackend.locale.data[type][module] == 'undefined' || typeof jsBackend.locale.data[type][module][key] == 'undefined')
 		{
 			// not available in core?
-			if(typeof jsBackend.locale.data[type]['core'][key] == 'undefined') return '{$' + type + key + '}';
+			if(typeof jsBackend.locale.data[type]['core'][key] == 'undefined')
+			{
+				return '{$' + type + key + '}';
+			}
 
 			// fallback to core
 			return jsBackend.locale.data[type]['core'][key];
