2de9badf7b69b30bcb9ce03edd4cfaa19fb7557c
Tijs Verkoyen <tijs@verkoyen.eu>
2012-09-18 18:24:40 +0200

    Don't add exception handlers if we have defined our own exception handler

--- backend/init.php
+++ backend/init.php
@@ -231,9 +231,6 @@ class BackendInit extends KernelLoader
 	 */
 	private function setDebugging()
 	{
-		// in debug mode notices are triggered when using non existing locale, so we use a custom errorhandler to cleanup the message
-		set_error_handler(array('BackendInit', 'errorHandler'));
-
 		// debugging enabled
 		if(SPOON_DEBUG)
 		{
