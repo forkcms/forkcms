25b7ed909e20367af06ff1150831677247a930f0
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2012-09-21 23:50:27 +0200

    Frontend: add base js kernel functionallity.
    Signed-off-by: Jelmer Snoeck <jelmer.snoeck@gmail.com>

--- frontend/js.php
+++ frontend/js.php
@@ -6,7 +6,14 @@
  * For the full copyright and license information, please view the license
  * file that was distributed with this source code.
  */
-require_once '../autoload.php';
 
-new FrontendInit('frontend_js');
-new FrontendJavascript();
+use Symfony\Component\HttpFoundation\Request;
+
+// require
+// @todo this is wrong. It will break our install validating.
+require_once 'autoload.php';
+
+$kernel = new AppKernel();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
