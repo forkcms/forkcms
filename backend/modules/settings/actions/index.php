ef101d7e54a0d56941fb3a70f9588cbb08466300
Tijs Verkoyen <tijs@verkoyen.eu>
2012-10-14 10:38:41 +0200

    No need for the full classname with namespace because of the use-statement.

--- backend/modules/settings/actions/index.php
+++ backend/modules/settings/actions/index.php
@@ -191,7 +191,7 @@ class BackendSettingsIndex extends BackendBaseActionIndex
 				if($this->frm->getField('akismet_key')->getValue() != BackendModel::getModuleSetting('core', 'akismet_key', null))
 				{
 					 // create instance
-					$akismet = new \TijsVerkoyen\Akismet\Akismet($this->frm->getField('akismet_key')->getValue(), SITE_URL);
+					$akismet = new Akismet($this->frm->getField('akismet_key')->getValue(), SITE_URL);
 
 					// invalid key
 					if(!$akismet->verifyKey()) $this->frm->getField('akismet_key')->setError(BL::err('InvalidAPIKey'));
