e7d0232a7af38ec3589ff0ca3af32e0585fac6fa
Dave Lens <github@davelens.be>
2012-10-24 14:06:18 +0200

    remove app/config

--- app/config/globals_dev.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DB_TYPE', 'mysql');
-define('DB_DATABASE', '');
-define('DB_HOSTNAME', 'localhost');
-define('DB_PORT', '3306');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
-
