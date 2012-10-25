d269247e2f4f038f1470de32c01490b30d7a318f
Dave Lens <github@davelens.be>
2012-10-25 15:29:31 +0200

    move bootstrap.php require to autoload

--- autoload.php
+++ autoload.php
@@ -123,3 +123,4 @@ set_include_path(__DIR__ . '/library' . PATH_SEPARATOR . get_include_path());
 require_once 'spoon/spoon.php';
 
 require_once __DIR__ . '/app/AppKernel.php';
+require_once __DIR__ . '/app/bootstrap.php';
--- index.php
+++ index.php
@@ -7,7 +7,4 @@
  * file that was distributed with this source code.
  */
 
-use Symfony\Component\HttpFoundation\Request;
-
 require_once __DIR__ . '/autoload.php';
-require_once __DIR__ . '/app/bootstrap.php';
