ccdfe8dc8fba45b03f2a541a10efe4f1fafd1262
Tijs Verkoyen <tijs@verkoyen.eu>
2012-11-05 08:16:44 +0100

    Added vars for different db-credentials on the production-server

--- deployment/production.rb
+++ deployment/production.rb
@@ -2,3 +2,9 @@ set :production_server, "#{production_hostname}"
 set :user, "#{production_account}"
 set :app_path, "apps/#{application}"
 set :document_root, "#{production_document_root}"
+
+# Uncomment the variables below if the DB-credentials are different from
+# the credentials on the staging server
+#set :db_username, ""
+#set :db_password, ""
+#set :db_host, ""
