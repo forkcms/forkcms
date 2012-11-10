0f7cbf845f2a64c3bbe987d0f5ef2a66dd1950a7
Tijs Verkoyen <tijs@verkoyen.eu>
2012-11-10 20:03:26 +0100

    Added a file for theme related JS.

---
+++ frontend/themes/bootstrap/core/js/theme.js
@@ -0,0 +1,14 @@
+/**
+ * Theme related JS
+ *
+ * @author	Tijs Verkoyen <tijs@sumocoders.be>
+ */
+jsFrontend.theme =
+{
+	init: function()
+	{
+		// insert your code here
+	}
+}
+
+$(jsFrontend.theme.init);
