78812ce1e75caeff1323e13dea11f22564423df8
Siesqo <siesqo2@imac-van-francis-logghe.local>
2013-02-05 11:46:24 +0100

    Pages: copy function added in Model
    Copy function could be used from other module.

--- backend/modules/pages/engine/model.php
+++ backend/modules/pages/engine/model.php
@@ -13,6 +13,7 @@
  * @author Tijs Verkoyen <tijs@sumocoders.be>
  * @author Davy Hellemans <davy.hellemans@netlash.com>
  * @author Matthias Mullie <forkcms@mullie.eu>
+ * @author Jeroen Desloovere <jeroen@siesqo.be>
  */
 class BackendPagesModel
 {
@@ -408,6 +409,234 @@ class BackendPagesModel
 
 		// trigger an event
 		BackendModel::triggerEvent('pages', 'after_recreated_cache');
+	}
+
+	/**
+	 * Copy pages
+	 *
+	 * @param string $from
+	 * @param string $to
+	 */
+	public static function copy($from, $to)
+	{
+		// get db
+		$db = BackendModel::getDB(true);
+
+		// init variables
+		$contentBlockIds = array();
+		$oldIds = array();
+		$newIds = array();
+
+		// copy the contentblocks
+		$contentBlocks = (array) $db->getRecords(
+			'SELECT * FROM content_blocks WHERE language = ? AND status = "active"',
+			array($from)
+		);
+
+		// define counter
+		$i = 1;
+
+		// loop existing content blocks
+		foreach($contentBlocks as $contentBlock)
+		{
+			// define old id
+			$oldId = $contentBlock['extra_id'];
+
+			// init new block
+			$newBlock = array();
+
+			// build new block
+			$newBlock['id'] = BackendContentBlocksModel::getMaximumId() + $i;
+			$newBlock['language'] = $to;
+			$newBlock['created_on'] = BackendModel::getUTCDate();
+			$newBlock['edited_on'] = BackendModel::getUTCDate();
+			$newBlock['status'] = $contentBlock['status'];
+			$newBlock['user_id'] = BackendAuthentication::getUser()->getUserId();
+			$newBlock['template'] = $contentBlock['template'];
+			$newBlock['title'] = $contentBlock['title'];
+			$newBlock['text'] = $contentBlock['text'];
+			$newBlock['hidden'] = $contentBlock['hidden'];
+
+			// inset content block
+			$newId = BackendContentBlocksModel::insert($newBlock);
+
+			// save ids for later
+			$oldIds[] = $oldId;
+			$newIds[$oldId] = $newId;
+
+			// redefine counter
+			$i++;
+		}
+
+		// get the extra Ids for the content blocks
+		if(!empty($newIds))
+		{
+			// get content block extra ids
+			$contenBlockExtraIds = (array) $db->getRecords(
+				'SELECT revision_id, extra_id FROM content_blocks WHERE revision_id IN (' . implode(',', $newIds) . ')'
+			);
+
+			// loop new ids
+			foreach($newIds as $oldId => $newId)
+			{
+				foreach($contenBlockExtraIds as $extraId)
+				{
+					if($extraId['revision_id'] == $newId) $contentBlockIds[$oldId] = $extraId['extra_id'];
+				}
+			}
+		}
+
+		// get all old pages
+		$ids = $db->getColumn(
+			'SELECT id
+			 FROM pages AS i
+			 WHERE i.language = ? AND i.status = ?',
+			array($to, 'active')
+		);
+
+		// any old pages
+		if(!empty($ids))
+		{
+			// delete existing pages
+			foreach($ids as $id)
+			{
+				// redefine
+				$id = (int) $id;
+
+				// get revision ids
+				$revisionIDs = (array) $db->getColumn(
+					'SELECT i.revision_id
+					 FROM pages AS i
+					 WHERE i.id = ? AND i.language = ?',
+					array($id, $to)
+				);
+
+				// get meta ids
+				$metaIDs = (array) $db->getColumn(
+					'SELECT i.meta_id
+					 FROM pages AS i
+					 WHERE i.id = ? AND i.language = ?',
+					array($id, $to)
+				);
+
+				// delete meta records
+				if(!empty($metaIDs)) $db->delete('meta', 'id IN (' . implode(',', $metaIDs) . ')');
+
+				// delete blocks and their revisions
+				if(!empty($revisionIDs)) $db->delete('pages_blocks', 'revision_id IN (' . implode(',', $revisionIDs) . ')');
+
+				// delete page and the revisions
+				if(!empty($revisionIDs)) $db->delete('pages', 'revision_id IN (' . implode(',', $revisionIDs) . ')');
+			}
+		}
+
+		// delete search indexes
+		$db->delete('search_index', 'module = ? AND language = ?', array('pages', $to));
+
+		// get all active pages
+		$ids = BackendModel::getDB()->getColumn(
+			'SELECT id
+			 FROM pages AS i
+			 WHERE i.language = ? AND i.status = ?',
+			array($from, 'active')
+		);
+
+		// loop
+		foreach($ids as $id)
+		{
+			// get data
+			$sourceData = BackendPagesModel::get($id, null, $from);
+
+			// get and build meta
+			$meta = $db->getRecord(
+				'SELECT *
+				 FROM meta
+				 WHERE id = ?',
+				array($sourceData['meta_id'])
+			);
+
+			// remove id
+			unset($meta['id']);
+
+			// init page
+			$page = array();
+
+			// build page
+			$page['id'] = $sourceData['id'];
+			$page['user_id'] = BackendAuthentication::getUser()->getUserId();
+			$page['parent_id'] = $sourceData['parent_id'];
+			$page['template_id'] = $sourceData['template_id'];
+			$page['meta_id'] = (int) $db->insert('meta', $meta);
+			$page['language'] = $to;
+			$page['type'] = $sourceData['type'];
+			$page['title'] = $sourceData['title'];
+			$page['navigation_title'] = $sourceData['navigation_title'];
+			$page['navigation_title_overwrite'] = $sourceData['navigation_title_overwrite'];
+			$page['hidden'] = $sourceData['hidden'];
+			$page['status'] = 'active';
+			$page['publish_on'] = BackendModel::getUTCDate();
+			$page['created_on'] = BackendModel::getUTCDate();
+			$page['edited_on'] = BackendModel::getUTCDate();
+			$page['allow_move'] = $sourceData['allow_move'];
+			$page['allow_children'] = $sourceData['allow_children'];
+			$page['allow_edit'] = $sourceData['allow_edit'];
+			$page['allow_delete'] = $sourceData['allow_delete'];
+			$page['sequence'] = $sourceData['sequence'];
+			$page['data'] = ($sourceData['data'] !== null) ? serialize($sourceData['data']) : null;
+
+			// insert page, store the id, we need it when building the blocks
+			$revisionId = BackendPagesModel::insert($page);
+
+			// init var
+			$blocks = array();
+			$hasBlock = ($sourceData['has_extra'] == 'Y');
+
+			// get the blocks
+			$sourceBlocks = BackendPagesModel::getBlocks($id, null, $from);
+
+			// loop blocks
+			foreach($sourceBlocks as $sourceBlock)
+			{
+				// build block
+				$block = $sourceBlock;
+				$block['revision_id'] = $revisionId;
+				$block['created_on'] = BackendModel::getUTCDate();
+				$block['edited_on'] = BackendModel::getUTCDate();
+
+				if(in_array($block['extra_id'], $oldIds))
+				{
+					$block['extra_id'] = $contentBlockIds[$block['extra_id']];
+				}
+
+				// add block
+				$blocks[] = $block;
+			}
+
+			// insert the blocks
+			BackendPagesModel::insertBlocks($blocks, $hasBlock);
+
+			// check if the method exists
+			if(method_exists('BackendSearchModel', 'saveIndex'))
+			{
+				// init var
+				$text = '';
+
+				// build search-text
+				foreach($blocks as $block) $text .= ' ' . $block['html'];
+
+				// add
+				BackendSearchModel::saveIndex('pages', (int) $page['id'], array('title' => $page['title'], 'text' => $text), $to);
+			}
+
+			// get tags
+			$tags = BackendTagsModel::getTags('pages', $id, 'string', $from);
+
+			// save tags
+			if($tags != '') BackendTagsModel::saveTags($page['id'], $tags, 'pages');
+		}
+
+		// build cache
+		BackendPagesModel::buildCache($to);
 	}
 
 	/**
