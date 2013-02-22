a9741e739a791f05463d1ea67e4e87b1c90b51db
Tijs Verkoyen <tijs@verkoyen.eu>
2013-02-22 16:24:52 +0100

    Revert "Revert "Added Errbit class""
    This reverts commit 13d4b0c01d65fd0d12320e9336b2c40692d144f9.

---
+++ library/external/errbit/Errbit/Errors/Error.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Errbit PHP Notifier.
+ *
+ * Copyright © Flippa.com Pty. Ltd.
+ * See the LICENSE file for details.
+ */
+
+class Errbit_Errors_Error extends Errbit_Errors_Base {
+}
