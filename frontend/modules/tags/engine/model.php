28a4b2fbf7e4b08c9b8108f9c6a6854bca065be9
Siesqo <siesqo2@imac-van-francis-logghe.local>
2013-03-11 17:58:38 +0100

    Tags: $language added to FrontendTagsModel::get()

--- frontend/modules/tags/engine/model.php
+++ frontend/modules/tags/engine/model.php
@@ -48,16 +48,20 @@ class FrontendTagsModel
 	 * Get the tag for a given URL
 	 *
 	 * @param string $URL The URL to get the tag for.
+	 * @param string[optional] $language
 	 * @return array
 	 */
-	public static function get($URL)
+	public static function get($URL, $language = null)
 	{
+		// redefine language
+		$language = ($language !== null) ? (string) $language : FRONTEND_LANGUAGE;
+
 		// exists
 		return (array) FrontendModel::getDB()->getRecord(
 			'SELECT id, language, tag AS name, number, url
 			 FROM tags
 			 WHERE url = ? AND language = ?',
-			array((string) $URL, FRONTEND_LANGUAGE)
+			array((string) $URL, $language)
 		);
 	}
 
