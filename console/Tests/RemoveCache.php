d67426e63ba2441ae8f978818260afd059c66133
Tijs Verkoyen <tijs@verkoyen.eu>
2013-04-12 20:04:38 +0200

    Added checks to see if the folders are really empty

--- console/Tests/RemoveCache.php
+++ console/Tests/RemoveCache.php
@@ -5,6 +5,8 @@ require_once __DIR__.'/../Cache/RemoveCache.php';
 
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 use Fork\ConsoleBundle\Command\RemoveCache;
 
 class RemoveCacheCommandTest extends \PHPUnit_Framework_TestCase
@@ -22,5 +24,25 @@ class RemoveCacheCommandTest extends \PHPUnit_Framework_TestCase
 
 		// check the output
 		$this->assertRegExp('/All done/', $commandTester->getDisplay());
+
+		// create some instances
+		$finder = new Finder();
+		$fs = new Filesystem();
+
+		// build path to the rootdirectory
+		$rootPath = __DIR__ . '/../..';
+
+		// check if the installers cache is empty
+		$finder->files()->in($rootPath . '/install/cache');
+		$finder->files()->notName('installed.txt');
+		$this->assertCount(0, $finder);
+
+		// check if the frontends cache is empty
+		$finder->files()->in($rootPath . '/frontend/cache');
+		$this->assertCount(0, $finder);
+
+		// check if the frontends cache is empty
+		$finder->files()->in($rootPath . '/backend/cache');
+		$this->assertCount(0, $finder);
 	}
 }
