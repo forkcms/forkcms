c3088b94c60db0101fd7cdb71736a695bbd64522
Tijs Verkoyen <tijs@verkoyen.eu>
2013-04-12 19:34:26 +0200

    We will use the Finder & Filesystem components, so initiate them.

--- console/Cache/RemoveCache.php
+++ console/Cache/RemoveCache.php
@@ -5,6 +5,8 @@ namespace Fork\ConsoleBundle\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 
 class RemoveCache extends Command
 {
@@ -18,6 +20,13 @@ class RemoveCache extends Command
 
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
+		// create some instances
+		$finder = new Finder();
+		$fs = new Filesystem();
+
+		// build path to the rootdirectory
+		$rootPath = __DIR__ . '/../..';
+
 		$text = '<info>All done</info>';
 		$output->writeln($text);
 	}
