fc936eba223c49eefbaa2a5047381f9207b433fc
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2013-05-06 16:19:14 +0200

    Installer: move yaml configuration creation.
    This way, the configuration is build after step 6. Therefore, we can use
    the configuration in step 7.

--- install/engine/step_7.php
+++ install/engine/step_7.php
@@ -99,33 +99,6 @@ class InstallerStep7 extends InstallerStep
 	}
 
 	/**
-	 * Writes a config file to app/config/parameters.yml.
-	 */
-	private function createYAMLConfig()
-	{
-		// these variables should be parsed inside the config file(s).
-		$variables = $this->getConfigurationVariables();
-
-		// map the config templates to their destination filename
-		$yamlFiles = array(
-			PATH_WWW . '/app/config/parameters.base.yml' => PATH_WWW . '/app/config/parameters.yml',
-		);
-
-		foreach($yamlFiles as $sourceFilename => $destinationFilename)
-		{
-			$yamlContent = SpoonFile::getContent($sourceFilename);
-			$yamlContent = str_replace(
-				array_keys($variables),
-				array_values($variables),
-				$yamlContent
-			);
-
-			// write app/config/parameters.yml
-			SpoonFile::setContent($destinationFilename, $yamlContent);
-		}
-	}
-
-	/**
 	 * Create locale cache files
 	 */
 	private function createLocaleFiles()
@@ -231,9 +204,6 @@ class InstallerStep7 extends InstallerStep
 		// delete cached data
 		$this->deleteCachedData();
 
-		// create configuration files
-		$this->createYAMLConfig();
-
 		// define paths
 		$this->definePaths();
 
@@ -251,31 +221,6 @@ class InstallerStep7 extends InstallerStep
 
 		// clear session
 		SpoonSession::destroy();
-	}
-
-	/**
-	 * @return array A list of variables that should be parsed into the configuration file(s).
-	 */
-	protected function getConfigurationVariables()
-	{
-		return array(
-			'<debug-mode>'				=> SpoonSession::get('debug_mode') ? 'true' : 'false',
-			'<debug-email>'				=> SpoonSession::get('different_debug_email') ? SpoonSession::get('debug_email') : SpoonSession::get('email'),
-			'<database-name>'			=> SpoonSession::get('db_database'),
-			'<database-host>'			=> addslashes(SpoonSession::get('db_hostname')),
-			'<database-user>'			=> addslashes(SpoonSession::get('db_username')),
-			'<database-password>'		=> addslashes(SpoonSession::get('db_password')),
-			'<database-port>'			=> (SpoonSession::exists('db_port') && SpoonSession::get('db_port') != '') ? addslashes(SpoonSession::get('db_port')) : 3306,
-			'<site-protocol>'			=> isset($_SERVER['SERVER_PROTOCOL']) ? (strpos(strtolower($_SERVER['SERVER_PROTOCOL']), 'https') === false ? 'http' : 'https') : 'http',
-			'<site-domain>'				=> (isset($_SERVER['HTTP_HOST'])) ? $_SERVER['HTTP_HOST'] : 'fork.local',
-			'<site-default-title>'		=> 'Fork CMS',
-			'<site-multilanguage>'		=> SpoonSession::get('multiple_languages') ? 'true' : 'false',
-			'<site-default-language>'	=> SpoonSession::get('default_language'),
-			'<path-www>'				=> PATH_WWW,
-			'<path-library>'			=> PATH_LIBRARY,
-			'<action-group-tag>'		=> '\@actiongroup',
-			'<action-rights-level>'		=> 7
-		);
 	}
 
 	/**
