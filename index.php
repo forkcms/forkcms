57449166bc2d8a680e2a21b9821e784de332dc86
Tijs Verkoyen <tijs@verkoyen.eu>
2013-10-30 17:21:29 +0100

    Applied PSR-code-styling standards

--- index.php
+++ index.php
@@ -8,8 +8,11 @@
  */
 
 // vendors not installed
-if(!is_dir(__DIR__ . '/vendor')) {
-    echo 'Your install is missing some dependencies. If you have composer installed you should run: <code>composer install</code>. If you don\'t have composer installed you really should, see http://getcomposer.org for more information';
+if (!is_dir(__DIR__ . '/vendor')) {
+    echo 'Your install is missing some dependencies. If you have composer
+          installed you should run: <code>composer install</code>. If you
+          don\'t have composer installed you really should, see
+          http://getcomposer.org for more information';
     exit;
 }
 
@@ -19,20 +22,18 @@ use Symfony\Component\HttpFoundation\Request;
 
 // Fork has not yet been installed
 $installer = dirname(__FILE__) . '/install/cache';
-$request = Request::createFromGlobals();
-if(
-    file_exists($installer) &&
+$request   = Request::createFromGlobals();
+if (file_exists($installer) &&
     is_dir($installer) &&
     !file_exists($installer . '/installed.txt') &&
     substr($request->getRequestURI(), 0, 8) != '/install'
-)
-{
+) {
     header('Location: /install');
     exit;
 }
 
 require_once __DIR__ . '/app/AppKernel.php';
 
-$kernel = new AppKernel();
+$kernel   = new AppKernel();
 $response = $kernel->handle($request);
 $response->send();
