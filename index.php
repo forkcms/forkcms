8899802d07e11594587dbd50ef0dab49a4b1912d
Tijs Verkoyen <tijs@verkoyen.eu>
2013-11-20 16:34:49 +0100

    Applied PSR2-code-styling

--- index.php
+++ index.php
@@ -23,15 +23,17 @@ use Symfony\Component\HttpFoundation\Request;
 // Fork has not yet been installed
 $installer = dirname(__FILE__) . '/install/cache';
 $request   = Request::createFromGlobals();
-if (
-    file_exists($installer) &&
+if (file_exists($installer) &&
     is_dir($installer) &&
     !file_exists($installer . '/installed.txt') &&
     substr($request->getRequestURI(), 0, 8) != '/install'
 ) {
     // check .htaccess
     if (!file_exists('.htaccess') && !isset($_GET['skiphtaccess'])) {
-        echo 'Your install is missing the .htaccess file. Make sure you show hidden files while uploading Fork CMS. Read the article about <a href="http://www.fork-cms.com/community/documentation/detail/installation/webservers">webservers</a> for further information. <a href="?skiphtaccess">Skip .htaccess check</a>';
+        echo 'Your install is missing the .htaccess file. Make sure you show
+              hidden files while uploading Fork CMS. Read the article about
+              <a href="http://www.fork-cms.com/community/documentation/detail/installation/webservers">webservers</a>
+              for further information. <a href="?skiphtaccess">Skip .htaccess check</a>';
         exit;
     }
 
@@ -41,13 +43,13 @@ if (
 
 require_once __DIR__ . '/app/AppKernel.php';
 
-$kernel = new AppKernel();
+$kernel   = new AppKernel();
 $response = $kernel->handle($request);
 if ($response->getCharset() === null && $kernel->getContainer() != null) {
     $response->setCharset(
-             $kernel->getContainer()->getParameter(
-                    'kernel.charset'
-             )
+        $kernel->getContainer()->getParameter(
+            'kernel.charset'
+        )
     );
 }
 $response->send();
