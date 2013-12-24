badd6481c07b5156ad003a4e67a37046fe6aa179
Dieter Wyns <dieter.wyns@wijs.be>
2013-12-24 14:51:28 +0100

    Fixed syntax. Was invalid after merge

--- backend/modules/tags/ajax/edit.php
+++ backend/modules/tags/ajax/edit.php
@@ -27,34 +27,19 @@ class BackendTagsAjaxEdit extends BackendBaseAJAXAction
 
 		// validate id
 		if($id === 0) $this->output(self::BAD_REQUEST, null, 'no id provided');
-		if($tag === '') $this->output(self::BAD_REQUEST, null, BL::err('NameIsRequired'));
 
-		// check if tag exists
-		if(BackendTagsModel::existsTag($tag)) $this->output(self::BAD_REQUEST, null, BL::err('TagAlreadyExists'));
-
-		// build array
-		$item['id'] = $id;
-		$item['tag'] = SpoonFilter::htmlspecialchars($tag);
-		$item['url'] = BackendTagsModel::getURL(CommonUri::getUrl(SpoonFilter::htmlspecialcharsDecode($item['tag'])), $id);
-
-		// update
-		BackendTagsModel::update($item);
-
-		// output
-		$this->output(self::OK, $item, vsprintf(BL::msg('Edited'), array($item['tag'])));
-		
 		// validated id
 		else
 		{
 			// validate tag name
 			if($tag === '') $this->output(self::BAD_REQUEST, null, BL::err('NameIsRequired'));
-			
+
 			// validated tag name
 			else
 			{
 				// check if tag exists
 				if(BackendTagsModel::existsTag($tag)) $this->output(self::BAD_REQUEST, null, BL::err('TagAlreadyExists'));
-		
+
 				// tags doesn't exists yet
 				else
 				{
@@ -62,10 +47,10 @@ class BackendTagsAjaxEdit extends BackendBaseAJAXAction
 					$item['id'] = $id;
 					$item['tag'] = SpoonFilter::htmlspecialchars($tag);
 					$item['url'] = BackendTagsModel::getURL(SpoonFilter::urlise(SpoonFilter::htmlspecialcharsDecode($item['tag'])), $id);
-			
+
 					// update
 					BackendTagsModel::update($item);
-			
+
 					// output
 					$this->output(self::OK, $item, vsprintf(BL::msg('Edited'), array($item['tag'])));
 				}
