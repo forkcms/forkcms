3b12fe01bcac2cb43e5fe4423bd67a74acefd1f6
Waldo Cosman <waldo_cosman@hotmail.com>
2014-01-28 14:15:24 +0100

    Change notation of comments

--- backend/modules/pages/actions/generate_images.php
+++ backend/modules/pages/actions/generate_images.php
@@ -28,59 +28,59 @@ class BackendPagesGenerateImages extends BackendBaseActionIndex
 		// call parent, this will probably add some general CSS/JS or other required files
 		parent::execute();
 
-		//--Get all the modules
+		// Get all the modules
 		$modules = BackendModel::getModules();
 
-		//--Check if modules is not empty
+		// Check if modules is not empty
 		if(!empty($modules))
 		{
-			//--Create filesystem object
+			// Create filesystem object
 			$filesystem = new Filesystem();
 
-			//--Loop the modules
+			// Loop the modules
 			foreach($modules as $module)
 			{
-				//--Create var dir for ease of use
+				// Create var dir for ease of use
 				$dir = FRONTEND_FILES_PATH . '/' . $module . '/images/';
 
-				//--Check if dir exists
+				// Check if dir exists
 				if($filesystem->exists($dir . 'source/'))
 				{
-					//--Create Finder object
+					// Create Finder object
 					$finderDir = new Finder();
 
-					//--Get all the dirs in the modules/images folder.
+					// Get all the dirs in the modules/images folder.
 					$dirs = $finderDir->directories()->in($dir)->exclude(array("source"));
 
-					//--Check if $dirs is not empty
+					// Check if $dirs is not empty
 					if(!empty($dirs))
 					{
-						//--Create Finder object for the files
+						// Create Finder object for the files
 						$finderFiles = new Finder();
 
-						//--Get all the files in the source-dir
+						// Get all the files in the source-dir
 						$files = $finderFiles->files()->in($dir . 'source/');
 
-						//--Check if $files is not empty
+						// Check if $files is not empty
 						if(!empty($files))
 						{
-							//--Loop all the dirs
+							// Loop all the dirs
 							foreach($dirs as $row)
 							{
-								//--Explode the dir-name
+								// Explode the dir-name
 								$chunks = explode("x", $row->getBasename(), 2);
 
-								//--Create folder array
+								// Create folder array
 								$folder = array();
 								$folder['dirname'] = $row->getBasename();
 								$folder['path'] = $row->getRealPath();
 								$folder['width'] = ($chunks[0] != '') ? (int)$chunks[0] : null;
 								$folder['height'] = ($chunks[1] != '') ? (int)$chunks[1] : null;
 
-								//--Loop all the files
+								// Loop all the files
 								foreach($files as $file)
 								{
-									//--Check if the file exists
+									// Check if the file exists
 									if(!$filesystem->exists($dir . $row->getBasename() . '/' . $file->getBasename()))
 									{
 
