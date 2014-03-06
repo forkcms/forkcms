76adb37666d93693b15ac2b556e2a8b37e2d714b
Jelmer Prins <jelmer@sumocoders.be>
2014-03-06 13:22:49 +0100

    the backend model for the partner widget

---
+++ backend/modules/partner_module/engine/model.php
@@ -0,0 +1,98 @@
+<?php
+
+/*
+ * This file is part of Fork CMS.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
+/**
+ * This is the model voor the partner module.
+ *
+ * @author Jelmer Prins <jelmer@sumocoders.be>
+ */
+class BackendPartnerModuleModel
+{
+	const QRY_DATAGRID_BROWSE =
+		'SELECT i.id, i.name, i.img, i.url
+		 FROM partner_module AS i';
+
+	/**
+	 * Deletes one or more items
+	 *
+	 * @param int $id
+	 */
+	public static function delete($id)
+	{
+		$id = (int) $id;
+		$db = BackendModel::getContainer()->get('database');
+
+		// delete records
+		$db->delete('partner_module', 'id = ?', $id);
+
+		// invalidate the cache for partner_module
+		BackendModel::invalidateFrontendCache('partner_module');
+	}
+
+	/**
+	 * Get all data for a given id
+	 *
+	 * @param int $id
+	 * @return array
+	 */
+	public static function get($id)
+	{
+		return (array) BackendModel::getContainer()->get('database')->getRecord(
+            'SELECT i.id, i.name, i.img, i.url
+             FROM partner_module AS i
+             WHERE i.id = ?',
+			array((int) $id)
+		);
+	}
+
+	/**
+	 * Get the maximum id
+	 *
+	 * @return int
+	 */
+	public static function getMaximumId()
+	{
+		return (int) BackendModel::getContainer()->get('database')->getVar('SELECT MAX(id) FROM partner_module LIMIT 1');
+	}
+
+	/**
+	 * Inserts an item into the database
+	 *
+	 * @param array $item
+	 * @return int
+	 */
+	public static function insert(array $item)
+	{
+		// insert and return the new partner id
+		$item['id'] = BackendModel::getContainer()->get('database')->insert('partner_module', $item);
+
+		// invalidate the cache for blog
+		BackendModel::invalidateFrontendCache('partner_module');
+
+		return $item['id'];
+	}
+
+	/**
+	 * Update an existing item
+	 *
+	 * @param array $item
+	 * @return int
+	 */
+	public static function update(array $item)
+	{
+		// update
+		BackendModel::getContainer()->get('database')->update('partner_module', $item, 'id = ?', array($item['id']));
+
+		// invalidate the cache for blog
+		BackendModel::invalidateFrontendCache('partner_module');
+
+		return $item['id'];
+	}
+
+}
