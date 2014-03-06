3958bfae35ddbf8760df5491228a275b6e6998b1
Jelmer Prins <jelmer@sumocoders.be>
2014-03-06 11:30:17 +0100

    added initial config

---
+++ backend/modules/partner_module/config.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of Fork CMS.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
+/**
+ * This action will add a post to the blog module.
+ *
+ * @author Jelmer Prins <jelmer@sumocoders.be>
+ */
+final class BackendPartnerModuleConfig extends BackendBaseConfig
+{
+	/**
+	 * The default action
+	 *
+	 * @var	string
+	 */
+	protected $defaultAction = 'index';
+
+
+	/**
+	 * The disabled actions
+	 *
+	 * @var	array
+	 */
+	protected $disabledActions = array();
+}
