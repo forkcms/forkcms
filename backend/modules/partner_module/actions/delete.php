f421e91d5d349f663264130038e3238d39e58fba
Jelmer Prins <jelmer@sumocoders.be>
2014-03-06 16:39:49 +0100

    implemented deletion of a partner

---
+++ backend/modules/partner_module/actions/delete.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of Fork CMS.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
+/**
+ * This action will delete a partner
+ *
+ * @author Jelmer Prins <jelmer@ubuntu.com>
+ */
+class BackendPartnerModuleDelete extends BackendBaseActionDelete
+{
+	/**
+	 * Execute the action
+	 */
+	public function execute()
+	{
+		$this->id = $this->getParameter('id', 'int');
+		// does the item exist
+		if($this->id !== null && BackendPartnerModuleModel::exists($this->id))
+		{
+			// call parent, this will probably add some general CSS/JS or other required files
+
+
+			// get data
+			$this->record = (array) BackendPartnerModuleModel::get($this->id);
+
+			// delete item
+			BackendPartnerModuleModel::delete($this->id);
+
+			// item was deleted, so redirect
+			$this->redirect(BackendModel::createURLForAction('index') . '&report=deleted&var=' . urlencode($this->record['name']));
+		}
+
+		// something went wrong
+		else $this->redirect(BackendModel::createURLForAction('index') . '&error=non-existing');
+	}
+}
