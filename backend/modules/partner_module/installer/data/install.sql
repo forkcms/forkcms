77ea58c430a5a6c7d60bfe4fda03460fada6193b
Jelmer Prins <jelmer@sumocoders.be>
2014-03-06 16:44:35 +0100

    updated the installer

--- backend/modules/partner_module/installer/data/install.sql
+++ backend/modules/partner_module/installer/data/install.sql
@@ -1,7 +1,10 @@
 CREATE TABLE `partner_module` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT '',
-  `img` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL DEFAULT '',
   `url` varchar(255) NOT NULL DEFAULT '',
+  `created_by` int(11) NOT NULL,
+  `created_on` datetime NOT NULL,
+  `edited_on` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
