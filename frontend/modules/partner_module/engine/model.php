dc16c909e2808303ab1460d8c9c720436f8ba154
Jelmer Prins <jelmer@sumocoders.be>
2014-03-14 14:40:04 +0100

    file location change

--- frontend/modules/partner_module/engine/model.php
+++ frontend/modules/partner_module/engine/model.php
@@ -16,7 +16,7 @@ class FrontendPartnerModuleModel
     /**
      * The location where the images are stored within the files directory
      */
-    const IMAGE_PATH = 'partner_module/source';
+    const IMAGE_PATH = 'partner_module';
 
     /**
      * The location where the thumbnails are stored within the files directory
