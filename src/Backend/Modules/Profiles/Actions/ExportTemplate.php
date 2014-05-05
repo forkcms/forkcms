0abaaace23dac1020f763b6a6f34a999db09fafb
Jeroen Desloovere <jeroendesloovere@gmail.com>
2014-05-05 08:14:09 +0200

    Using Backend_cache_path

--- src/Backend/Modules/Profiles/Actions/ExportTemplate.php
+++ src/Backend/Modules/Profiles/Actions/ExportTemplate.php
@@ -11,7 +11,6 @@ namespace Backend\Modules\Profiles\Actions;
 
 use Backend\Core\Engine\Base\ActionAdd as BackendBaseActionAdd;
 use Backend\Core\Engine\Model as BackendModel;
-use Backend\Modules\Profiles\Engine\Model as BackendProfilesModel;
 
 /**
  * This is the add-action, it will display a form to add a new profile.
@@ -21,24 +20,24 @@ use Backend\Modules\Profiles\Engine\Model as BackendProfilesModel;
 class ExportTemplate extends BackendBaseActionAdd
 {
     /**
-	 * Execute the action.
-	 */
-	public function execute()
-	{
-	    // define path
-		$path = BackendModel::getContainer()->getParameter('site.path_www') . '/src/Backend/Cache/Profiles/import_template.csv';
+     * Execute the action.
+     */
+    public function execute()
+    {
+        // define path
+        $path = BACKEND_CACHE_PATH . '/Profiles/import_template.csv';
 
-		// define required fields
-		$fields = array(
-		    'email',
-		    'display_name',
-		    'password'
-		);
+        // define required fields
+        $fields = array(
+            'email',
+            'display_name',
+            'password'
+        );
 
-		// define file
-		$file = new \SpoonFileCSV();
+        // define file
+        $file = new \SpoonFileCSV();
 
-		// download the file
-		$file->arrayToFile($path, array(), $fields, null, ',', '"', true);
-	}
+        // download the file
+        $file->arrayToFile($path, array(), $fields, null, ',', '"', true);
+    }
 }
