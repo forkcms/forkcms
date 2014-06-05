7c6a8a215e6d2ce5a800066499e98b6c83bdadce
woutersioen <wouter.sioen@wijs.be>
2014-06-05 13:23:34 +0200

    Convert indentation to spaces

--- src/Common/Multisite/CurrentSite.php
+++ src/Common/Multisite/CurrentSite.php
@@ -9,132 +9,132 @@ namespace Common\Multisite;
  */
 class CurrentSite
 {
-	/**
-	 * @type int The id of the current site
-	 */
-	private $id;
+    /**
+     * @type int The id of the current site
+     */
+    private $id;
 
-	/**
-	 * @type array The list of active languages for this site (active meaning:
-	 *       selectable in the frontend
-	 */
-	private $activeLanguages;
+    /**
+     * @type array The list of active languages for this site (active meaning:
+     *       selectable in the frontend
+     */
+    private $activeLanguages;
 
-	/**
-	 * @type array The list of working languages for this site
-	 */
-	private $workingLanguages;
+    /**
+     * @type array The list of working languages for this site
+     */
+    private $workingLanguages;
 
-	/**
-	 * @type string The domain of the current site
-	 */
-	private $domain;
+    /**
+     * @type string The domain of the current site
+     */
+    private $domain;
 
-	/**
-	 * @type string The possible prefix for the current site
-	 */
-	private $prefix;
+    /**
+     * @type string The possible prefix for the current site
+     */
+    private $prefix;
 
-	/**
-	 * @type bool Whether or not this is a domain site, ie. that the real domain
-	 *       is used and not our prefixing system.
-	 */
-	private $isDomainSite;
+    /**
+     * @type bool Whether or not this is a domain site, ie. that the real domain
+     *       is used and not our prefixing system.
+     */
+    private $isDomainSite;
 
-	/**
-	 * @type bool Whether or not this is the main site
-	 */
-	private $isMainSite;
+    /**
+     * @type bool Whether or not this is the main site
+     */
+    private $isMainSite;
 
-	/**
-	 * Provisions the current site with all its data
-	 * This unction is only called once from the multisite service
-	 *
-	 * @param array
-	 * @return self
-	 */
-	public function provision(array $currentSiteArray)
-	{
-		$this->id = $currentSiteArray['id'];
-		$this->activeLanguages = $currentSiteArray['active_languages'];
-		$this->workingLanguages = $currentSiteArray['working_languages'];
-		$this->domain = $currentSiteArray['domain'];
-		$this->prefix = $currentSiteArray['prefix'];
-		$this->isDomainSite = $currentSiteArray['is_domain_site'];
-		$this->isMainSite = $currentSiteArray['is_main_site'];
-	}
+    /**
+     * Provisions the current site with all its data
+     * This unction is only called once from the multisite service
+     *
+     * @param array
+     * @return self
+     */
+    public function provision(array $currentSiteArray)
+    {
+        $this->id = $currentSiteArray['id'];
+        $this->activeLanguages = $currentSiteArray['active_languages'];
+        $this->workingLanguages = $currentSiteArray['working_languages'];
+        $this->domain = $currentSiteArray['domain'];
+        $this->prefix = $currentSiteArray['prefix'];
+        $this->isDomainSite = $currentSiteArray['is_domain_site'];
+        $this->isMainSite = $currentSiteArray['is_main_site'];
+    }
 
-	/**
-	 * @return int ID of the current site.
-	 */
-	public function getId()
-	{
-		return $this->id;
-	}
+    /**
+     * @return int ID of the current site.
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	/**
-	 * @return array List of the active languages
-	 */
-	public function getActiveLanguages()
-	{
-		return $this->activeLanguages;
-	}
+    /**
+     * @return array List of the active languages
+     */
+    public function getActiveLanguages()
+    {
+        return $this->activeLanguages;
+    }
 
-	/**
-	 * @return array List of the working languages
-	 */
-	public function getWorkingLanguages()
-	{
-		return $this->workingLanguages;
-	}
+    /**
+     * @return array List of the working languages
+     */
+    public function getWorkingLanguages()
+    {
+        return $this->workingLanguages;
+    }
 
-	/**
-	 * @return string Prefix of the current site
-	 */
-	public function getPrefix()
-	{
-		return $this->prefix;
-	}
+    /**
+     * @return string Prefix of the current site
+     */
+    public function getPrefix()
+    {
+        return $this->prefix;
+    }
 
-	/**
-	 * @return boolean Is this site a domain site, ie. does it _not_ use the
-	 *         prefixing system.
-	 */
-	public function isDomainSite()
-	{
-		return $this->isDomainSite;
-	}
+    /**
+     * @return boolean Is this site a domain site, ie. does it _not_ use the
+     *         prefixing system.
+     */
+    public function isDomainSite()
+    {
+        return $this->isDomainSite;
+    }
 
-	/**
-	 * @return boolean is the current site the main site/HQ?
-	 */
-	public function isMainSite()
-	{
-		return $this->isMainSite;
-	}
+    /**
+     * @return boolean is the current site the main site/HQ?
+     */
+    public function isMainSite()
+    {
+        return $this->isMainSite;
+    }
 
-	/**
-	 * @return string The default language for this site.
-	 */
-	public function getDefaultLanguage()
-	{
-		return !empty($this->activeLanguages)
-			? $this->activeLanguages[0]
-			: null;
-	}
+    /**
+     * @return string The default language for this site.
+     */
+    public function getDefaultLanguage()
+    {
+        return !empty($this->activeLanguages)
+            ? $this->activeLanguages[0]
+            : null;
+    }
 
-	/**
-	 * @return string The current domain without possible prefix.
-	 * @internal Only call this method when we are NOT dealing with a domainsite.
-	 */
-	public function getUnprefixedDomain()
-	{
-		if (!$this->isMainSite && !$this->isDomainSite) {
-			$domainParts = explode('.', SITE_DOMAIN);
-			array_shift($domainParts);
-			return implode('.', $domainParts);
-		} else {
-			return SITE_DOMAIN;
-		}
-	}
+    /**
+     * @return string The current domain without possible prefix.
+     * @internal Only call this method when we are NOT dealing with a domainsite.
+     */
+    public function getUnprefixedDomain()
+    {
+        if (!$this->isMainSite && !$this->isDomainSite) {
+            $domainParts = explode('.', SITE_DOMAIN);
+            array_shift($domainParts);
+            return implode('.', $domainParts);
+        } else {
+            return SITE_DOMAIN;
+        }
+    }
 }
