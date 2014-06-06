e40b426bdd9985e81ed4b467cdd836dab78cbd2a
woutersioen <wouter.sioen@wijs.be>
2014-06-06 13:58:22 +0200

    Install Tags pages for all sites
    We now couple tags pages to a site_id.
    This commit also contains some extract method refactorings
    to improve the readability of the installer

--- src/Backend/Modules/Tags/Installer/Installer.php
+++ src/Backend/Modules/Tags/Installer/Installer.php
@@ -24,64 +24,93 @@ class Installer extends ModuleInstaller
      */
     public function install()
     {
-        // load install.sql
-        $this->importSQL(dirname(__FILE__) . '/Data/install.sql');
-
-        // add 'blog' as a module
+        // add the Tags module
         $this->addModule('Tags');
 
-        // import locale
+        // load database scheme and locale
+        $this->importSQL(dirname(__FILE__) . '/Data/install.sql');
         $this->importLocale(dirname(__FILE__) . '/Data/locale.xml');
 
-        // module rights
+        // rights
         $this->setModuleRights(1, 'Tags');
-
-        // action rights
         $this->setActionRights(1, 'Tags', 'Autocomplete');
         $this->setActionRights(1, 'Tags', 'Edit');
         $this->setActionRights(1, 'Tags', 'Index');
         $this->setActionRights(1, 'Tags', 'MassAction');
 
-        // set navigation
+        // backend navigation
         $navigationModulesId = $this->setNavigation(null, 'Modules');
         $this->setNavigation($navigationModulesId, 'Tags', 'tags/index', array('tags/edit'));
 
+        $extras = $this->insertExtras();
+        foreach ($this->getSites() as $site) {
+            foreach ($this->getLanguages($site['id']) as $language) {
+                // check if a page for tags already exists in this language
+                if (!(bool) $this->getDB()->getVar(
+                    'SELECT 1
+                     FROM pages AS p
+                     INNER JOIN pages_blocks AS b ON b.revision_id = p.revision_id
+                     WHERE b.extra_id = ? AND p.language = ?
+                     LIMIT 1',
+                    array($extras['tags'], $language)
+                )
+                ) {
+                    $this->insertTagsPages($language, $site['id'], $textras);
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Inserts extras
+     *
+     * @return array Key value pairs presenting widget => extras_id
+     */
+    protected function insertExtras()
+    {
+        $extras = array();
+
         // add extra
-        $tagsID = $this->insertExtra('Tags', 'block', 'Tags', null, null, 'N', 30);
+        $extras['tags'] = $this->insertExtra('Tags', 'block', 'Tags', null, null, 'N', 30);
         $this->insertExtra('Tags', 'widget', 'TagCloud', 'TagCloud', null, 'N', 31);
         $this->insertExtra('Tags', 'widget', 'Related', 'Related', null, 'N', 32);
 
         // get search extra id
-        $searchId = (int) $this->getDB()->getVar(
+        $extras['search'] = (int) $this->getDB()->getVar(
             'SELECT id FROM modules_extras WHERE module = ? AND type = ? AND action = ?',
             array('Search', 'widget', 'Form')
         );
 
-        // loop languages
-        foreach ($this->getLanguages() as $language) {
-            // check if a page for tags already exists in this language
-            // @todo refactor this if statement
-            if (!(bool) $this->getDB()->getVar(
-                'SELECT 1
-                 FROM pages AS p
-                 INNER JOIN pages_blocks AS b ON b.revision_id = p.revision_id
-                 WHERE b.extra_id = ? AND p.language = ?
-                 LIMIT 1',
-                array($tagsID, $language)
+        return $extras;
+    }
+
+    /**
+     * Inserts pages for the tags module
+     *
+     * @param string $language
+     * @param int $siteId
+     * @param array $extras
+     */
+    protected function insertTagsPages($language, $siteId, $extras)
+    {
+        // insert contact page
+        $this->insertPage(
+            array(
+                 'title'    => 'Tags',
+                 'type'     => 'root',
+                 'language' => $language,
+                 'site_id'  => $siteId,
+            ),
+            null,
+            array(
+                'extra_id' => $extras['tags'],
+                'position' => 'main',
+            ),
+            array(
+                'extra_id' => $extras['search'],
+                'position' => 'top',
             )
-            ) {
-                // insert contact page
-                $this->insertPage(
-                    array(
-                         'title' => 'Tags',
-                         'type' => 'root',
-                         'language' => $language
-                    ),
-                    null,
-                    array('extra_id' => $tagsID, 'position' => 'main'),
-                    array('extra_id' => $searchId, 'position' => 'top')
-                );
-            }
-        }
+        );
     }
 }
