4f514ea1de3be87ec8c7fc2f6b3dd9f01b09d2b0
Ghazi Triki <ghazi.triki@speakserve.com>
2014-07-01 14:52:21 +0100

    Renamed getName method in ModuleInstaller to getModule

--- src/Backend/Core/Installer/ModuleInstaller.php
+++ src/Backend/Core/Installer/ModuleInstaller.php
@@ -119,7 +119,7 @@ class ModuleInstaller
 
     /**
      * Inserts a new module.
-	 * The getName method becomes available after using addModule and returns $name parameter.
+	 * The getModule method becomes available after using addModule and returns $name parameter.
      *
      * @param string $name The name of the module.
      */
@@ -228,7 +228,7 @@ class ModuleInstaller
      *
      * @return string
      */
-    protected function getName()
+    protected function getModule()
     {
         return $this->name;
     }
