cd72deb1c6ead3afe9cd872c31da2dcc4bd8b99f
Jeroen Desloovere <jeroendesloovere@gmail.com>
2014-07-09 16:02:05 +0200

    Added getCoordinates
    This function can now be used from every other custom module.

--- src/Backend/Modules/Location/Engine/Model.php
+++ src/Backend/Modules/Location/Engine/Model.php
@@ -17,6 +17,7 @@ use Backend\Core\Engine\Model as BackendModel;
  *
  * @author Matthias Mullie <forkcms@mullie.eu>
  * @author Jelmer Snoeck <jelmer@siphoc.com>
+ * @author Jeroen Desloovere <info@jeroendesloovere.be>
  */
 class Model
 {
@@ -96,6 +97,57 @@ class Model
              FROM location AS i
              WHERE i.language = ? AND i.show_overview = ?',
             array(BL::getWorkingLanguage(), 'Y')
+        );
+    }
+
+    /**
+     * Get coordinates latitude/longitude
+     *
+     * @return array  The latitude/longitude coordinates
+     * @param  string $street[optional]
+     * @param  string $streetNumber[optional]
+     * @param  string $city[optional]
+     * @param  string $zip[optional]
+     * @param  string $country[optional]
+     */
+    public static function getCoordinates(
+        $street = null,
+        $streetNumber = null,
+        $city = null,
+        $zip = null,
+        $country = null
+    ) {
+        // init item
+        $item = array();
+
+        // add street
+        if (!empty($street)) $item[] = $street;
+
+        // add street number
+        if (!empty($streetNumber)) $item[] = $streetNumber;
+
+        // add city
+        if (!empty($city)) $item[] = $city;
+
+        // add zip
+        if (!empty($zip)) $item[] = $zip;
+
+        // add country
+        if (!empty($country)) $item[] = \SpoonLocale::getCountry($country, BL::getWorkingLanguage());
+
+        // define address
+        $address = implode(' ', $item);
+
+        // define url
+        $url = 'http://maps.googleapis.com/maps/api/geocode/json?address=' . urlencode($address) . '&sensor=false';
+
+        // define result
+        $geocodes = json_decode(\SpoonHTTP::getContent($url), true);
+
+        // return coordinates latitude/longitude
+        return array(
+            'latitude' => array_key_exists(0, $geocodes['results']) ? $geocodes['results'][0]['geometry']['location']['lat'] : null,
+            'longitude' => array_key_exists(0, $geocodes['results']) ? $geocodes['results'][0]['geometry']['location']['lng'] : null
         );
     }
 
