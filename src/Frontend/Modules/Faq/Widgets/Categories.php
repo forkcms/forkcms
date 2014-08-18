4fe2a97d6b74b28236bb09156c065cb5348a0b2d
Jeroen Desloovere <jeroendesloovere@gmail.com>
2014-08-18 15:42:07 +0200

    Categories widget added

---
+++ src/Frontend/Modules/Faq/Widgets/Categories.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Frontend\Modules\Faq\Widgets;
+
+/*
+ * This file is part of Fork CMS.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
+use Frontend\Core\Engine\Base\Widget as FrontendBaseWidget;
+use Frontend\Core\Engine\Form as FrontendForm;
+use Frontend\Core\Engine\Language as FL;
+use Frontend\Core\Engine\Model as FrontendModel;
+use Frontend\Core\Engine\Navigation as FrontendNavigation;
+use Frontend\Modules\Faq\Engine\Model as FrontendFaqModel;
+
+/**
+ * This is a widget with faq categories
+ *
+ * @author Jeroen Desloovere <jeroen@siesqo.be>
+ */
+class Categories extends FrontendBaseWidget
+{
+	/**
+	 * Execute the extra
+	 */
+	public function execute()
+	{
+		// call parent
+		parent::execute();
+
+		$this->loadTemplate();
+		$this->parse();
+	}
+
+	/**
+	 * Parse
+	 */
+	private function parse()
+	{
+		$this->tpl->assign('widgetFaqCategories', FrontendFaqModel::getCategories());
+	}
+}
