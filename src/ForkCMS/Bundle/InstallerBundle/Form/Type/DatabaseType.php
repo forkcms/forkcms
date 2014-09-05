74eb1e46c0254a9f157440ac45df6db959acc4fa
woutersioen <wouter.sioen@wijs.be>
2014-09-05 08:18:37 +0200

    Calculate default values for the database data.
    Since we're not working with entities, we can't set default data in a
    normal way. Using an event listener on the formBuilder, we can
    manipulate the data right before it gets inserted in the form.
    
    I haven't decided yet if we should use an object containing all
    installation data instead off the parameterBag in the session.

--- src/ForkCMS/Bundle/InstallerBundle/Form/Type/DatabaseType.php
+++ src/ForkCMS/Bundle/InstallerBundle/Form/Type/DatabaseType.php
@@ -4,6 +4,8 @@ namespace ForkCMS\Bundle\InstallerBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 /**
  * Builds the form to set up database information
@@ -21,12 +23,7 @@ class DatabaseType extends AbstractType
             )
             ->add(
                 'port',
-                'text',
-                array(
-                    'attr' => array(
-                        'value' => 3306,
-                    ),
-                )
+                'text'
             )
             ->add(
                 'database',
@@ -41,6 +38,34 @@ class DatabaseType extends AbstractType
                 'text'
             )
         ;
+
+        // make sure the default data is set
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function (FormEvent $event) {
+                $data = $event->getData();
+
+                if (empty($data)) {
+                    $data = array();
+
+                    // guess db & username
+                    $host = $_SERVER['HTTP_HOST'];
+                    $chunks = explode('.', $host);
+
+                    // seems like windows can't handle localhost...
+                    $data['hostname'] = (substr(PHP_OS, 0, 3) == 'WIN') ? '127.0.0.1' : 'localhost';
+
+                    // remove tld
+                    array_pop($chunks);
+
+                    // create base
+                    $data['database'] = $data['username'] = implode('_', $chunks);
+                    $data['port'] = 3306;
+
+                    $event->setData($data);
+                }
+            }
+        );
     }
 
     public function getName()
