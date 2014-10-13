74b811c4e9b53a8185177b333eb9f79663f21e0a
Jeroen Desloovere <jeroendesloovere@gmail.com>
2014-10-13 11:58:05 +0200

    Moved position

--- autoload.php
+++ autoload.php
@@ -1,8 +1,8 @@
 <?php
 
 // use vendor generated autoloader
-require_once __DIR__ . '/app/KernelLoader.php';
 require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/app/KernelLoader.php';
 
 // Spoon is not autoloaded via Composer but uses its own old skool autoloader
 set_include_path(__DIR__ . '/vendor/spoon/library' . PATH_SEPARATOR . get_include_path());
