37f12973482a7d3f57d3ae5bd02facf28bbc0f35
vytenizs <vytenizs@yahoo.com>
2014-10-17 14:42:34 +0300

    Feature/Bug: JS files loading order #893

--- src/Frontend/Core/Engine/Header.php
+++ src/Frontend/Core/Engine/Header.php
@@ -29,6 +29,26 @@ use Frontend\Core\Engine\Base\Object as FrontendBaseObject;
 class Header extends FrontendBaseObject
 {
     /**
+     * Index of priority group for global files
+     */
+    const FILE_PRIORITY_GROUP_GLOBAL = 0;
+
+    /**
+     * Index of priority group for default files
+     */
+    const FILE_PRIORITY_GROUP_DEFAULT = 1;
+
+    /**
+     * Index of priority group for module files
+     */
+    const FILE_PRIORITY_GROUP_MODULE = 2;
+
+    /**
+     * Index of priority group for widget files
+     */
+    const FILE_PRIORITY_GROUP_WIDGET = 3;
+
+    /**
      * The canonical URL
      *
      * @var string
@@ -102,11 +122,11 @@ class Header extends FrontendBaseObject
         }
 
         // add default javascript-files
-        $this->addJS('/src/Frontend/Core/Js/jquery/jquery.js', false);
-        $this->addJS('/src/Frontend/Core/Js/jquery/jquery.ui.js', false);
-        $this->addJS('/src/Frontend/Core/Js/jquery/jquery.frontend.js', true);
-        $this->addJS('/src/Frontend/Core/Js/utils.js', true);
-        $this->addJS('/src/Frontend/Core/Js/frontend.js', false);
+        $this->addJS('/src/Frontend/Core/Js/jquery/jquery.js', false, null, self::FILE_PRIORITY_GROUP_GLOBAL);
+        $this->addJS('/src/Frontend/Core/Js/jquery/jquery.ui.js', false, null, self::FILE_PRIORITY_GROUP_GLOBAL);
+        $this->addJS('/src/Frontend/Core/Js/jquery/jquery.frontend.js', true, null, self::FILE_PRIORITY_GROUP_GLOBAL);
+        $this->addJS('/src/Frontend/Core/Js/utils.js', true, null, self::FILE_PRIORITY_GROUP_GLOBAL);
+        $this->addJS('/src/Frontend/Core/Js/frontend.js', false, null, self::FILE_PRIORITY_GROUP_GLOBAL);
     }
 
     /**
@@ -154,7 +174,7 @@ class Header extends FrontendBaseObject
      * @param bool   $minify       Should the file be minified?
      * @param bool   $addTimestamp May we add a timestamp for caching purposes?
      */
-    public function addJS($file, $minify = true, $addTimestamp = null)
+    public function addJS($file, $minify = true, $addTimestamp = null, $priorityGroup = self::FILE_PRIORITY_GROUP_DEFAULT)
     {
         $file = (string) $file;
         $minify = (bool) $minify;
@@ -174,9 +194,11 @@ class Header extends FrontendBaseObject
             $file = $this->minifyJS($file);
         }
 
+        $jsFile = array('file' => $file, 'add_timestamp' => $addTimestamp, 'priority_group' => $priorityGroup);
+
         // already in array?
-        if (!in_array(array('file' => $file, 'add_timestamp' => $addTimestamp), $this->jsFiles)) {
-            $this->jsFiles[] = array('file' => $file, 'add_timestamp' => $addTimestamp);
+        if (!in_array($jsFile, $this->jsFiles)) {
+            $this->jsFiles[] = $jsFile;
         }
     }
 
@@ -768,6 +790,7 @@ class Header extends FrontendBaseObject
     private function parseJS()
     {
         $jsFiles = array();
+        $jsFilesGrouped = array();
         $existingJSFiles = $this->getJSFiles();
 
         // if there aren't any JS-files added we don't need to do something
@@ -779,6 +802,8 @@ class Header extends FrontendBaseObject
             );
 
             foreach ($existingJSFiles as $file) {
+                $priorityGroup = $file['priority_group'];
+
                 // some files shouldn't be uncacheable
                 if (in_array($file['file'], $ignoreCache) || $file['add_timestamp'] === false) {
                     $file = array('file' => $file['file']);
@@ -791,7 +816,13 @@ class Header extends FrontendBaseObject
                     $file = array('file' => $file['file'] . $modifiedTime);
                 }
 
-                $jsFiles[] = $file;
+                $jsFilesGrouped[$priorityGroup][] = $file;
+            }
+
+            ksort($jsFilesGrouped);
+
+            foreach ($jsFilesGrouped as $jsFile) {
+                $jsFiles = array_merge($jsFiles, $jsFile);
             }
         }
 
