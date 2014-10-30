9a959c81cf9a95beb7683495d199f20752a6cca1
Jonas De Keukelaere <jonas@sumocoders.be>
2014-10-30 16:15:49 +0000

    Adds variable to tell if page has children

--- src/Frontend/Core/Engine/Page.php
+++ src/Frontend/Core/Engine/Page.php
@@ -17,6 +17,7 @@ use Common\Cookie as CommonCookie;
 use Frontend\Core\Engine\Base\Object as FrontendBaseObject;
 use Frontend\Core\Engine\Block\Extra as FrontendBlockExtra;
 use Frontend\Core\Engine\Block\Widget as FrontendBlockWidget;
+use Frontend\Modules\Pages\Engine\Model as PagesModel;
 
 /**
  * Frontend page class, this class will handle everything on a page
@@ -449,6 +450,7 @@ class Page extends FrontendBaseObject
         new Navigation($this->getKernel());
 
         // assign content
+        $this->record['has_children'] = (count(PagesModel::getSubPages($this->record['id'])) > 0);
         $this->tpl->assign('page', $this->record);
 
         // set template path
