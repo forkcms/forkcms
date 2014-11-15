80bf34fa888a4fc6cadeae32a7232331d058b05b
Ghazi Triki <ghazi.triki@speakserve.com>
2014-11-15 21:34:44 +0100

    Added quick acceptation test.

---
+++ tests/acceptance/ForkCMS/Bundle/InstallerBundle/FastInstallCept.php
@@ -0,0 +1,18 @@
+<?php
+$I = new WebGuy($scenario);
+$I->wantTo('Install Fork CMS');
+
+# Step 1
+$I->amOnPage('/');
+$I->see('Install Fork CMS');
+$I->see('Languages');
+$I->click('Next');
+
+# Step 2
+$I->see('Modules');
+$I->uncheckOption('install_modules_example_data');
+$I->click('Next');
+
+# Step 3
+$I->see('Database');
+
