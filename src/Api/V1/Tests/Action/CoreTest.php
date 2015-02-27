78aab0cbffe1ef35a5a1f17d689e0e6b8035e901
Tijs Verkoyen <tijs@verkoyen.eu>
2015-02-28 00:20:14 +0100

    Tests for a positive call and the core.getAPIKey

--- src/Api/V1/Tests/Action/CoreTest.php
+++ src/Api/V1/Tests/Action/CoreTest.php
@@ -35,11 +35,46 @@ class CoreTest extends WebTestCase
         );
     }
 
+    public function testOkOutput()
+    {
+        $client = static::createClient();
+        $this->loadFixtures($client);
+        $request = $this->requestWithGetParameters(
+            $client,
+            '/api',
+            array(
+                'method' => 'core.getAPIKey',
+                'email' => 'noreply@fork-cms.com',
+                'password' => 'fork',
+            )
+        );
+
+        $this->assertEquals(
+            200,
+            $client->getResponse()->getStatusCode()
+        );
+
+        $this->assertEquals(
+            '200',
+            $request->filter('fork')->attr('status_code')
+        );
+        $this->assertEquals(
+            'ok',
+            $request->filter('fork')->attr('status')
+        );
+        $this->assertNotEmpty(
+            $request->filter('fork')->attr('version')
+        );
+        $this->assertNotEmpty(
+            $request->filter('fork')->attr('endpoint')
+        );
+    }
+
     public function testNoMethod()
     {
         $client = static::createClient();
-
         $client->request('GET', '/api');
+
         $this->assertEquals(
             200,
             $client->getResponse()->getStatusCode()
@@ -53,7 +88,6 @@ class CoreTest extends WebTestCase
     public function testInvalidMethod()
     {
         $client = static::createClient();
-
         $this->requestWithGetParameters(
             $client,
             '/api',
@@ -69,6 +103,30 @@ class CoreTest extends WebTestCase
         $this->assertContains(
             'Invalid method.',
             $client->getResponse()->getContent()
+        );
+    }
+
+    public function testCoreGetAPIKey()
+    {
+        $client = static::createClient();
+        $this->loadFixtures($client);
+        $request = $this->requestWithGetParameters(
+            $client,
+            '/api',
+            array(
+                'method' => 'core.getAPIKey',
+                'email' => 'noreply@fork-cms.com',
+                'password' => 'fork',
+            )
+        );
+
+        $this->assertEquals(
+            200,
+            $client->getResponse()->getStatusCode()
+        );
+        $this->assertEquals(
+            '54f0fb1222403',
+            $request->filter('api_key')->text()
         );
     }
 }
