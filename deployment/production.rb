3c4211053771540b0b9ab34f323e5aa03ddc23aa
Wouter Sioen <wouter.sioen@gmail.com>
2015-03-25 13:22:29 +0100

    commit production from the master branch

--- deployment/production.rb
+++ deployment/production.rb
@@ -2,6 +2,7 @@ set :production_server, "#{production_hostname}"
 set :user, "#{production_account}"
 set :app_path, "apps/#{application}"
 set :document_root, "#{production_document_root}"
+set :branch, "master"
 
 # Uncomment the variables below if the DB-credentials are different from
 # the credentials on the staging server
