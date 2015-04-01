e4aecdca0e6749dccea72f6c6e2f3c537e5bb8fa
Jeroen Desloovere <jeroendesloovere@gmail.com>
2015-04-01 16:54:15 +0200

    Tags: database installer updated with new filename
    Also integrated modules_tags in the Tags module installer

--- src/Backend/Modules/Tags/Installer/Data/install.sql
+++ src/Backend/Modules/Tags/Installer/Data/install.sql
@@ -1,8 +1,15 @@
 CREATE TABLE IF NOT EXISTS `tags` (
  `id` int(11) NOT NULL auto_increment,
  `language` varchar(5) NOT NULL,
- `tag` varchar(255) NOT NULL,
- `number` int(11) NOT NULL,
+ `name` varchar(255) NOT NULL,
+ `number_of_connections` int(11) NOT NULL,
  `url` varchar(255) NOT NULL,
  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `modules_tags` (
+ `module` varchar(255) NOT NULL,
+ `tag_id` int(11) NOT NULL,
+ `other_id` int(11) NOT NULL,
+ PRIMARY KEY (`module`,`tag_id`,`other_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
