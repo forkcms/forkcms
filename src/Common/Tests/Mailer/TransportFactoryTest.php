68083d33694d0d9811bd9f87a663bd6d79f4758b
WouterSioen <wouter.sioen@gmail.com>
2015-05-19 20:59:14 +0200

    Add some tests for the Mailer classes.

---
+++ src/Common/Tests/Mailer/TransportFactoryTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Common\Tests\Mailer;
+
+use PHPUnit_Framework_TestCase;
+use Common\Mailer\TransportFactory;
+
+/**
+ * Tests for our module settings
+ *
+ * @author Wouter Sioen <wouter@sumocoders.be>
+ */
+class TransportFactoryTest extends PHPUnit_Framework_TestCase
+{
+    public function testCreatesMailTransportByDefault()
+    {
+        $this->assertInstanceOf(
+            'Swift_MailTransport',
+            TransportFactory::create()
+        );
+    }
+
+    public function testCreatesSmtpTransportIfWanted()
+    {
+        $this->assertInstanceOf(
+            'Swift_SmtpTransport',
+            TransportFactory::create('smtp')
+        );
+    }
+
+    public function testEncryptionCanBeSet()
+    {
+        $transport = TransportFactory::create('smtp', null, null, null, null, 'ssl');
+        $this->assertEquals(
+            'ssl',
+            $transport->getEncryption()
+        );
+
+        $transport = TransportFactory::create('smtp', null, null, null, null, 'tls');
+        $this->assertEquals(
+            'tls',
+            $transport->getEncryption()
+        );
+    }
+}
