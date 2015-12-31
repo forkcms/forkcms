3ed9722fa8ff104b72eef529658b1b262532ef7e
Jonas De Keukelaere <jonas@sumocoders.be>
2015-12-31 10:02:48 +0000

    Add test for encoded redirect URL

---
+++ src/Backend/Modules/Pages/Tests/Model/ModelTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Backend\Modules\Pages\Tests\Model;
+
+use Backend\Modules\Pages\Engine\Model;
+
+class ModelTest extends \PHPUnit_Framework_TestCase
+{
+    public function testUrlIsEncoded()
+    {
+        $this->assertEquals(
+            'http://www.google.be/Quote',
+            Model::getEncodedRedirectURL('http://www.google.be/Quote')
+        );
+        $this->assertEquals(
+            'http://www.google.be/Quote%22HelloWorld%22',
+            Model::getEncodedRedirectURL('http://www.google.be/Quote"HelloWorld"')
+        );
+        $this->assertEquals(
+            'http://www.google.be/Quote%27HelloWorld%27',
+            Model::getEncodedRedirectURL("http://www.google.be/Quote'HelloWorld'")
+        );
+    }
+}
