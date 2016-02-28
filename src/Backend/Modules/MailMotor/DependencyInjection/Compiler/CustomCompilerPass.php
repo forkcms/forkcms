2fa105e40f72ec22e26dfaf40fefffd0aa7c54a8
Jeroen Desloovere <info@jeroendesloovere.be>
2016-02-28 10:22:30 +0100

    Fix for installing Fork CMS, since fork.settings does not exists

--- src/Backend/Modules/MailMotor/DependencyInjection/Compiler/CustomCompilerPass.php
+++ src/Backend/Modules/MailMotor/DependencyInjection/Compiler/CustomCompilerPass.php
@@ -12,18 +12,28 @@ class CustomCompilerPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        // we must set these parameters to be usable
-        $container->setParameter(
-            'mailmotor.mail_engine',
-            $container->get('fork.settings')->get('MailMotor', 'mail_engine')
-        );
-        $container->setParameter(
-            'mailmotor.api_key',
-            $container->get('fork.settings')->get('MailMotor', 'api_key')
-        );
-        $container->setParameter(
-            'mailmotor.list_id',
-            $container->get('fork.settings')->get('MailMotor', 'list_id')
-        );
+        // we have the service fork.settings
+        if ($container->has('fork.settings')) {
+            // we must set these parameters to be usable
+            $container->setParameter(
+                'mailmotor.mail_engine',
+                $container->get('fork.settings')->get('MailMotor', 'mail_engine')
+            );
+            $container->setParameter(
+                'mailmotor.api_key',
+                $container->get('fork.settings')->get('MailMotor', 'api_key')
+            );
+            $container->setParameter(
+                'mailmotor.list_id',
+                $container->get('fork.settings')->get('MailMotor', 'list_id')
+            );
+        // when in fork cms installer, we don't have the service fork.settings
+        // but we must set the parameters
+        } else {
+            // we must set these parameters to be usable
+            $container->setParameter('mailmotor.mail_engine', null);
+            $container->setParameter('mailmotor.api_key', null);
+            $container->setParameter('mailmotor.list_id', null);
+        }
     }
 }
