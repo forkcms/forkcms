ec61bd820913004bd625151616465062ea9e4c06
Jelmer Prins <jelmer@sumocoders.be>
2016-02-29 12:01:13 +0100

    The datepicker didn't show the correct date since the attributes where not prefixed with data The javascript was looking for data attributes.

--- src/Frontend/Core/Engine/Form.php
+++ src/Frontend/Core/Engine/Form.php
@@ -136,9 +136,9 @@ class Form extends \Common\Core\Form
             $mask
         );
         $attributes['data-firstday'] = $firstDay;
-        $attributes['year'] = date('Y', $value);
-        $attributes['month'] = date('n', $value);
-        $attributes['day'] = date('j', $value);
+        $attributes['data-year'] = date('Y', $value);
+        $attributes['data-month'] = date('n', $value);
+        $attributes['data-day'] = date('j', $value);
 
         // add extra classes based on type
         switch ($type) {
