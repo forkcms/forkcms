f8a2ed1860ca8c1c4f59d50445683436255a54dd
Daan De Deckere <daan@sumocoders.be>
2016-03-07 10:39:48 +0100

    Remove sweet-scroll plugin. It can easily be re-added with two lines of code in project that need it, and saves a few kBs in projects that don't need it.

--- src/Frontend/Themes/Bootstrap/src/Js/index.js
+++ src/Frontend/Themes/Bootstrap/src/Js/index.js
@@ -1,8 +1,6 @@
 import Bootstrap from 'bootstrap-sass/assets/javascripts/bootstrap';
 import ImgResponsive from './fork/img-responsive';
-import SweetScroll from 'sweet-scroll';
 
 $(document).ready(function() {
   ImgResponsive();
-  new SweetScroll();
 });
