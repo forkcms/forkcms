7c889d004219aa95089961114ec03fa6b0f5f67f
Daan De Deckere <daan@sumocoders.be>
2016-03-07 10:42:45 +0100

    Don't add img-responsive to all images, instead set max-width to 100% on all images through css, like Foundation does.

--- src/Frontend/Themes/Bootstrap/src/Js/fork/img-responsive.js
+++
@@ -1,3 +0,0 @@
-export default function() {
-  $('img').addClass('img-responsive');
-}
--- src/Frontend/Themes/Bootstrap/src/Js/index.js
+++ src/Frontend/Themes/Bootstrap/src/Js/index.js
@@ -1,6 +1,1 @@
 import Bootstrap from 'bootstrap-sass/assets/javascripts/bootstrap';
-import ImgResponsive from './fork/img-responsive';
-
-$(document).ready(function() {
-  ImgResponsive();
-});
