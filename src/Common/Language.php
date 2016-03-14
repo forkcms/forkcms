3f89603b914d7ea5e99720284a9475a4d9bbb788
Jelmer Prins <jelmer@sumocoders.be>
2016-03-14 10:33:41 +0100

    Adds lbl, msg, err and act to make it easier to work with
    These are shortcuts and only implement the key parameter.
    The other parameters are different for the front- and backend.
    They use the function callLanguageFunction that also can be used to call a function not implemented in Common::Language.

--- src/Common/Language.php
+++ src/Common/Language.php
@@ -2,6 +2,8 @@
 
 namespace Common;
 
+use InvalidArgumentException;
+
 /**
  * This class will make it possible to have 1 function to get the correct language class.
  * This is useful for when you want to use the same code in the Back- and Frontend.
@@ -9,10 +11,88 @@ namespace Common;
  *
  * @author Jelmer Prins <jelmer@$sumocoders.be>
  */
-class Language
+final class Language
 {
     public static function get()
     {
         return APPLICATION . '\Core\Engine\Language';
+    }
+
+    /**
+     * @param $function
+     * @param $parameters
+     *
+     * @throws InvalidArgumentException when the function can't be called
+     *
+     * @return mixed
+     */
+    public static function callLanguageFunction($function, $parameters = [])
+    {
+        $languageClass = self::get();
+        $callback = [$languageClass, $function];
+        if (!method_exists($languageClass, $function) || !is_callable($callback)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'The function %s::%s does not exist',
+                    $languageClass,
+                    $function
+                )
+            );
+        }
+
+        return call_user_func_array($callback, $parameters);
+    }
+
+    /**
+     * Get a label.
+     * This only implements the key because the other parameters differ between the front- and backend.
+     *
+     * @param $key
+     *
+     * @return string
+     */
+    public static function lbl($key)
+    {
+        return self::callLanguageFunction('lbl', [$key]);
+    }
+
+    /**
+     * Get an error.
+     * This only implements the key because the other parameters differ between the front- and backend.
+     *
+     * @param $key
+     *
+     * @return string
+     */
+    public static function err($key)
+    {
+        return self::callLanguageFunction('err', [$key]);
+    }
+
+    /**
+     * Get an action.
+     *
+     * @throws InvalidArgumentException when used in the backend.
+     *
+     * @param $key
+     *
+     * @return string
+     */
+    public static function act($key)
+    {
+        return self::callLanguageFunction('act', [$key]);
+    }
+
+    /**
+     * Get a message.
+     * This only implements the key because the other parameters differ between the front- and backend.
+     *
+     * @param $key
+     *
+     * @return string
+     */
+    public static function msg($key)
+    {
+        return self::callLanguageFunction('msg', [$key]);
     }
 }
