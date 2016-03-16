c2927188d5ccd1d4a565bf5d1947efb8b3ce4c58
Jeroen Desloovere <jeroendesloovere@gmail.com>
2016-03-16 14:42:25 +0100

    Removing unused files
    Since this is now integrated in MailMotorBundle

--- src/Backend/Modules/MailMotor/DependencyInjection/MailMotorExtension.php
+++ src/Backend/Modules/MailMotor/DependencyInjection/MailMotorExtension.php
@@ -19,7 +19,5 @@ class MailMotorExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
     }
 }
