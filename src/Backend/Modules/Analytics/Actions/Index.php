1a8a749919da918da4ac89885e1ea0a9da0cbb3f
Jelmer Prins <jelmer@sumocoders.be>
2016-06-18 01:42:54 +0200

    Move the date range used to filter the analytics into a seperate class

--- src/Backend/Modules/Analytics/Actions/Index.php
+++ src/Backend/Modules/Analytics/Actions/Index.php
@@ -7,48 +7,56 @@ use Backend\Core\Engine\Form;
 use Backend\Core\Engine\Language;
 use Backend\Core\Engine\Model;
 use Backend\Core\Engine\DataGridArray;
+use Backend\Modules\Orders\DateRange\DateRange;
 
 /**
  * This is the index-action (default), it will display the overview of analytics data
  *
  * @author Wouter Sioen <wouter@sumocoders.be>
+ * @author Jelmer Prins <jelmer@sumocoders.be>
  */
 final class Index extends ActionIndex
 {
-    /**
-     * The start and end timestamp of the collected data
-     *
-     * @var int
-     */
-    private $startDate;
-    private $endDate;
-
     /**
      * @var Form
      */
     private $form;
 
+    /**
+     * @var DateRange
+     */
+    private $dateRange;
+
     public function execute()
     {
         parent::execute();
-        $this->setDates();
+
+        /* Set the initial date range */
+        $this->dateRange = new DateRange();
+
         $this->loadForm();
         $this->validateForm();
         $this->parse();
         $this->display();
     }
 
-    private function setDates()
-    {
-        $this->startDate = strtotime('-1 week', mktime(0, 0, 0));
-        $this->endDate = mktime(0, 0, 0);
-    }
-
     private function loadForm()
     {
         $this->form = new Form('dates');
-        $this->form->addDate('start_date', $this->startDate, 'range', mktime(0, 0, 0, 1, 1, 2005), time());
-        $this->form->addDate('end_date', $this->endDate, 'range', mktime(0, 0, 0, 1, 1, 2005), time());
+        $this->form->addDate(
+            'start_date',
+            $this->dateRange->getStartDate(),
+            'range',
+            mktime(0, 0, 0, 1, 1, 2005),
+            time()
+        );
+        $this->form->addDate(
+            'end_date',
+            $this->dateRange->getEndDate(),
+            'range',
+            mktime(0, 0, 0, 1, 1, 2005),
+            time()
+        );
     }
 
     private function validateForm()
@@ -87,8 +95,7 @@ final class Index extends ActionIndex
             }
 
             if ($this->form->isCorrect()) {
-                $this->startDate = $newStartDate;
-                $this->endDate = $newEndDate;
+                $this->dateRange->update($newStartDate, $newEndDate);
             }
         }
     }
@@ -100,8 +107,8 @@ final class Index extends ActionIndex
         $this->header->addJS('highcharts.js', 'Core', false);
 
         $this->form->parse($this->tpl);
-        $this->tpl->assign('startTimestamp', $this->startDate);
-        $this->tpl->assign('endTimestamp', $this->endDate);
+        $this->tpl->assign('startTimestamp', $this->dateRange->getStartDate());
+        $this->tpl->assign('endTimestamp', $this->dateRange->getEndDate());
 
         // if we don't have a token anymore, redirect to the settings page
         if (
@@ -117,38 +124,38 @@ final class Index extends ActionIndex
 
         $this->tpl->assign(
             'page_views',
-            $analytics->getPageViews($this->startDate, $this->endDate)
+            $analytics->getPageViews($this->dateRange->getStartDate(), $this->dateRange->getEndDate())
         );
         $this->tpl->assign(
             'visitors',
-            $analytics->getVisitors($this->startDate, $this->endDate)
+            $analytics->getVisitors($this->dateRange->getStartDate(), $this->dateRange->getEndDate())
         );
         $this->tpl->assign(
             'pages_per_visit',
-            $analytics->getPagesPerVisit($this->startDate, $this->endDate)
+            $analytics->getPagesPerVisit($this->dateRange->getStartDate(), $this->dateRange->getEndDate())
         );
         $this->tpl->assign(
             'time_on_site',
-            $analytics->getTimeOnSite($this->startDate, $this->endDate)
+            $analytics->getTimeOnSite($this->dateRange->getStartDate(), $this->dateRange->getEndDate())
         );
         $this->tpl->assign(
             'new_sessions_percentage',
-            $analytics->getNewSessionsPercentage($this->startDate, $this->endDate)
+            $analytics->getNewSessionsPercentage($this->dateRange->getStartDate(), $this->dateRange->getEndDate())
         );
         $this->tpl->assign(
             'bounce_rate',
-            $analytics->getBounceRate($this->startDate, $this->endDate)
+            $analytics->getBounceRate($this->dateRange->getStartDate(), $this->dateRange->getEndDate())
         );
         $this->tpl->assign(
             'visitors_graph_data',
-            $analytics->getVisitorsGraphData($this->startDate, $this->endDate)
+            $analytics->getVisitorsGraphData($this->dateRange->getStartDate(), $this->dateRange->getEndDate())
         );
         $this->tpl->assign(
             'source_graph_data',
-            $analytics->getSourceGraphData($this->startDate, $this->endDate)
+            $analytics->getSourceGraphData($this->dateRange->getStartDate(), $this->dateRange->getEndDate())
         );
         $dataGrid = new DataGridArray(
-            $analytics->getMostVisitedPagesData($this->startDate, $this->endDate)
+            $analytics->getMostVisitedPagesData($this->dateRange->getStartDate(), $this->dateRange->getEndDate())
         );
         $this->tpl->assign(
             'dataGridMostViewedPages',
