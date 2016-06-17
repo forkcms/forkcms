d54248657ec0b454028729c7fb9666adec53d54e
Jelmer Prins <jelmer@sumocoders.be>
2016-06-18 01:40:26 +0200

    Fix missing class name in phpdoc

--- src/Backend/Core/Engine/Form.php
+++ src/Backend/Core/Engine/Form.php
@@ -305,7 +305,7 @@ class Form extends \Common\Core\Form
     /**
      * Parse the form
      *
-     * @param $tpl The template instance wherein the form will be parsed.
+     * @param $tpl TwigTemplate The template instance wherein the form will be parsed.
      */
     public function parse($tpl)
     {
