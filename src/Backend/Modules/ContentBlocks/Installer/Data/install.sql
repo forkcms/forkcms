7791128c4ca273f5ed209535f628fc50e7ab804f
Mathias Helin <mathias@sumocoders.be>
2016-06-27 14:43:30 +0200

    Use twig template for content block installer

--- src/Backend/Modules/ContentBlocks/Installer/Data/install.sql
+++ src/Backend/Modules/ContentBlocks/Installer/Data/install.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `content_blocks` (
  `revision_id` int(11) NOT NULL auto_increment,
  `user_id` int(11) NOT NULL,
  `extra_id` int(11) NOT NULL,
- `template` varchar(255) NOT NULL default 'default.tpl',
+ `template` varchar(255) NOT NULL default 'Default.html.twig',
  `language` varchar(10) NOT NULL,
  `title` varchar(255) NOT NULL,
  `text` text,
