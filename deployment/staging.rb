7910de68cf6e8f8e91f1fc73ce6fe9ff03d31c8e
Mathias Helin <mathias@sumocoders.be>
2016-06-27 14:28:08 +0200

    Deploy to php 5.6 by default

--- deployment/staging.rb
+++ deployment/staging.rb
@@ -1,1 +1,4 @@
 set :branch, "staging"
+set :staging_server, "dev02.sumocoders.eu"
+set :staging_url, "#{project}.#{client}.php56.sumocoders.eu"
+set :document_root, "/home/#{user}/php56/#{client}/#{project}"
