c836ab72d3efa1f169f7cc4029d95320338a533d
Jelmer Prins <jelmer@sumocoders.be>
2016-07-08 10:46:11 +0200

    Add a service to add the schema's of the entities to the database

---
+++ src/Backend/Doctrine/Entity/CreateSchema.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Backend\Doctrine\Entity;
+
+use Doctrine\DBAL\Exception\TableExistsException;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\SchemaTool;
+
+class CreateSchema
+{
+    /** @var EntityManager */
+    private $entityManager;
+
+    /**
+     * @param EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * Adds a new doctrine entity in the database
+     *
+     * @param string $entityClass
+     */
+    public function forEntityClass($entityClass)
+    {
+        $this->forEntityClasses([$entityClass]);
+    }
+
+    /**
+     * Adds new doctrine entities in the database
+     *
+     * @param array $entityClasses
+     */
+    public function forEntityClasses(array $entityClasses)
+    {
+        // create the database table for the given class using the doctrine SchemaTool
+        $schemaTool = new SchemaTool($this->entityManager);
+
+        try {
+            $schemaTool->createSchema(
+                array_map(
+                    function ($entityClass) {
+                        return $this->entityManager->getClassMetadata($entityClass);
+                    },
+                    $entityClasses
+                )
+            );
+        } catch (TableExistsException $tableExists) {
+            // do nothing because it already exists in the database
+        }
+    }
+}
