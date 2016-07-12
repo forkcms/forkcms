4ecd932c10c4d145ec8ffe079879b2d52e20a66c
Jeroen Desloovere <info@jeroendesloovere.be>
2016-07-13 00:10:39 +0200

    Integrated new MailMotor

---
+++ src/Backend/Modules/MailMotor/Actions/Settings.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace Backend\Modules\MailMotor\Actions;
+
+/*
+ * This file is part of the Fork CMS MailMotor Module from SIESQO.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
+use Backend\Core\Engine\Base\ActionIndex;
+use Backend\Core\Engine\Form;
+use Backend\Core\Engine\Language;
+use Backend\Core\Engine\Model;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * This is the settings-action (default), 
+ * it will be used to couple your "mail-engine" account
+ *
+ * @author Jeroen Desloovere <jeroen@siesqo.be>
+ */
+final class Settings extends ActionIndex
+{
+    /**
+     * The form instance
+     *
+     * @var Form
+     */
+    private $form;
+
+    /**
+     * Execute
+     */
+    public function execute()
+    {
+        parent::execute();
+        $this->loadForm();
+        $this->validateForm();
+        $this->parse();
+        $this->display();
+    }
+
+    /**
+     * Get dropdown values from mail engines
+     *
+     * @return array(key => value)
+     */
+    private function getDropdownValuesForMailEngines()
+    {
+        // init dropdown values
+        $ddmValuesForMailEngines = array();
+
+        // loop all container services to find "mail-engine" gateway services
+        foreach ($this->getContainer()->getServiceIds() as $serviceId) {
+            // the pattern to find mail engines
+            $pattern = '/^mailmotor.(?P<mailengine>\w+).subscriber.gateway/';
+            $matches = array();
+
+            // we found a mail-engine gateway service
+            if (preg_match($pattern, $serviceId, $matches)) {
+                // we skip the fallback gateway
+                if ($matches['mailengine'] == 'not_implemented') {
+                    continue;
+                }
+
+                // add mailengine to dropdown values
+                $ddmValuesForMailEngines[$matches['mailengine']] = ucfirst($matches['mailengine']);
+            }
+        }
+
+        return $ddmValuesForMailEngines;
+    }
+
+    /**
+     * Load form
+     */
+    public function loadForm()
+    {
+        $this->form = new Form('settings');
+
+        // define dropdown values for mail engines
+        $ddmValuesForMailEngines = $this->getDropdownValuesForMailEngines();
+
+        $this->form
+            ->addDropdown(
+                'mail_engine',
+                $ddmValuesForMailEngines,
+                $this->get('fork.settings')->get($this->URL->getModule(), 'mail_engine')
+            )
+            ->setDefaultElement(ucfirst(Language::lbl('None')))
+        ;
+        $this->form->addText(
+            'api_key',
+            $this->get('fork.settings')->get($this->URL->getModule(), 'api_key')
+        );
+        $this->form->addText(
+            'list_id',
+            $this->get('fork.settings')->get($this->URL->getModule(), 'list_id')
+        );
+    }
+
+    /**
+     * Parse
+     */
+    protected function parse()
+    {
+        parent::parse();
+
+        $this->form->parse($this->tpl);
+    }
+
+    /**
+     * Validate form
+     */
+    private function validateForm()
+    {
+        if ($this->form->isSubmitted()) {
+            // define fields
+            $fields = $this->form->getFields();
+
+            // define variables
+            $mailEngine = $fields['mail_engine']->getValue();
+            $apiKey = $fields['api_key']->getValue();
+            $listId = $fields['list_id']->getValue();
+
+            if ($mailEngine !== null) {
+                $fields['api_key']->isFilled(Language::err('FieldIsRequired'));
+                $fields['list_id']->isFilled(Language::err('FieldIsRequired'));
+            }
+
+            if ($this->form->isCorrect()) {
+                // set our settings
+                $this->get('fork.settings')->set($this->URL->getModule(), 'mail_engine', $mailEngine);
+
+                // mail engine is empty
+                if ($mailEngine == '') {
+                    $this->get('fork.settings')->delete($this->URL->getModule(), 'api_key');
+                    $this->get('fork.settings')->delete($this->URL->getModule(), 'list_id');
+                } else {
+                    $this->get('fork.settings')->set($this->URL->getModule(), 'api_key', $apiKey);
+                    $this->get('fork.settings')->set($this->URL->getModule(), 'list_id', $listId);
+                }
+
+                /**
+                 * We must remove our container cache after this request.
+                 * Because this is not only saved in the module settings,
+                 * but the compiler pass pushes this in the container.
+                 * The settings cache is cleared, but the container should be cleared too,
+                 * to make it rebuild with the new chosen engine
+                 */
+                $fs = new Filesystem();
+                $fs->remove($this->getContainer()->getParameter('kernel.cache_dir'));
+
+                // trigger event
+                Model::triggerEvent(
+                    $this->getModule(),
+                    'after_saved_settings'
+                );
+
+                // redirect to the settings page
+                $this->redirect(
+                    Model::createURLForAction('Settings')
+                    . '&report=saved'
+                );
+            }
+        }
+    }
+}
---
+++ src/Frontend/Modules/MailMotor/Actions/Subscribe.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace Frontend\Modules\MailMotor\Actions;
+
+/*
+ * This file is part of the Fork CMS MailMotor Module from SIESQO.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
+use Frontend\Core\Engine\Base\Block as FrontendBaseBlock;
+use Frontend\Core\Engine\Language;
+use Frontend\Core\Engine\Form as FrontendForm;
+use Frontend\Core\Engine\Navigation as FrontendNavigation;
+use Frontend\Modules\MailMotor\Engine\Model as FrontendMailMotorModel;
+use MailMotor\Bundle\MailMotorBundle\Exception\NotImplementedException;
+
+/**
+ * This is the Subscribe-action for our MailMotor
+ *
+ * @author Jeroen Desloovere <jeroen@siesqo.be>
+ */
+class Subscribe extends FrontendBaseBlock
+{
+    /**
+     * FrontendForm instance
+     *
+     * @var	FrontendForm
+     */
+    private $frm;
+
+    /**
+     * Execute the extra
+     *
+     * @return void
+     */
+    public function execute()
+    {
+        parent::execute();
+        $this->loadTemplate();
+        $this->loadForm();
+        $this->validateForm();
+        $this->parse();
+    }
+
+    /**
+     * Load the form
+     *
+     * @return void
+     */
+    private function loadForm()
+    {
+        // create the form
+        $this->frm = new FrontendForm('mailMotorSubscribeForm');
+
+        // define email
+        $email = null;
+
+        // request contains an email
+        if ($this->get('request')->request->get('email') != null) {
+            $email = $this->get('request')->request->get('email');
+        }
+
+        // create & add elements
+        $this->frm->addText('email', $email);
+        $this->frm->addText('email')
+            ->setAttributes(
+                array(
+                    'required' => null,
+                    'type' => 'email',
+                    'placeholder' => ucfirst(Language::lbl('YourEmail')),
+                )
+            )
+        ;
+    }
+
+    /**
+     * Parse the data into the template
+     *
+     * @return void
+     */
+    protected function parse()
+    {
+        // form was sent?
+        if ($this->URL->getParameter('sent') == 'true') {
+            // show message
+            $this->tpl->assign('mailMotorSubscribeIsSuccess', true);
+
+            // hide form
+            $this->tpl->assign('mailMotorSubscribeHideForm', true);
+        }
+
+        // parse the form
+        $this->frm->parse($this->tpl);
+    }
+
+    /**
+     * Validate the form
+     *
+     * @return void
+     */
+    private function validateForm()
+    {
+        // is the form submitted
+        if ($this->frm->isSubmitted()) {
+            // validate required fields
+            $email = $this->frm->getField('email');
+
+            // build
+            $mergeFields = array();
+
+            // validate required fields
+            if ($email->isEmail(Language::err('EmailIsInvalid'))) {
+                try {
+                    if ($this->get('mailmotor.subscriber')->isSubscribed($email->getValue())) {
+                        $email->addError(Language::err('AlreadySubscribed'));
+
+                        // do not remove this line, it is required to make the form show error messages properly
+                        $this->frm->addError(Language::err('AlreadySubscribed'));
+                    }
+                // fallback for when no mail-engine is chosen in the Backend
+                } catch (NotImplementedException $e) {
+                    // do nothing
+                }
+            // we need to add this because the line below
+            // $this->frm->getErrors() only checks if form errors are set, not if an element in the form has errors.
+            } else {
+                $this->frm->addError(Language::err('EmailIsInvalid'));
+            }
+
+            // no errors
+            if (trim($this->frm->getErrors()) == '') {
+                try {
+                    // subscribe the user to our default group
+                    $this->get('mailmotor.subscriber')->subscribe(
+                        $email->getValue(),
+                        $mergeFields,
+                        FRONTEND_LANGUAGE
+                    );
+                // fallback for when no mail-engine is chosen in the Backend
+                } catch (NotImplementedException $e) {
+                    // mail admin instead
+                    FrontendMailMotorModel::mailAdminToSubscribeSubscriber(
+                        $email->getValue(),
+                        FRONTEND_LANGUAGE
+                    );
+                }
+
+                // redirect
+                $this->redirect(
+                    FrontendNavigation::getURLForBlock(
+                        'MailMotor',
+                        'Subscribe'
+                    )
+                    . '?sent=true'
+                    . '#mailMotorSubscribeForm'
+                );
+            // show errors
+            } else {
+                $this->tpl->assign('mailMotorSubscribeHasFormError', true);
+            }
+        }
+    }
+}
