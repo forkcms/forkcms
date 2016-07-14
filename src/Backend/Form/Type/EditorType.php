2b70cc98373208ebfa292e1fe0724697af4c600f
Jelmer Prins <jelmer@sumocoders.be>
2016-07-14 12:20:14 +0200

    Symfony Form Type for editors

---
+++ src/Backend/Form/Type/EditorType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Backend\Form\Type;
+
+use Backend\Core\Engine\Model;
+use Backend\Core\Language\Language;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EditorType extends TextareaType
+{
+    public function configureOptions(OptionsResolver $optionsResolver)
+    {
+        $optionsResolver->setDefaults(['attr' => ['class' => 'inputEditor']]);
+
+        if (!Model::getContainer()->has('header')) {
+            return;
+        }
+        // add the needed javascript to the header;
+        $header = Model::getContainer()->get('header');
+        // we add JS because we need CKEditor
+        $header->addJS('ckeditor/ckeditor.js', 'Core', false);
+        $header->addJS('ckeditor/adapters/jquery.js', 'Core', false);
+        $header->addJS('ckfinder/ckfinder.js', 'Core', false);
+
+        // add the internal link lists-file
+        if (is_file(FRONTEND_CACHE_PATH . '/Navigation/editor_link_list_' . Language::getWorkingLanguage() . '.js')) {
+            $timestamp = @filemtime(
+                FRONTEND_CACHE_PATH . '/Navigation/editor_link_list_' . Language::getWorkingLanguage() . '.js'
+            );
+            $header->addJS(
+                '/src/Frontend/Cache/Navigation/editor_link_list_' . Language::getWorkingLanguage(
+                ) . '.js?m=' . $timestamp,
+                null,
+                false,
+                true,
+                false
+            );
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getParent()
+    {
+        return TextareaType::class;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBlockPrefix()
+    {
+        return 'editor';
+    }
+}
