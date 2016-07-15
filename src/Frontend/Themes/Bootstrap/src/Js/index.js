974c9b9db3008d7b3ab73614d2fcf17bcb52364c
Katrien Vanhaute <vanhautekatrien@gmail.com>
2016-07-15 11:20:02 +0200

    Remove default react import examples

--- src/Frontend/Themes/Bootstrap/src/Js/index.js
+++ src/Frontend/Themes/Bootstrap/src/Js/index.js
@@ -1,11 +1,6 @@
 /* Bootstrap imports */
 import Bootstrap from 'bootstrap-sass/assets/javascripts/bootstrap';
 
-/* React imports */
-/* eg. import React from 'react'; */
-/* eg. import social from './components/Social'; */
-
-
 /* Utilities imports */
 import SweetScroll from "sweet-scroll"
 import {resizeFunction} from './utilities/resize';
