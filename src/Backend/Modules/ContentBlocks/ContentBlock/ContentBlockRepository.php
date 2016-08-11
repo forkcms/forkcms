702a30cd6a67dee3d72cd78fcb942e9416a139eb
Jelmer Prins <jelmer@sumocoders.be>
2016-08-11 16:33:55 +0200

    Fix datagrid query

--- src/Backend/Modules/ContentBlocks/ContentBlock/ContentBlockRepository.php
+++ src/Backend/Modules/ContentBlocks/ContentBlock/ContentBlockRepository.php
@@ -9,34 +9,34 @@ use Doctrine\ORM\Query;
 class ContentBlockRepository extends EntityRepository
 {
     /**
-     * @param Locale $language
+     * @param Locale $locale
      *
      * @return Query
      */
-    public function getDataGridQuery(Locale $language)
+    public function getDataGridQuery(Locale $locale)
     {
         return $this->getEntityManager()
             ->createQueryBuilder()
             ->select('cb.id, cb.title, cb.isHidden')
             ->from(ContentBlock::class, 'cb')
-            ->where('cb.status = ?1 AND cb.language = ?2')
-            ->setParameters([Status::active(), $language])
+            ->where('cb.status = ?1 AND cb.locale = ?2')
+            ->setParameters([Status::active(), $locale])
             ->getQuery();
     }
 
     /**
-     * @param Locale $language
+     * @param Locale $locale
      *
      * @return int
      */
-    public function getNextIdForLanguage(Locale $language)
+    public function getNextIdForLanguage(Locale $locale)
     {
         return (int) $this->getEntityManager()
             ->createQueryBuilder()
             ->select('MAX(cb.id) + 1 as id')
             ->from(ContentBlock::class, 'cb')
-            ->where('cb.language = :language')
-            ->setParameter('language', $language)
+            ->where('cb.locale = :locale')
+            ->setParameter('locale', $locale)
             ->getQuery()
             ->getSingleScalarResult();
     }
