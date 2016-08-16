a76f9bee5b9bd79ff94ce6e34a478f3391e9a747
Jelmer Prins <jelmer@sumocoders.be>
2016-08-16 14:54:17 +0200

    Split up the services.yml config file

--- src/Backend/Modules/ContentBlocks/DependencyInjection/ContentBlocksExtension.php
+++ src/Backend/Modules/ContentBlocks/DependencyInjection/ContentBlocksExtension.php
@@ -21,7 +21,8 @@ class ContentBlocksExtension extends Extension implements PrependExtensionInterf
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
+        $loader->load('command.yml');
+        $loader->load('repository.yml');
     }
 
     /**
