d4749931ec5e7be3e5b62f9ec62e38ff47d94417
Jelmer Prins <jelmer@sumocoders.be>
2016-08-24 16:16:40 +0200

    Implement setting if a template has the banner option in the xml

--- src/Backend/Modules/Extensions/Engine/Model.php
+++ src/Backend/Modules/Extensions/Engine/Model.php
@@ -813,6 +813,7 @@ class Model
             $item['path'] = $template['path'];
             $item['active'] = 'Y';
             $item['data']['format'] = $template['format'];
+            $item['data']['banner'] = $template['banner'];
 
             // build positions
             $item['data']['names'] = array();
@@ -1032,6 +1033,8 @@ class Model
             // template data
             $template['label'] = (string) $templateXML['label'];
             $template['path'] = (string) $templateXML['path'];
+            $template['banner'] = isset($templateXML['banner'])
+                ? (string) $templateXML['banner'] && (string) $templateXML['banner'] !== 'false' : false;
             $template['format'] = trim(str_replace(array("\n", "\r", ' '), '', (string) $templateXML->format));
 
             // loop positions
