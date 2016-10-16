a44a1694ed36c7658c8a6952a2cf6b3246fe9699
Jelmer Prins <jelmer@sumocoders.be>
2016-10-17 00:30:48 +0200

    Add abstract form types for the File and Image value objects

---
+++ src/Common/Doctrine/Type/AbstractImageType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Common\Doctrine\Type;
+
+use Common\Doctrine\ValueObject\AbstractImage;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+
+abstract class AbstractImageType extends Type
+{
+    /**
+     * @param array $fieldDeclaration
+     * @param AbstractPlatform $platform
+     *
+     * @return string
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return 'VARCHAR(255)';
+    }
+
+    /**
+     * @param string $imageFileName
+     * @param AbstractPlatform $platform
+     *
+     * @return AbstractImage
+     */
+    public function convertToPHPValue($imageFileName, AbstractPlatform $platform)
+    {
+        return $this->createFromString($imageFileName);
+    }
+
+    /**
+     * @param AbstractImage $image
+     * @param AbstractPlatform $platform
+     *
+     * @return string
+     */
+    public function convertToDatabaseValue($image, AbstractPlatform $platform)
+    {
+        return (string) $image;
+    }
+
+    /**
+     * @param string $imageFileName
+     *
+     * @return AbstractImage
+     */
+    abstract protected function createFromString($imageFileName);
+}
