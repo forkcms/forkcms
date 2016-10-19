4db88c8313d4aca79bf7d34199862969972c384f
Jelmer Prins <jelmer@sumocoders.be>
2016-10-19 17:30:56 +0200

    Use return instead of exit in the index.php
    The result is the same, the code execution stops but it makes it testable

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (!is_dir(__DIR__ . '/vendor')) {
          installed you should run: <code>composer install</code>. If you
          don\'t have composer installed you really should, see
          http://getcomposer.org for more information';
-    exit;
+    return;
 }
 
 require_once __DIR__ . '/autoload.php';
@@ -41,11 +41,11 @@ if (!file_exists($parametersFile)) {
                  <a href="http://www.fork-cms.com/community/documentation/detail/installation/webservers">webservers</a>
                  for further information. <a href="?skiphtaccess">Skip .htaccess
                  check</a>';
-            exit;
+            return;
         }
 
         header('Location: /install');
-        exit;
+        return;
     }
 }
 
