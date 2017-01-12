d6849d6dbf59f04e4faf90243122f662a2a43fd0
Jelmer Prins <jelmer@sumocoders.be>
2017-01-12 17:28:27 +0100

    Add collection with working add and remove implementation for bootstrap

--- src/Backend/Core/Js/jquery/jquery.backend.js
+++ src/Backend/Core/Js/jquery/jquery.backend.js
@@ -1604,3 +1604,105 @@
         });
     };
 })(jQuery);
+
+/**
+ * Collection class
+ */
+(function($) {
+    var addField = '[data-addfield="collection"]',
+        removeField = '[data-removefield="collection"]',
+        CollectionAdd = function(el) {
+            $(el).on('click', addField, this.addField);
+        },
+        CollectionRemove = function(el) {
+            $(el).on('click', removeField, this.removeField);
+        };
+
+    CollectionAdd.prototype.addField = function(e) {
+        var $this = $(this),
+            selector = $this.attr('data-collection'),
+            prototypeName = $this.attr('data-prototype-name');
+
+        e && e.preventDefault();
+
+        var collection = $('#' + selector),
+            list = collection.find('ul.js-collection'),
+            count = list.find('> li').length;
+
+        var newWidget = collection.attr('data-prototype');
+
+        // Check if an element with this ID already exists.
+        // If it does, increase the count by one and try again
+        var newName = newWidget.match(/id="(.*?)"/);
+        var re = new RegExp(prototypeName, "g");
+        while ($('#' + newName[1].replace(re, count)).length > 0) {
+            count++;
+        }
+        newWidget = newWidget.replace(re, count);
+        newWidget = newWidget.replace(/__id__/g, newName[1].replace(re, count));
+        var newLi = $('<li class="list-group-item"></li>').html(newWidget);
+        newLi.appendTo(list);
+        $this.trigger('collection-field-added');
+    };
+
+    CollectionRemove.prototype.removeField = function(e) {
+        var $this = $(this),
+            parent = $this.closest('li').parent();
+
+        e && e.preventDefault();
+
+        $this.trigger('collection-field-removed');
+        $this.trigger('collection-field-removed-before');
+        $this.closest('li').remove();
+        parent.trigger('collection-field-removed-after');
+    };
+
+    // Extra jquery functions
+    // first get the old ones in case there is a conflict
+    var oldAdd = $.fn.addField;
+    var oldRemove = $.fn.removeField;
+
+    $.fn.addField = function(option) {
+        return this.each(function() {
+            var $this = $(this),
+                data = $this.data('addfield')
+                ;
+            if (!data) {
+                $this.data('addfield', (data = new CollectionAdd(this)));
+            }
+            if (typeof option == 'string') {
+                data[option].call($this);
+            }
+        });
+    };
+
+    $.fn.removeField = function(option) {
+        return this.each(function() {
+            var $this = $(this),
+                data = $this.data('removefield')
+                ;
+            if (!data) {
+                $this.data('removefield', (data = new CollectionRemove(this)));
+            }
+            if (typeof option == 'string') {
+                data[option].call($this);
+            }
+        });
+    };
+
+    $.fn.addField.Constructor = CollectionAdd;
+    $.fn.removeField.Constructor = CollectionRemove;
+
+    $.fn.addField.noConflict = function() {
+        $.fn.addField = oldAdd;
+        return this;
+    };
+    $.fn.removeField.noConflict = function() {
+        $.fn.removeField = oldRemove;
+        return this;
+    };
+
+    $(document).on('click.addfield.data-api', addField, CollectionAdd.prototype.addField);
+    $(document).on('click.removefield.data-api', removeField, CollectionRemove.prototype.removeField);
+
+})(jQuery);
