65fa6d1cc89f0d84274a50dbd57666bae16f7a61
Jelmer Prins <jelmer@sumocoders.be>
2017-02-08 12:35:32 +0100

    Fix tests not finding the kernel

--- src/Common/WebTestCase.php
+++ src/Common/WebTestCase.php
@@ -2,6 +2,7 @@
 
 namespace Common;
 
+use ForkCMS\App\AppKernel;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTestCase;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\FileSystem\Filesystem;
@@ -28,23 +29,7 @@ abstract class WebTestCase extends BaseWebTestCase
      */
     protected static function getKernelClass()
     {
-        $dir = isset($_SERVER['KERNEL_DIR']) ? $_SERVER['KERNEL_DIR'] : static::getPhpUnitXmlDir();
-
-        $finder = new Finder();
-        $finder->name('AppKernel.php')->depth(0)->in($dir);
-        $results = iterator_to_array($finder);
-        if (!count($results)) {
-            throw new \RuntimeException(
-                'Either set KERNEL_DIR in your phpunit.xml according to http://symfony.com/doc/current/book/testing.html#your-first-functional-test or override the WebTestCase::createKernel() method.'
-            );
-        }
-
-        $file = current($results);
-        $class = $file->getBasename('.php');
-
-        require_once $file;
-
-        return $class;
+        return AppKernel::class;
     }
 
     /**
