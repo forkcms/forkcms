22ec8396aeaa3cf7968bb99404c8b416dff1eaee
Jeroen Desloovere <info@jeroendesloovere.be>
2017-02-22 17:52:57 +0100

    tests updated

--- src/Backend/Modules/MediaLibrary/Tests/Component/ImageSettingsTest.php
+++ src/Backend/Modules/MediaLibrary/Tests/Component/ImageSettingsTest.php
@@ -16,39 +16,18 @@ class ImageSettingsTest extends PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->correctFolderNames = [
-            '800x600-crop-100',
-            '800x600-resize-100',
-            '800x600-100',
-            '800x600-crop-20',
-            '800x600-resize-20',
-            '800x600-20',
-            '800x-crop-100',
-            '800x-resize-100',
-            '800x-100',
-            '800x-crop-20',
-            '800x-resize-20',
-            '800x-20',
-            'x800-crop-100',
-            'x800-resize-100',
-            'x800-100',
-            'x800-crop-20',
-            'x800-resize-20',
-            'x800-20',
-            'x800-crop-20',
-            'x800-resize-20',
-        ];
-
-        $this->falseFolderNames = [
-            '800x600-crop',
-            '800x600-resize',
             '800x600',
-            '800x-crop',
-            '800x-resize',
+            '800x600-20',
+            '800x600-crop',
+            '800x600-crop-20',
             '800x',
-            'x800-crop',
-            'x800-resize',
+            '800x-20',
+            '800x-crop',
+            '800x-crop-20',
             'x800',
-            'abcde',
+            'x800-20',
+            'x800-crop',
+            'x800-crop-20',
         ];
     }
 
@@ -64,21 +43,6 @@ class ImageSettingsTest extends PHPUnit_Framework_TestCase
             );
 
             $this->assertEquals($folderName, $imageSettings->toString());
-        }
-    }
-
-    /**
-     * @expectedException \Exception
-     */
-    public function testFalseFolderNamesFromString()
-    {
-        foreach ($this->falseFolderNames as $folderName) {
-            /** @var ImageSettings $imageSettings */
-            $imageSettings = ImageSettings::fromString(
-                $folderName
-            );
-
-            $this->assertNotEquals($folderName, $imageSettings->toString());
         }
     }
 }
