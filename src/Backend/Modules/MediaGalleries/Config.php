b2ebdf0f423a74cd82ff80478f420c460192962f
Jeroen Desloovere <info@jeroendesloovere.be>
2017-02-28 10:21:16 +0100

    Using default Config variables

--- src/Backend/Modules/MediaGalleries/Config.php
+++ src/Backend/Modules/MediaGalleries/Config.php
@@ -5,21 +5,8 @@ namespace Backend\Modules\MediaGalleries;
 use Backend\Core\Engine\Base\Config as BackendBaseConfig;
 
 /**
- * This is the configuration-object for the media module
+ * This is the configuration-object for the media galleries module
  */
 class Config extends BackendBaseConfig
 {
-    /**
-     * The default action
-     *
-     * @var string
-     */
-    protected $defaultAction = 'Index';
-
-    /**
-     * The disabled actions
-     *
-     * @var array
-     */
-    protected $disabledActions = array();
 }
--- src/Backend/Modules/MediaLibrary/Config.php
+++ src/Backend/Modules/MediaLibrary/Config.php
@@ -5,21 +5,8 @@ namespace Backend\Modules\MediaLibrary;
 use Backend\Core\Engine\Base\Config as BackendBaseConfig;
 
 /**
- * This is the configuration-object for the media module
+ * This is the configuration-object for the media library module
  */
 class Config extends BackendBaseConfig
 {
-    /**
-     * The default action
-     *
-     * @var string
-     */
-    protected $defaultAction = 'Index';
-
-    /**
-     * The disabled actions
-     *
-     * @var array
-     */
-    protected $disabledActions = array();
 }
