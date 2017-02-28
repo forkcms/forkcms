0a428e436d35085e6dbe97fdadf903154789256f
Jeroen Desloovere <info@jeroendesloovere.be>
2017-02-28 15:02:49 +0100

    Updating the switch

--- src/Backend/Modules/MediaLibrary/Ajax/UploadMediaItem.php
+++ src/Backend/Modules/MediaLibrary/Ajax/UploadMediaItem.php
@@ -285,15 +285,20 @@ class UploadMediaItem extends BackendBaseAJAXAction
     {
         $val = trim($val);
         $last = strtolower($val[strlen($val)-1]);
+
+        $amplifier = 1;
         switch ($last) {
-            // The 'G' modifier is available since PHP 5.1.0
             case 'g':
-                $val *= 1024;
+                $amplifier = 1024 * 1024 * 1024;
+                break;
             case 'm':
-                $val *= 1024;
+                $amplifier = 1024 * 1024;
+                break;
             case 'k':
-                $val *= 1024;
+                $amplifier = 1024;
+                break;
         }
-        return $val;
+
+        return $val * $amplifier;
     }
 }
