0c321912d98c7a95d66bfe823b9450f917e21ccb
stijnvrolijk <stijn@sumocoders.be>
2017-03-02 12:00:00 +0100

    Create a service for the forkcms:cache:clear command

---
+++ src/Backend/Modules/Settings/DependencyInjection/SettingsExtension.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Backend\Modules\Settings\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+class SettingsExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
