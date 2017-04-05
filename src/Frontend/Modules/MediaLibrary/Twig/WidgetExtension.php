f4e65a495b6e2c2b6f0b8f4d1d40350101fa9b43
Jeroen Desloovere <info@jeroendesloovere.be>
2017-04-05 15:48:05 +0200

    PHPDoc updated

--- src/Frontend/Modules/MediaLibrary/Twig/WidgetExtension.php
+++ src/Frontend/Modules/MediaLibrary/Twig/WidgetExtension.php
@@ -35,8 +35,7 @@ class WidgetExtension extends \Twig_Extension
      * @param string $mediaWidgetAction - The ClassName from the Media widget you want to use.
      * @param string $mediaGroupId - The MediaGroup id you want to parse
      * @param string $title - You can give your optional custom title.
-     * @param string $module - You can parse a widget from a custom module. Default is the "Media" module.
-     *
+     * @param string $module - You can parse a widget from a custom module. Default is the "MediaLibrary" module.
      * @return \Twig_Markup
      */
     public function parseWidget(
