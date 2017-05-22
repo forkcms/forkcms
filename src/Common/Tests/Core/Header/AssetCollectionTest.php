e175dc4b22b423eee612aa84177ede210cbd0f92
Jeroen Desloovere <info@jeroendesloovere.be>
2017-05-22 14:27:34 +0200

    AssetCollectionTest tests expanded

--- src/Common/Tests/Core/Header/AssetCollectionTest.php
+++ src/Common/Tests/Core/Header/AssetCollectionTest.php
@@ -5,6 +5,7 @@ namespace Common\Tests\Core\Header;
 use Common\Core\Header\Asset;
 use Common\Core\Header\AssetCollection;
 use Common\Core\Header\Minifier;
+use Common\Core\Header\Priority;
 
 class AssetCollectionTest extends \PHPUnit_Framework_TestCase
 {
@@ -12,9 +13,22 @@ class AssetCollectionTest extends \PHPUnit_Framework_TestCase
     {
         $assetCollection = new AssetCollection(Minifier::js(__DIR__, __DIR__, __DIR__));
 
-        $asset = new Asset(__DIR__ . '/../../../../../js/vendors/jquery.min.js');
+        $asset = new Asset(__DIR__ . '/../../../../../js/vendors/bootstrap.min.js', false, Priority::core());
+        $asset2 = new Asset(__DIR__ . '/../../../../../js/vendors/jquery-ui.min.js', false, Priority::standard());
+        $asset3 = new Asset(__DIR__ . '/../../../../../js/vendors/jquery.min.js', false, Priority::core());
 
         $assetCollection->add($asset, false);
-        $this->assertContains($asset, $assetCollection->getAssets());
+        $assetCollection->add($asset2, false);
+        $assetCollection->add($asset3, false);
+
+        $unorderedAssets = $assetCollection->getAssets();
+        $this->assertEquals($asset, $unorderedAssets[0]);
+        $this->assertEquals($asset2, $unorderedAssets[1]);
+        $this->assertEquals($asset3, $unorderedAssets[2]);
+
+        $orderedAssets = $assetCollection->getAssets(true);
+        $this->assertEquals($asset, $orderedAssets[0]);
+        $this->assertEquals($asset2, $orderedAssets[2]);
+        $this->assertEquals($asset3, $orderedAssets[1]);
     }
 }
