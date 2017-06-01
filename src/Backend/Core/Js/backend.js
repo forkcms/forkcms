d92c9660f2162041658283130c89902eafcc4dfa
Jelmer Prins <jelmer@sumocoders.be>
2017-06-01 14:04:03 +0200

    Fix textareas in ckeditor popups not working inside bootstrap modals

--- src/Backend/Core/Js/backend.js
+++ src/Backend/Core/Js/backend.js
@@ -548,7 +548,11 @@ jsBackend.ckeditor =
             var modal_this;
             modal_this = this;
             $(document).on('focusin.modal', function(e) {
-                if (modal_this.$element[0] !== e.target && !modal_this.$element.has(e.target).length && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_select') && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_text')) {
+                if (modal_this.$element[0] !== e.target
+                    && !modal_this.$element.has(e.target).length
+                    && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_select')
+                    && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_text')
+                    && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_textarea')) {
                     modal_this.$element.focus();
                 }
             });
