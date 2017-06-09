e709b29f30e5042b8b653fde703454e70f5f6db1
Jeroen Desloovere <info@jeroendesloovere.be>
2017-06-09 17:44:52 +0200

    Updated "Location" installer

--- src/Backend/Modules/Location/Installer/Installer.php
+++ src/Backend/Modules/Location/Installer/Installer.php
@@ -19,47 +19,41 @@ class Installer extends ModuleInstaller
 {
     public function install(): void
     {
-        // load install.sql
-        $this->importSQL(__DIR__ . '/Data/install.sql');
-
-        // add 'location' as a module
         $this->addModule('Location');
-
-        // import locale
+        $this->importSQL(__DIR__ . '/Data/install.sql');
         $this->importLocale(__DIR__ . '/Data/locale.xml');
+        $this->configureSettings();
+        $this->configureBackendNavigation();
+        $this->configureBackendRights();
+        $this->configureFrontendExtras();
+    }
 
-        // general settings
-        $this->setSetting($this->getModule(), 'zoom_level', 'auto');
-        $this->setSetting($this->getModule(), 'width', 400);
-        $this->setSetting($this->getModule(), 'height', 300);
-        $this->setSetting($this->getModule(), 'map_type', 'ROADMAP');
-        $this->setSetting($this->getModule(), 'zoom_level_widget', 13);
-        $this->setSetting($this->getModule(), 'width_widget', 400);
-        $this->setSetting($this->getModule(), 'height_widget', 300);
-        $this->setSetting($this->getModule(), 'map_type_widget', 'ROADMAP');
-        $this->setSetting($this->getModule(), 'requires_google_maps', true);
-
-        // module rights
-        $this->setModuleRights(1, $this->getModule());
-
-        // action rights
-        $this->setActionRights(1, $this->getModule(), 'Index');
-        $this->setActionRights(1, $this->getModule(), 'Add');
-        $this->setActionRights(1, $this->getModule(), 'Edit');
-        $this->setActionRights(1, $this->getModule(), 'Delete');
-        $this->setActionRights(1, $this->getModule(), 'SaveLiveLocation');
-        $this->setActionRights(1, $this->getModule(), 'UpdateMarker');
-
-        // set navigation
+    private function configureBackendNavigation(): void
+    {
+        // Set navigation for "modules"
         $navigationModulesId = $this->setNavigation(null, 'Modules');
         $this->setNavigation($navigationModulesId, 'Location', 'location/index', ['location/add', 'location/edit']);
 
-        // settings navigation
+        // Set navigation for "settings"
         $navigationSettingsId = $this->setNavigation(null, 'Settings');
         $navigationModulesId = $this->setNavigation($navigationSettingsId, 'Modules');
         $this->setNavigation($navigationModulesId, 'Location', 'location/settings');
+    }
 
-        // add extra's
+    private function configureBackendRights(): void
+    {
+        $this->setModuleRights(1, $this->getModule());
+
+        $this->setActionRights(1, $this->getModule(), 'Add');
+        $this->setActionRights(1, $this->getModule(), 'Delete');
+        $this->setActionRights(1, $this->getModule(), 'Edit');
+        $this->setActionRights(1, $this->getModule(), 'Index');
+        $this->setActionRights(1, $this->getModule(), 'SaveLiveLocation');
+        $this->setActionRights(1, $this->getModule(), 'UpdateMarker');
+    }
+
+    private function configureFrontendExtras(): void
+    {
         $this->insertExtra(
             $this->getModule(),
             ModuleExtraType::block(),
@@ -68,5 +62,18 @@ class Installer extends ModuleInstaller
             ['url' => '/private/location/index?token=true'],
             false
         );
+    }
+
+    private function configureSettings(): void
+    {
+        $this->setSetting($this->getModule(), 'height', 300);
+        $this->setSetting($this->getModule(), 'height_widget', 300);
+        $this->setSetting($this->getModule(), 'map_type', 'ROADMAP');
+        $this->setSetting($this->getModule(), 'map_type_widget', 'ROADMAP');
+        $this->setSetting($this->getModule(), 'requires_google_maps', true);
+        $this->setSetting($this->getModule(), 'width', 400);
+        $this->setSetting($this->getModule(), 'width_widget', 400);
+        $this->setSetting($this->getModule(), 'zoom_level', 'auto');
+        $this->setSetting($this->getModule(), 'zoom_level_widget', 13);
     }
 }
