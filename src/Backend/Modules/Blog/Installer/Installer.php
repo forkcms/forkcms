b996269fc94c37d3045c39625207008020c22279
Jeroen Desloovere <info@jeroendesloovere.be>
2017-06-09 20:45:58 +0200

    Capital letter for "Modules" and "Settings"

--- src/Backend/Modules/Blog/Installer/Installer.php
+++ src/Backend/Modules/Blog/Installer/Installer.php
@@ -64,7 +64,7 @@ class Installer extends ModuleInstaller
 
     private function configureBackendNavigation(): void
     {
-        // Set navigation for "modules"
+        // Set navigation for "Modules"
         $navigationModulesId = $this->setNavigation(null, 'Modules');
         $navigationBlogId = $this->setNavigation($navigationModulesId, 'Blog');
         $this->setNavigation(
@@ -81,7 +81,7 @@ class Installer extends ModuleInstaller
             ['blog/add_category', 'blog/edit_category']
         );
 
-        // Set navigation for "settings"
+        // Set navigation for "Settings"
         $navigationSettingsId = $this->setNavigation(null, 'Settings');
         $navigationModulesId = $this->setNavigation($navigationSettingsId, 'Modules');
         $this->setNavigation($navigationModulesId, 'Blog', 'blog/settings');
