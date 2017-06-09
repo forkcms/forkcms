dece677c209114650bb9ffef2fe2f2da0a5f2549
Jeroen Desloovere <info@jeroendesloovere.be>
2017-06-09 22:09:23 +0200

    Capital letter for "Modules"

--- src/Backend/Modules/Tags/Installer/Installer.php
+++ src/Backend/Modules/Tags/Installer/Installer.php
@@ -33,7 +33,7 @@ class Installer extends ModuleInstaller
 
     private function configureBackendNavigation(): void
     {
-        // Set navigation for "modules"
+        // Set navigation for "Modules"
         $navigationModulesId = $this->setNavigation(null, 'Modules');
         $this->setNavigation($navigationModulesId, $this->getModule(), 'tags/index', ['tags/edit']);
     }
