ba8644650dfa29ac8f1192b5fd0a2baa1888355e
Jeroen Desloovere <info@jeroendesloovere.be>
2017-06-09 14:54:36 +0200

    Updated "Analytics" installer

--- src/Backend/Modules/Analytics/Installer/Installer.php
+++ src/Backend/Modules/Analytics/Installer/Installer.php
@@ -12,37 +12,36 @@ class Installer extends ModuleInstaller
     public function install(): void
     {
         $this->addModule('Analytics');
-
-        // import locale
         $this->importLocale(__DIR__ . '/Data/locale.xml');
+        $this->configureRights();
+        $this->configureBackendNavigation();
+        $this->configureBackendWidgets();
+    }
 
-        // add the needed rights
-        $this->setModuleRights(1, $this->getModule());
-        $this->setActionRights(1, $this->getModule(), 'Index');
-        $this->setActionRights(1, $this->getModule(), 'Settings');
-        $this->setActionRights(1, $this->getModule(), 'Reset');
-
-        // module navigation
+    private function configureBackendNavigation(): void
+    {
+        // Navigation for "Modules"
         $navigationMarketingId = $this->setNavigation(null, 'Marketing', 'analytics/index', null, 4);
         $this->setNavigation($navigationMarketingId, 'Analytics', 'analytics/index');
 
-        // settings navigation
+        // Navigation for "Settings"
         $navigationSettingsId = $this->setNavigation(null, 'Settings');
         $navigationModulesId = $this->setNavigation($navigationSettingsId, 'Modules');
         $this->setNavigation($navigationModulesId, $this->getModule(), 'analytics/settings');
-
-        $this->insertWidgets();
     }
 
-    private function insertWidgets(): void
+    private function configureBackendWidgets(): void
     {
-        $this->insertDashboardWidget(
-            'Analytics',
-            'RecentVisits'
-        );
-        $this->insertDashboardWidget(
-            'Analytics',
-            'TraficSources'
-        );
+        $this->insertDashboardWidget('Analytics', 'RecentVisits');
+        $this->insertDashboardWidget('Analytics', 'TraficSources');
+    }
+
+    private function configureRights(): void
+    {
+        $this->setModuleRights(1, $this->getModule());
+
+        $this->setActionRights(1, $this->getModule(), 'Index');
+        $this->setActionRights(1, $this->getModule(), 'Settings');
+        $this->setActionRights(1, $this->getModule(), 'Reset');
     }
 }
