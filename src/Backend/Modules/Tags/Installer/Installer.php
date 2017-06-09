d906a08d41796a3bf826a93dc09be5fb676fb29b
Jeroen Desloovere <info@jeroendesloovere.be>
2017-06-09 20:03:25 +0200

    Updated "Tags" installer

--- src/Backend/Modules/Tags/Installer/Installer.php
+++ src/Backend/Modules/Tags/Installer/Installer.php
@@ -17,54 +17,51 @@ use Common\ModuleExtraType;
  */
 class Installer extends ModuleInstaller
 {
+    /** @var int */
+    private $tagsExtraId;
+
     public function install(): void
     {
-        // load install.sql
-        $this->importSQL(__DIR__ . '/Data/install.sql');
-
-        // add 'blog' as a module
         $this->addModule('Tags');
-
-        // import locale
+        $this->importSQL(__DIR__ . '/Data/install.sql');
         $this->importLocale(__DIR__ . '/Data/locale.xml');
+        $this->configureBackendNavigation();
+        $this->configureBackendRights();
+        $this->configureFrontendExtras();
+        $this->configureFrontendPages();
+    }
 
-        // module rights
+    private function configureBackendNavigation(): void
+    {
+        // Set navigation for "modules"
+        $navigationModulesId = $this->setNavigation(null, 'Modules');
+        $this->setNavigation($navigationModulesId, $this->getModule(), 'tags/index', ['tags/edit']);
+    }
+
+    private function configureBackendRights(): void
+    {
         $this->setModuleRights(1, $this->getModule());
 
-        // action rights
         $this->setActionRights(1, $this->getModule(), 'Autocomplete');
         $this->setActionRights(1, $this->getModule(), 'Edit');
         $this->setActionRights(1, $this->getModule(), 'Index');
         $this->setActionRights(1, $this->getModule(), 'MassAction');
+    }
 
-        // set navigation
-        $navigationModulesId = $this->setNavigation(null, 'Modules');
-        $this->setNavigation($navigationModulesId, 'Tags', 'tags/index', ['tags/edit']);
-
-        // add extra
-        $tagsID = $this->insertExtra($this->getModule(), ModuleExtraType::block(), 'Tags', null, null, false, 30);
+    private function configureFrontendExtras(): void
+    {
+        $this->tagsExtraId = $this->insertExtra($this->getModule(), ModuleExtraType::block(), 'Tags', null, null, false, 30);
         $this->insertExtra($this->getModule(), ModuleExtraType::widget(), 'TagCloud', 'TagCloud', null, false, 31);
         $this->insertExtra($this->getModule(), ModuleExtraType::widget(), 'Related', 'Related', null, false, 32);
+    }
 
-        // get search extra id
-        $searchId = (int) $this->getDB()->getVar(
-            'SELECT id FROM modules_extras WHERE module = ? AND type = ? AND action = ?',
-            ['Search', ModuleExtraType::widget(), 'Form']
-        );
+    private function configureFrontendPages(): void
+    {
+        $searchId = $this->getSearchWidgetId();
 
         // loop languages
         foreach ($this->getLanguages() as $language) {
-            // check if a page for tags already exists in this language
-            // @todo refactor this if statement
-            if (!(bool) $this->getDB()->getVar(
-                'SELECT 1
-                 FROM pages AS p
-                 INNER JOIN pages_blocks AS b ON b.revision_id = p.revision_id
-                 WHERE b.extra_id = ? AND p.language = ?
-                 LIMIT 1',
-                [$tagsID, $language]
-            )
-            ) {
+            if (!$this->hasTagsExtra($language)) {
                 // insert contact page
                 $this->insertPage(
                     [
@@ -73,10 +70,30 @@ class Installer extends ModuleInstaller
                         'language' => $language,
                     ],
                     null,
-                    ['extra_id' => $tagsID, 'position' => 'main'],
+                    ['extra_id' => $this->tagsExtraId, 'position' => 'main'],
                     ['extra_id' => $searchId, 'position' => 'top']
                 );
             }
         }
+    }
+
+    private function getSearchWidgetId(): int
+    {
+        return (int) $this->getDB()->getVar(
+            'SELECT id FROM modules_extras WHERE module = ? AND type = ? AND action = ?',
+            ['Search', ModuleExtraType::widget(), 'Form']
+        );
+    }
+
+    private function hasTagsExtra(string $language): bool
+    {
+        return (bool) $this->getDB()->getVar(
+            'SELECT 1
+             FROM pages AS p
+             INNER JOIN pages_blocks AS b ON b.revision_id = p.revision_id
+             WHERE b.extra_id = ? AND p.language = ?
+             LIMIT 1',
+            [$this->tagsExtraId, $language]
+        );
     }
 }
