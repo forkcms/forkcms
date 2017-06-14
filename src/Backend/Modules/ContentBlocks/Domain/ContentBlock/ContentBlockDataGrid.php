2401c3c31478b2fecdd2e32df279460e89a54dd5
Jeroen Desloovere <info@jeroendesloovere.be>
2017-06-14 10:11:12 +0200

    Refactored datagrids (all datagrid logic is moved in the class)

--- src/Backend/Modules/ContentBlocks/Domain/ContentBlock/ContentBlockDataGrid.php
+++ src/Backend/Modules/ContentBlocks/Domain/ContentBlock/ContentBlockDataGrid.php
@@ -3,8 +3,11 @@
 namespace Backend\Modules\ContentBlocks\Domain\ContentBlock;
 
 use Backend\Core\Engine\DataGridDB;
+use Backend\Core\Engine\TemplateModifiers;
+use Backend\Core\Engine\Authentication as BackendAuthentication;
+use Backend\Core\Engine\Model;
+use Backend\Core\Language\Language;
 use Backend\Core\Language\Locale;
-use Backend\Modules\ContentBlocks\ValueObject\ContentBlockStatus;
 
 /**
  * @TODO replace with a doctrine implementation of the data grid
@@ -17,7 +20,25 @@ class ContentBlockDataGrid extends DataGridDB
             'SELECT i.id, i.title, i.hidden
              FROM content_blocks AS i
              WHERE i.status = :active AND i.language = :language',
-            ['active' => ContentBlockStatus::active(), 'language' => $locale]
+            ['active' => Status::active(), 'language' => $locale]
         );
+
+        $this->setSortingColumns(['title']);
+
+        // show the hidden status
+        $this->addColumn('isHidden', ucfirst(Language::lbl('VisibleOnSite')), '[hidden]');
+        $this->setColumnFunction([TemplateModifiers::class, 'showBool'], ['[hidden]', true], 'isHidden');
+
+        // check if this action is allowed
+        if (BackendAuthentication::isAllowedAction('ContentBlockEdit')) {
+            $editUrl = Model::createURLForAction('ContentBlockEdit', null, null, ['id' => '[id]'], false);
+            $this->setColumnURL('title', $editUrl);
+            $this->addColumn('edit', null, Language::lbl('Edit'), $editUrl, Language::lbl('Edit'));
+        }
+    }
+
+    public static function getHtml(Locale $locale): string
+    {
+        return (new self($locale))->getContent();
     }
 }
--- src/Backend/Modules/ContentBlocks/Domain/ContentBlock/ContentBlockRevisionDataGrid.php
+++ src/Backend/Modules/ContentBlocks/Domain/ContentBlock/ContentBlockRevisionDataGrid.php
@@ -2,10 +2,12 @@
 
 namespace Backend\Modules\ContentBlocks\Domain\ContentBlock;
 
+use Backend\Core\Engine\Authentication;
 use Backend\Core\Engine\DataGridDB;
+use Backend\Core\Engine\DataGridFunctions;
+use Backend\Core\Engine\Model;
+use Backend\Core\Language\Language;
 use Backend\Core\Language\Locale;
-use Backend\Modules\ContentBlocks\Entity\ContentBlock;
-use Backend\Modules\ContentBlocks\ValueObject\ContentBlockStatus;
 
 /**
  * @TODO replace with a doctrine implementation of the data grid
@@ -19,7 +21,52 @@ class ContentBlockRevisionDataGrid extends DataGridDB
              FROM content_blocks AS i
              WHERE i.status = :archived AND i.id = :id AND i.language = :language
              ORDER BY i.edited_on DESC',
-            ['archived' => ContentBlockStatus::archived(), 'language' => $locale, 'id' => $contentBlock->getId()]
+            ['archived' => Status::archived(), 'language' => $locale, 'id' => $contentBlock->getId()]
         );
+
+        // hide columns
+        $this->setColumnsHidden(['id', 'revision_id']);
+
+        // disable paging
+        $this->setPaging(false);
+
+        // set headers
+        $this->setHeaderLabels(
+            [
+                'user_id' => ucfirst(Language::lbl('By')),
+                'edited_on' => ucfirst(Language::lbl('LastEditedOn')),
+            ]
+        );
+
+        // set column-functions
+        $this->setColumnFunction([DataGridFunctions::class, 'getUser'], ['[user_id]'], 'user_id');
+        $this->setColumnFunction([DataGridFunctions::class, 'getTimeAgo'], ['[edited_on]'], 'edited_on');
+
+        // check if this action is allowed
+        if (Authentication::isAllowedAction('Edit')) {
+            $editRevisionUrl = Model::createURLForAction(
+                'ContentBlockEdit',
+                null,
+                null,
+                ['id' => '[id]', 'revision' => '[revision_id]'],
+                false
+            );
+            // set column URLs
+            $this->setColumnURL('title', $editRevisionUrl);
+
+            // add use column
+            $this->addColumn(
+                'use_revision',
+                null,
+                Language::lbl('UseThisVersion'),
+                $editRevisionUrl,
+                Language::lbl('UseThisVersion')
+            );
+        }
+    }
+
+    public static function getHtml(ContentBlock $contentBlock, Locale $locale): string
+    {
+        return (new self($contentBlock, $locale))->getContent();
     }
 }
