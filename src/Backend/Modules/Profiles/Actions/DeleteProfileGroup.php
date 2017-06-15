347ba131fdec5dfa87364bc84c8cbb55340113b3
Jonas De Keukelaere <jonas@sumocoders.be>
2017-06-15 07:25:36 +0100

    Fix code styling in profile group link delete

--- src/Backend/Modules/Profiles/Actions/DeleteProfileGroup.php
+++ src/Backend/Modules/Profiles/Actions/DeleteProfileGroup.php
@@ -32,24 +32,24 @@ class DeleteProfileGroup extends BackendBaseActionDelete
         $this->id = (int) $deleteFormData['id'];
 
         // does the item exist
-        if ($this->id !== 0 && BackendProfilesModel::existsProfileGroup($this->id)) {
-            // call parent, this will probably add some general CSS/JS or other required files
-            parent::execute();
-
-            // get profile group
-            $profileGroup = BackendProfilesModel::getProfileGroup($this->id);
-
-            // delete profile group
-            BackendProfilesModel::deleteProfileGroup($this->id);
-
-            // profile group was deleted, so redirect
-            $this->redirect(
-                BackendModel::createURLForAction(
-                    'edit'
-                ) . '&id=' . $profileGroup['profile_id'] . '&report=membership-deleted#tabGroups'
-            );
-        } else {
-            $this->redirect(BackendModel::createURLForAction('index') . '&error=non-existing');
+        if ($this->id === 0 || !BackendProfilesModel::existsProfileGroup($this->id)) {
+            $this->redirect(BackendModel::createURLForAction('Index') . '&error=non-existing');
         }
+
+        // call parent, this will probably add some general CSS/JS or other required files
+        parent::execute();
+
+        // get profile group
+        $profileGroup = BackendProfilesModel::getProfileGroup($this->id);
+
+        // delete profile group
+        BackendProfilesModel::deleteProfileGroup($this->id);
+
+        // profile group was deleted, so redirect
+        $this->redirect(
+            BackendModel::createURLForAction(
+                'Edit'
+            ) . '&id=' . $profileGroup['profile_id'] . '&report=membership-deleted#tabGroups'
+        );
     }
 }
