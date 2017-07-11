7f33308d541c03a7a0438e7d852a5a8630e7dd5a
Jelmer Prins <jelmer@sumocoders.be>
2017-07-11 11:35:00 +0200

    Free the base config class from the Object class

--- src/Backend/Core/Engine/Base/Config.php
+++ src/Backend/Core/Engine/Base/Config.php
@@ -9,6 +9,7 @@ namespace Backend\Core\Engine\Base;
  * file that was distributed with this source code.
  */
 
+use ForkCMS\App\KernelLoader;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Backend\Core\Engine\Exception as BackendException;
 use Backend\Core\Engine\Model as BackendModel;
@@ -17,7 +18,7 @@ use Backend\Core\Engine\Model as BackendModel;
  * This is the base-object for config-files. The module-specific config-files
  * can extend the functionality from this class
  */
-class Config extends Object
+class Config extends KernelLoader
 {
     /**
      * The default action
@@ -58,7 +59,7 @@ class Config extends Object
     {
         parent::__construct($kernel);
 
-        $this->setModule($module);
+        $this->isModuleAvailable($module);
     }
 
     public function getDefaultAction(): string
@@ -76,7 +77,7 @@ class Config extends Object
      *
      * @throws BackendException If module is not allowed
      */
-    public function setModule(string $module): void
+    private function isModuleAvailable(string $module): void
     {
         // does this module exist?
         $modules = BackendModel::getModulesOnFilesystem();
@@ -87,9 +88,6 @@ class Config extends Object
             // throw exception
             throw new BackendException('Module not allowed.');
         }
-
-        // set property
-        $this->module = $module;
     }
 
     public function getPossibleActionTypes(): array
@@ -102,18 +100,15 @@ class Config extends Object
 
     public function isActionAvailable(string $action): bool
     {
-        // Save our action
-        $this->action = $action;
-
         // Loop over every action type
         foreach ($this->getPossibleActionTypes() as $actionType) {
             // If this action is disabled for this type, continue on to the next type
-            if ($this->isActionDisabled($actionType)) {
+            if ($this->isActionDisabled($actionType, $action)) {
                 continue;
             }
 
             // If the action file is missing, continue on to the next type
-            if (!$this->isActionFilePresent($actionType)) {
+            if (!$this->isActionFilePresent($actionType, $action)) {
                 continue;
             }
 
@@ -125,19 +120,19 @@ class Config extends Object
         return false;
     }
 
-    public function isActionDisabled(string $actionType): bool
+    private function isActionDisabled(string $actionType, string $action): bool
     {
         switch ($actionType) {
             case 'actions':
-                return in_array($this->getAction(), $this->disabledActions, true);
+                return in_array($action, $this->disabledActions, true);
             case 'ajax':
-                return in_array($this->action, $this->disabledAJAXActions, true);
+                return in_array($action, $this->disabledAJAXActions, true);
         }
 
         throw new \Exception($actionType . ' is not a valid action type');
     }
 
-    public function isActionFilePresent(string $actionType): bool
+    private function isActionFilePresent(string $actionType, string $action): bool
     {
         // Create the directory string
         $directory = BACKEND_MODULES_PATH . '/' . $this->module . '/' . ucfirst($actionType);
@@ -148,6 +143,6 @@ class Config extends Object
         }
 
         // Return if the file exists
-        return file_exists($directory . '/' . ucfirst($this->action) . '.php');
+        return file_exists($directory . '/' . ucfirst($action) . '.php');
     }
 }
