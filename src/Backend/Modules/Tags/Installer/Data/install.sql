335bb46475ea46ac014b769e1b6745e8f63d52c4
Jelmer Prins <jelmer@sumocoders.be>
2017-07-24 13:55:30 +0200

    Add the sql changes to the installers

--- src/Backend/Modules/Tags/Installer/Data/install.sql
+++ src/Backend/Modules/Tags/Installer/Data/install.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `tags` (
- `id` int(11) NOT NULL auto_increment,
- `language` varchar(5) NOT NULL,
- `tag` varchar(255) NOT NULL,
- `number` int(11) NOT NULL,
- `url` varchar(255) NOT NULL,
- PRIMARY KEY (`id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `language` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tag` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `number` int(11) NOT NULL,
+  `url` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
