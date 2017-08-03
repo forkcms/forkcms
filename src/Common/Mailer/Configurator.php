b359dda6850aa5e69ce27b65dc655a75fb1da271
Jelmer Prins <jelmer@sumocoders.be>
2017-08-03 18:18:26 +0200

    Fix tests for mailer configuration

--- src/Common/Mailer/Configurator.php
+++ src/Common/Mailer/Configurator.php
@@ -47,7 +47,10 @@ class Configurator
                 $this->modulesSettings->get('Core', 'smtp_password'),
                 $this->modulesSettings->get('Core', 'smtp_secure_layer')
             );
-            $this->container->get('mailer')->__construct($transport);
+            $mailer = $this->container->get('mailer');
+            if ($mailer !== null) {
+                $this->container->get('mailer')->__construct($transport);
+            }
             $this->container->set(
                 'swiftmailer.transport',
                 $transport
