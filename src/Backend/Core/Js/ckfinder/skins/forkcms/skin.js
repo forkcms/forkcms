932c9743f5733d67858d7d85a28a161b52953a34
stijnvrolijk <stijn@sumocoders.be>
2017-08-17 16:41:34 +0200

    Use our own icons

--- src/Backend/Core/Js/ckfinder/skins/forkcms/skin.js
+++ src/Backend/Core/Js/ckfinder/skins/forkcms/skin.js
@@ -3,7 +3,7 @@ CKFinder.define(
         config: function( config ) {
             // @TODO: We could probably move this to a SASS generated file
             config.themeCSS = 'skins/forkcms/skin.css';
-            config.iconsCSS = 'skins/moono/icons.css';
+            config.iconsCSS = 'skins/forkcms/icons.css';
 
             return config;
         },
