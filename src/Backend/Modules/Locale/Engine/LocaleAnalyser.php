69d4a125200349bfa1c2c3249c5b5e0ef5ced9f8
Jelmer Prins <jelmer@sumocoders.be>
2017-08-28 11:59:43 +0200

    Reorder methods

--- src/Backend/Modules/Locale/Engine/LocaleAnalyser.php
+++ src/Backend/Modules/Locale/Engine/LocaleAnalyser.php
@@ -32,93 +32,6 @@ final class LocaleAnalyser
         return [];
     }
 
-    /**
-     * Get a string between two key strings
-     *
-     * @param string $start front key string
-     * @param string $end back key string
-     * @param string $str the string that needs to be checked
-     *
-     * @return mixed return string or false
-     */
-    private function getInBetweenStrings(string $start, string $end, string $str)
-    {
-        $matches = [];
-        preg_match_all("@$start([a-zA-Z0-9_]*)$end@", $str, $matches);
-
-        return isset($matches[1]) ? current($matches[1]) : '';
-    }
-
-    private function getDefaultModule(string $currentModuleName): string
-    {
-        return $this->application === 'Backend' ? $currentModuleName : 'Core';
-    }
-
-    private function getLocaleArrayFromRegexMatches(string $defaultModuleName, array $matches): array
-    {
-        // remove the full match
-        $matchesCount = count(array_shift($matches));
-
-        [$types, $labels, $modules] = $matches;
-
-        $locale = [];
-
-        for ($index = 0; $index < $matchesCount; ++$index) {
-            $module = empty($modules[$index]) ? $defaultModuleName : $modules[$index];
-            $locale[$types[$index]][$module][$labels[$index]] = $labels[$index];
-        }
-
-        return $locale;
-    }
-
-    private function findLocaleInJsFile(string $moduleName, string $filename, string $fileContent): array
-    {
-        $locale = [];
-        $patterns = [
-            '"\.locale\.(act|err|lbl|msg)\(\'(\w+?)\'(?:, ?\'(\w+?)\')?\)"',
-            '"\.locale\.get\(\'(\w+?)\', ?\'(\w+?)\'(?:, ?\'(\w+?)\')?\)"',
-        ];
-        foreach ($patterns as $pattern) {
-            $matches = [];
-            preg_match_all($pattern, $fileContent, $matches);
-            $locale[] = $this->getLocaleArrayFromRegexMatches($this->getDefaultModule($moduleName), $matches);
-        }
-
-        if (count($locale) === 0) {
-            return [];
-        }
-
-        return [
-            'file' => $filename,
-            'locale' => array_merge_recursive(...$locale),
-        ];
-    }
-
-    private function findLocaleInFile(string $moduleName, string $filename, SplFileInfo $file): array
-    {
-        switch ($file->getExtension()) {
-            case 'js':
-                return $this->findLocaleInJsFile($moduleName, $filename, $file->getContents());
-            default:
-                return [];
-        }
-    }
-
-    private function findLocaleInModuleFiles(string $moduleName, array $module): array
-    {
-        $locale = [];
-        foreach ($module as $filename => $file) {
-            $fileLocale = $this->findLocaleInFile($moduleName, $filename, $file);
-
-
-            if (!empty($fileLocale)) {
-                $locale[$filename] = $fileLocale;
-            }
-        }
-
-        return $locale;
-    }
-
     private function findModuleFiles(): array
     {
         $moduleFiles = [];
@@ -149,5 +62,83 @@ final class LocaleAnalyser
         }
 
         return $locale;
+    }
+
+    private function findLocaleInModuleFiles(string $moduleName, array $module): array
+    {
+        $locale = [];
+        foreach ($module as $filename => $file) {
+            $fileLocale = $this->findLocaleInFile($moduleName, $filename, $file);
+
+
+            if (!empty($fileLocale)) {
+                $locale[$filename] = $fileLocale;
+            }
+        }
+
+        return $locale;
+    }
+
+    private function findLocaleInFile(string $moduleName, string $filename, SplFileInfo $file): array
+    {
+        switch ($file->getExtension()) {
+            case 'js':
+                return $this->findLocaleInJsFile($moduleName, $filename, $file->getContents());
+            default:
+                return [];
+        }
+    }
+
+    private function findLocaleInJsFile(string $moduleName, string $filename, string $fileContent): array
+    {
+        $locale = [];
+        $patterns = [
+            '"\.locale\.(act|err|lbl|msg)\(\'(\w+?)\'(?:, ?\'(\w+?)\')?\)"',
+            '"\.locale\.get\(\'(\w+?)\', ?\'(\w+?)\'(?:, ?\'(\w+?)\')?\)"',
+        ];
+        foreach ($patterns as $pattern) {
+            $matches = [];
+            preg_match_all($pattern, $fileContent, $matches);
+            $locale[] = $this->getLocaleArrayFromRegexMatches($this->getDefaultModule($moduleName), $matches);
+        }
+
+        if (count($locale) === 0) {
+            return [];
+        }
+
+        return [
+            'file' => $filename,
+            'locale' => array_merge_recursive(...$locale),
+        ];
+    }
+
+    private function getLocaleArrayFromRegexMatches(string $defaultModuleName, array $matches): array
+    {
+        // remove the full match
+        $matchesCount = count(array_shift($matches));
+
+        [$types, $labels, $modules] = $matches;
+
+        $locale = [];
+
+        for ($index = 0; $index < $matchesCount; ++$index) {
+            $module = empty($modules[$index]) ? $defaultModuleName : $modules[$index];
+            $locale[$types[$index]][$module][$labels[$index]] = $labels[$index];
+        }
+
+        return $locale;
+    }
+
+    private function getInBetweenStrings(string $start, string $end, string $str)
+    {
+        $matches = [];
+        preg_match_all("@$start([a-zA-Z0-9_]*)$end@", $str, $matches);
+
+        return isset($matches[1]) ? current($matches[1]) : '';
+    }
+
+    private function getDefaultModule(string $currentModuleName): string
+    {
+        return $this->application === 'Backend' ? $currentModuleName : 'Core';
     }
 }
