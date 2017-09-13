7cb28ab0db7c9222aaed67c33ed7b9ce82f48fa3
Jelmer Prins <jelmer@sumocoders.be>
2017-09-13 13:57:30 +0200

    Fix ancient bad merge

--- src/Frontend/Core/Engine/RssItem.php
+++ src/Frontend/Core/Engine/RssItem.php
@@ -84,13 +84,9 @@ class RssItem extends \SpoonFeedRSSItem
         // remove special chars
         $author = (string) \SpoonFilter::htmlspecialcharsDecode($author);
 
-        // add fake-emailaddress
+        // add fake emailaddress
         if (!filter_var($author, FILTER_VALIDATE_EMAIL)) {
             $author = CommonUri::getUrl($author) . '@example.com (' . $author . ')';
-        }
-        // add fake email address
-        if (!filter_var($author, FILTER_VALIDATE_EMAIL)) {
-            $author = \SpoonFilter::urlise($author) . '@example.com (' . $author . ')';
         }
 
         // set author
