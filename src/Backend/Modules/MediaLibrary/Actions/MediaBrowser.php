39eecff01858aac28a79cbed47aa8cdb3d3d7af9
Bruno Vitorino <bruno@sumocoders.be>
2017-09-20 13:26:58 +0200

    Show media items in list.

--- src/Backend/Modules/MediaLibrary/Actions/MediaBrowser.php
+++ src/Backend/Modules/MediaLibrary/Actions/MediaBrowser.php
@@ -2,14 +2,109 @@
 
 namespace Backend\Modules\MediaLibrary\Actions;
 
-use Backend\Core\Engine\Base\ActionIndex as BackendBaseActionIndex;
+use Backend\Core\Engine\Base\ActionAdd as BackendBaseActionAdd;
+use Backend\Core\Engine\DataGridDatabase;
+use Backend\Core\Language\Language;
+use Backend\Modules\MediaLibrary\Domain\MediaFolder\Exception\MediaFolderNotFound;
+use Backend\Modules\MediaLibrary\Domain\MediaFolder\MediaFolder;
+use Backend\Modules\MediaLibrary\Domain\MediaGroup\MediaGroupType;
+use Backend\Modules\MediaLibrary\Domain\MediaItem\MediaItemSelectionDataGrid;
+use Backend\Modules\MediaLibrary\Domain\MediaItem\Type;
 
-class MediaBrowser extends BackendBaseActionIndex
+class MediaBrowser extends BackendBaseActionAdd
 {
+    /** @var MediaFolder */
+    protected $mediaFolder;
+
     public function execute(): void
     {
         parent::execute();
+
+        /** @var MediaFolder|null $mediaFolder */
+        $this->mediaFolder = $this->getMediaFolder();
+
+        $this->parseJsFiles();
         $this->parse();
         $this->display();
+    }
+
+    protected function getMediaFolder(): ?MediaFolder
+    {
+        /** @var int $id */
+        $id = 1;
+
+        try {
+            return $this->get('media_library.repository.folder')->findOneById($id);
+        } catch (MediaFolderNotFound $mediaFolderNotFound) {
+            return null;
+        }
+    }
+
+    protected function parse(): void
+    {
+        // Parse files necessary for the media upload helper
+        MediaGroupType::parseFiles();
+
+        $this->parseDataGrids($this->mediaFolder);
+
+        /** @var int|null $mediaFolderId */
+        $mediaFolderId = ($this->mediaFolder instanceof MediaFolder) ? $this->mediaFolder->getId() : null;
+
+        $this->template->assign('folderId', $mediaFolderId);
+        $this->template->assign('tree', $this->get('media_library.manager.tree')->getHTML());
+        $this->header->addJsData('MediaLibrary', 'openedFolderId', $mediaFolderId);
+    }
+
+    private function parseJsFiles(): void
+    {
+        $this->header->addJS('jstree/jquery.tree.js', 'Pages');
+        $this->header->addJS('jstree/lib/jquery.cookie.js', 'Pages');
+        $this->header->addJS('jstree/plugins/jquery.tree.cookie.js', 'Pages');
+        $this->header->addJS('MediaLibraryHelper.js', 'MediaLibrary', true);
+    }
+
+    private function parseDataGrids(MediaFolder $mediaFolder = null): void
+    {
+        /** @var array $dataGrids */
+        $dataGrids = $this->getDataGrids($mediaFolder);
+
+        $this->template->assign('dataGrids', $dataGrids);
+        $this->template->assign('hasResults', $this->hasResults($dataGrids));
+    }
+
+    private function getDataGrids(MediaFolder $mediaFolder = null): array
+    {
+        return array_map(
+            function ($type) use ($mediaFolder) {
+                /** @var DataGridDatabase $dataGrid */
+                $dataGrid = MediaItemSelectionDataGrid::getDataGrid(
+                    Type::fromString($type),
+                    ($mediaFolder !== null) ? $mediaFolder->getId() : null
+                );
+
+                return [
+                    'label' => Language::lbl('MediaMultiple' . ucfirst($type)),
+                    'tabName' => 'tab' . ucfirst($type),
+                    'mediaType' => $type,
+                    'html' => $dataGrid->getContent(),
+                    'numberOfResults' => $dataGrid->getNumResults(),
+                ];
+            },
+            Type::POSSIBLE_VALUES
+        );
+    }
+
+    private function hasResults(array $dataGrids): bool
+    {
+        $totalResultCount = array_sum(
+            array_map(
+                function ($dataGrid) {
+                    return $dataGrid['numberOfResults'];
+                },
+                $dataGrids
+            )
+        );
+
+        return $totalResultCount > 0;
     }
 }
---
+++ src/Backend/Modules/MediaLibrary/Domain/MediaItem/MediaItemSelectionDataGrid.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Backend\Modules\MediaLibrary\Domain\MediaItem;
+
+use Backend\Core\Engine\DataGridDatabase;
+use Backend\Core\Engine\DataGridFunctions as BackendDataGridFunctions;
+use Backend\Core\Engine\Model;
+use Backend\Core\Language\Language;
+use SpoonFormDropdown;
+
+/**
+ * @TODO replace with a doctrine implementation of the data grid
+ */
+class MediaItemSelectionDataGrid extends DataGridDatabase
+{
+    public function __construct(Type $type, int $folderId = null)
+    {
+        parent::__construct(
+            'SELECT i.id, i.storageType, i.type, i.url, i.title, i.shardingFolderName,
+                COUNT(gi.mediaItemId) AS num_connected, i.mime, UNIX_TIMESTAMP(i.createdOn) AS createdOn
+             FROM MediaItem AS i
+             LEFT OUTER JOIN MediaGroupMediaItem AS gi ON gi.mediaItemId = i.id
+             WHERE i.type = ?' . $this->getWhere($folderId) . ' GROUP BY i.id',
+            $this->getParameters($type, $folderId)
+        );
+
+        // filter on folder?
+        if ($folderId !== null) {
+            $this->setURL('&folder=' . $folderId, true);
+        }
+
+        $this->setExtras($type);
+    }
+
+    private function getColumnHeaderLabels(Type $type): array
+    {
+        if ($type->isMovie()) {
+            return [
+                'storageType' => ucfirst(Language::lbl('MediaStorageType')),
+                'url' => ucfirst(Language::lbl('MediaMovieId')),
+                'title' => ucfirst(Language::lbl('MediaMovieTitle')),
+            ];
+        }
+
+        return [
+            'type' => '',
+            'url' => ucfirst(Language::lbl('Image')),
+        ];
+    }
+
+    private function getColumnsThatNeedToBeHidden(Type $type): array
+    {
+        if ($type->isImage()) {
+            return ['storageType', 'shardingFolderName', 'type', 'mime'];
+        }
+
+        if ($type->isMovie()) {
+            return ['shardingFolderName', 'type', 'mime'];
+        }
+
+        return ['storageType', 'shardingFolderName', 'type', 'mime', 'url'];
+    }
+
+    public static function getDataGrid(Type $type, int $folderId = null): DataGridDatabase
+    {
+        return new self($type, $folderId);
+    }
+
+    public static function getHtml(Type $type, int $folderId = null): string
+    {
+        return (string) (new self($type, $folderId))->getContent();
+    }
+
+    private function getParameters(Type $type, int $folderId = null): array
+    {
+        $parameters = [(string) $type];
+
+        if ($folderId !== null) {
+            $parameters[] = $folderId;
+        }
+
+        return $parameters;
+    }
+
+    private function getWhere(int $folderId = null): string
+    {
+        return ($folderId !== null) ? ' AND i.mediaFolderId = ?' : '';
+    }
+
+    private function setExtras(Type $type): void
+    {
+        $this->setHeaderLabels($this->getColumnHeaderLabels($type));
+        $this->setActiveTab('tab' . ucfirst((string) $type));
+        $this->setColumnsHidden($this->getColumnsThatNeedToBeHidden($type));
+        $this->setSortingColumns(
+            [
+                'createdOn',
+                'url',
+                'title',
+                'num_connected',
+                'mime',
+            ],
+            'title'
+        );
+        $this->setSortParameter('asc');
+
+        // If we have an image, show the image
+        if ($type->isImage()) {
+            // Add image url
+            $this->setColumnFunction(
+                [new BackendDataGridFunctions(), 'showImage'],
+                [
+                    Model::get('media_library.storage.local')->getWebDir() . '/[shardingFolderName]',
+                    '[url]',
+                    '[url]',
+                    null,
+                    null,
+                    null,
+                    'media_library_backend_thumbnail',
+                ],
+                'url',
+                true
+            );
+        }
+
+        // set column functions
+        $this->setColumnFunction(
+            [new BackendDataGridFunctions(), 'getLongDate'],
+            ['[createdOn]'],
+            'createdOn',
+            true
+        );
+
+        // our JS needs to know an id, so we can highlight it
+        $this->setRowAttributes(['id' => 'row-[id]']);
+    }
+}
