a77a5b659c1921d6425ea58c3be813e1d80bdb85
Bruno Vitorino <bruno@sumocoders.be>
2017-09-25 10:13:53 +0200

    Fix regex to allow relative urls.

--- src/Backend/Core/Js/ckeditor/plugins/medialibrary/dialogs/linkDialog.js
+++ src/Backend/Core/Js/ckeditor/plugins/medialibrary/dialogs/linkDialog.js
@@ -1,7 +1,7 @@
 CKEDITOR.dialog.add(
     'linkDialog',
     function (editor) {
-        var urlRegex = 'https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}(\\.[a-z]{2,6})?\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)';
+        var urlRegex = '(https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}(\\.[a-z]{2,6})?)?\\/([-a-zA-Z0-9@:%_\\+.~#?&//=]*)';
 
         return {
             title: 'Add link',
