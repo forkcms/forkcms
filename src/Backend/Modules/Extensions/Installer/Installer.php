6ba781adf782d2372469a85059b1e455406e2713
Mathias Helin <mathias@sumocoders.be>
2017-09-28 15:31:49 +0200

    Make sure the correct default template for the Custom theme is installed

--- src/Backend/Modules/Extensions/Installer/Installer.php
+++ src/Backend/Modules/Extensions/Installer/Installer.php
@@ -236,7 +236,7 @@ class Installer extends ModuleInstaller
         $this->setSetting('Core', 'theme', 'Custom', true);
 
         // set default template
-        $this->setSetting('Pages', 'default_template', $this->getTemplateId('Default'));
+        $this->setSetting('Pages', 'default_template', $this->getTemplateId('Default', 'Custom'));
 
         // disable meta navigation
         $this->setSetting('Pages', 'meta_navigation', false);
