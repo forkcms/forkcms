ce6aae673f016d289f45d68113c2d33b41124d43
Jelmer Prins <jelmer@sumocoders.be>
2017-10-02 13:06:03 +0200

    Clean up the RSS action of the blog module

--- src/Frontend/Modules/Blog/Actions/Rss.php
+++ src/Frontend/Modules/Blog/Actions/Rss.php
@@ -16,109 +16,119 @@ use Frontend\Core\Engine\Rss as FrontendRSS;
 use Frontend\Core\Engine\RssItem as FrontendRSSItem;
 use Frontend\Core\Engine\User as FrontendUser;
 use Frontend\Modules\Blog\Engine\Model as FrontendBlogModel;
+use SpoonFilter;
 
-/**
- * This is the RSS-feed
- */
 class Rss extends FrontendBaseBlock
 {
-    /**
-     * The articles
-     *
-     * @var array
-     */
-    private $items;
-
-    /**
-     * The settings
-     *
-     * @var array
-     */
-    private $settings;
-
     public function execute(): void
     {
         parent::execute();
-        $this->getData();
-        $this->parse();
+
+        $this->generateRss();
     }
 
-    private function getData(): void
+    private function generateRss()
     {
-        $this->items = FrontendBlogModel::getAll(30);
-        $this->settings = $this->get('fork.settings')->getForModule('Blog');
-    }
+        $blogPosts = FrontendBlogModel::getAll(30);
 
-    private function parse(): void
-    {
-        // get vars
-        $title = (isset($this->settings['rss_title_' . LANGUAGE])) ? $this->settings['rss_title_' . LANGUAGE] : $this->get('fork.settings')->get('Blog', 'rss_title_' . LANGUAGE, SITE_DEFAULT_TITLE);
-        $link = SITE_URL . FrontendNavigation::getUrlForBlock('Blog');
-        $description = (isset($this->settings['rss_description_' . LANGUAGE])) ? $this->settings['rss_description_' . LANGUAGE] : null;
+        $rss = new FrontendRSS(
+            $this->get('fork.settings')->get('Blog', 'rss_title_' . LANGUAGE, SITE_DEFAULT_TITLE),
+            SITE_URL . FrontendNavigation::getUrlForBlock($this->getModule()),
+            $this->settings['rss_description_' . LANGUAGE] ?? ''
+        );
 
-        // create new rss instance
-        $rss = new FrontendRSS($title, $link, $description);
-
-        // loop articles
-        foreach ($this->items as $item) {
-            $title = $item['title'];
-            $link = $item['full_url'];
-            $description = ($item['introduction'] != '') ? $item['introduction'] : $item['text'];
-
-            // meta is wanted
-            if ($this->get('fork.settings')->get('Blog', 'rss_meta_' . LANGUAGE, true)) {
-                // append meta
-                $description .= '<div class="meta">' . "\n";
-                $description .= '    <p><a href="' . $link . '" title="' . $title . '">' . $title . '</a> ' .
-                                sprintf(
-                                    FL::msg('WrittenBy'),
-                                    FrontendUser::getBackendUser($item['user_id'])->getSetting('nickname')
-                                );
-                $description .= ' ' . FL::lbl('In') . ' <a href="' . $item['category_full_url'] . '" title="' .
-                                $item['category_title'] . '">' . $item['category_title'] . '</a>.</p>' . "\n";
-
-                // any tags
-                if (isset($item['tags'])) {
-                    // append tags-paragraph
-                    $description .= '    <p>' . \SpoonFilter::ucfirst(FL::lbl('Tags')) . ': ';
-                    $first = true;
-
-                    // loop tags
-                    foreach ($item['tags'] as $tag) {
-                        // prepend separator
-                        if (!$first) {
-                            $description .= ', ';
-                        }
-
-                        // add
-                        $description .= '<a href="' . $tag['full_url'] . '" rel="tag" title="' . $tag['name'] . '">' .
-                                        $tag['name'] . '</a>';
-
-                        // reset
-                        $first = false;
-                    }
-
-                    // end
-                    $description .= '.</p>' . "\n";
-                }
-
-                // end HTML
-                $description .= '</div>' . "\n";
-            }
-
-            // create new instance
-            $rssItem = new FrontendRSSItem($title, $link, $description);
-
-            // set item properties
-            $rssItem->setPublicationDate($item['publish_on']);
-            $rssItem->addCategory($item['category_title']);
-            $rssItem->setAuthor(FrontendUser::getBackendUser($item['user_id'])->getEmail());
-
-            // add item
-            $rss->addItem($rssItem);
+        foreach ($blogPosts as $blogPost) {
+            $rss->addItem(
+                $this->getRssFeedItemForBlogPost(
+                    $blogPost,
+                    $this->get('fork.settings')->get($this->getModule(), 'rss_meta_' . LANGUAGE, true)
+                )
+            );
         }
 
-        // output
         $rss->parse();
+    }
+
+    private function getRssFeedItemForBlogPost(array $blogPost, bool $includeMeta): FrontendRSSItem
+    {
+        $rssItem = new FrontendRSSItem(
+            $blogPost['title'],
+            $blogPost['full_url'],
+            $this->getDescriptionForBlogPost($blogPost, $includeMeta)
+        );
+
+        $rssItem->setPublicationDate($blogPost['publish_on']);
+        $rssItem->addCategory($blogPost['category_title']);
+        $rssItem->setAuthor(FrontendUser::getBackendUser($blogPost['user_id'])->getEmail());
+
+        return $rssItem;
+    }
+
+    private function getMetaDescription(
+        string $blogPostTitle,
+        string $blogPostLink,
+        string $blogPostAuthor,
+        string $categoryTitle,
+        string $categoryLink
+    ): string {
+        return sprintf(
+            '<p><a href="%1$s" title="%2$s">%2$s</a> %3$s %4$s <a href="%5$s" title="%6$s">%6$s</a></p>',
+            $blogPostLink,
+            $blogPostTitle,
+            sprintf(FL::msg('WrittenBy'), $blogPostAuthor),
+            FL::lbl('In'),
+            $categoryLink,
+            $categoryTitle
+        );
+    }
+
+    private function getDescriptionForBlogPost(array $blogPost, bool $includeMeta): string
+    {
+        $description = empty($blogPost['introduction']) ? $blogPost['text'] : $blogPost['introduction'];
+
+        if (!$includeMeta) {
+            return $description;
+        }
+
+        $description .= '<div class="meta">';
+
+        $description .= $this->getMetaDescription(
+            $blogPost['title'],
+            $blogPost['full_url'],
+            FrontendUser::getBackendUser($blogPost['user_id'])->getSetting('nickname'),
+            $blogPost['category_title'],
+            $blogPost['category_full_url']
+        );
+
+        if (isset($blogPost['tags'])) {
+            $description .= $this->getTagsDescription($blogPost['tags']);
+        }
+
+        return $description;
+    }
+
+    private function getTagsDescription(array $tags): string
+    {
+        if (empty($tags)) {
+            return '';
+        }
+
+        return sprintf(
+            '<p>%1$s: %2$s</p>',
+            SpoonFilter::ucfirst(FL::lbl('Tags')),
+            implode(
+                ', ',
+                array_map(
+                    function (array $tag) : string {
+                        return sprintf(
+                            '<a href="%1$s" rel="tag" title="%2$s">%2$s</a>',
+                            $tag['full_url'],
+                            $tag['name']
+                        );
+                    },
+                    $tags
+                )
+            )
+        );
     }
 }
