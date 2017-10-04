2f9ee0384c958c3ae3e4752e7a6fe971f5d8fcc8
Jelmer Prins <jelmer@sumocoders.be>
2017-10-04 17:09:42 +0200

    Clean up the profiles index action

--- src/Frontend/Modules/Profiles/Actions/Index.php
+++ src/Frontend/Modules/Profiles/Actions/Index.php
@@ -10,36 +10,22 @@ namespace Frontend\Modules\Profiles\Actions;
  */
 
 use Frontend\Core\Engine\Base\Block as FrontendBaseBlock;
-use Frontend\Core\Engine\Navigation as FrontendNavigation;
 use Frontend\Modules\Profiles\Engine\Authentication as FrontendProfilesAuthentication;
+use Symfony\Component\Security\Core\Exception\InsufficientAuthenticationException;
 
 /**
- * This is the index-action, it can be used as a dashboard.
+ * You could use this as some kind of dashboard where you can show an activity
+ * stream, some statistics, ...
  */
 class Index extends FrontendBaseBlock
 {
     public function execute(): void
     {
-        // only logged in profiles can seer their dashboard
-        if (FrontendProfilesAuthentication::isLoggedIn()) {
-            // call the parent
-            parent::execute();
-
-            /*
-             * You could use this as some kind of dashboard where you can show an activity
-             * stream, some statistics, ...
-             */
-
-            $this->loadTemplate();
-        } else {
-            // profile not logged in
-            $this->redirect(
-                FrontendNavigation::getUrlForBlock(
-                    'Profiles',
-                    'Login'
-                ) . '?queryString=' . FrontendNavigation::getUrlForBlock('Profiles'),
-                307
-            );
+        if (!FrontendProfilesAuthentication::isLoggedIn()) {
+            throw new InsufficientAuthenticationException('You need to log in to see your dashboard');
         }
+
+        parent::execute();
+        $this->loadTemplate();
     }
 }
