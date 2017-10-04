2fda5b52fdb88a20a274abb6747f14f1356151c4
Jelmer Prins <jelmer@sumocoders.be>
2017-10-04 17:31:30 +0200

    Clean up the logout action

--- src/Frontend/Modules/Profiles/Actions/Logout.php
+++ src/Frontend/Modules/Profiles/Actions/Logout.php
@@ -12,19 +12,14 @@ namespace Frontend\Modules\Profiles\Actions;
 use Frontend\Core\Engine\Base\Block as FrontendBaseBlock;
 use Frontend\Modules\Profiles\Engine\Authentication as FrontendProfilesAuthentication;
 
-/**
- * This is the logout-action.
- */
 class Logout extends FrontendBaseBlock
 {
     public function execute(): void
     {
-        // logout
         if (FrontendProfilesAuthentication::isLoggedIn()) {
             FrontendProfilesAuthentication::logout();
         }
 
-        // redirect
         $this->redirect(SITE_URL);
     }
 }
