f01a0b47b34cfcfd3514620e103ec432b4a05c09
Jelmer Prins <jelmer@sumocoders.be>
2017-10-05 10:45:25 +0200

    Clean up the resend activation of the profiles module

--- src/Frontend/Modules/Profiles/Actions/ResendActivation.php
+++ src/Frontend/Modules/Profiles/Actions/ResendActivation.php
@@ -16,6 +16,7 @@ use Frontend\Core\Language\Language as FL;
 use Frontend\Core\Engine\Navigation as FrontendNavigation;
 use Frontend\Modules\Profiles\Engine\Authentication as FrontendProfilesAuthentication;
 use Frontend\Modules\Profiles\Engine\Model as FrontendProfilesModel;
+use Frontend\Modules\Profiles\Engine\Profile;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
@@ -28,101 +29,94 @@ class ResendActivation extends FrontendBaseBlock
      */
     private $form;
 
+    /** @var Profile */
+    private $profile;
+
     public function execute(): void
     {
-        // profile not logged in
-        if (!FrontendProfilesAuthentication::isLoggedIn()) {
-            parent::execute();
-            $this->loadTemplate();
-            $this->buildForm();
-            $this->validateForm();
-            $this->parse();
-        } else {
-            // profile logged in
+        if (FrontendProfilesAuthentication::isLoggedIn()) {
             throw new NotFoundHttpException();
         }
+
+        parent::execute();
+        $this->loadTemplate();
+        $this->buildForm();
+        $this->handleForm();
+        $this->parse();
     }
 
     private function buildForm(): void
     {
-        // create the form
         $this->form = new FrontendForm('resendActivation', null, null, 'resendActivation');
-
-        // create & add elements
-        $this->form->addText('email')->setAttributes(['required' => null, 'type' => 'email']);
+        $this->form->addText('email')->makeRequired()->setAttribute('type', 'email');
     }
 
     private function parse(): void
     {
-        // form was sent?
-        if ($this->url->getParameter('sent') == 'true') {
-            // show message
+        if ($this->url->getParameter('activationHasBeenResent') === 'true') {
             $this->template->assign('resendActivationSuccess', true);
-
-            // hide form
             $this->template->assign('resendActivationHideForm', true);
         }
 
-        // parse the form
         $this->form->parse($this->template);
     }
 
-    private function validateForm(): void
+    private function validateForm(): bool
     {
-        // is the form submitted
-        if ($this->form->isSubmitted()) {
-            // get field
-            $txtEmail = $this->form->getField('email');
+        $txtEmail = $this->form->getField('email');
 
-            // field is filled in?
-            if ($txtEmail->isFilled(FL::getError('EmailIsRequired'))) {
-                // valid email?
-                if ($txtEmail->isEmail(FL::getError('EmailIsInvalid'))) {
-                    // email exists?
-                    if (FrontendProfilesModel::existsByEmail($txtEmail->getValue())) {
-                        // get profile id using the filled in email
-                        $profileId = FrontendProfilesModel::getIdByEmail($txtEmail->getValue());
-
-                        // get profile
-                        $profile = FrontendProfilesModel::get($profileId);
-
-                        // must be inactive
-                        if ($profile->getStatus() != FrontendProfilesAuthentication::LOGIN_INACTIVE
-                        ) {
-                            $txtEmail->addError(FL::getError('ProfileIsActive'));
-                        }
-                    } else {
-                        // email don't exist
-                        $txtEmail->addError(FL::getError('EmailIsInvalid'));
-                    }
-                }
-            }
-
-            // valid login
-            if ($this->form->isCorrect()) {
-                // send email
-                $from = $this->get('fork.settings')->get('Core', 'mailer_from');
-                $replyTo = $this->get('fork.settings')->get('Core', 'mailer_reply_to');
-                $message = Message::newInstance(FL::getMessage('RegisterSubject'))
-                    ->setFrom([$from['email'] => $from['name']])
-                    ->setTo([$profile->getEmail() => ''])
-                    ->setReplyTo([$replyTo['email'] => $replyTo['name']])
-                    ->parseHtml(
-                        '/Profiles/Layout/Templates/Mails/Register.html.twig',
-                        [
-                            'activationUrl' => SITE_URL . FrontendNavigation::getUrlForBlock('Profiles', 'Activate') .
-                                               '/' . $profile->getSetting('activation_key'),
-                        ],
-                        true
-                    )
-                ;
-                $this->get('mailer')->send($message);
-
-                // redirect
-                $this->redirect(SITE_URL . $this->url->getQueryString() . '?sent=true');
-            } else {
-                $this->template->assign('resendActivationHasError', true);
-            }
+        if (!$txtEmail->isFilled(FL::getError('EmailIsRequired'))
+            || !$txtEmail->isEmail(FL::getError('EmailIsInvalid'))) {
+            return $this->form->isCorrect();
         }
+
+        if (!FrontendProfilesModel::existsByEmail($txtEmail->getValue())) {
+            $txtEmail->addError(FL::getError('EmailIsInvalid'));
+
+            return $this->form->isCorrect();
+        }
+
+        $this->profile = FrontendProfilesModel::get(FrontendProfilesModel::getIdByEmail($txtEmail->getValue()));
+
+        if ($this->profile->getStatus() !== FrontendProfilesAuthentication::LOGIN_INACTIVE) {
+            $txtEmail->addError(FL::getError('ProfileIsActive'));
+        }
+
+        return $this->form->isCorrect();
+    }
+
+    private function handleForm(): void
+    {
+        if (!$this->form->isSubmitted()) {
+            return;
+        }
+
+        if (!$this->validateForm()) {
+            $this->template->assign('resendActivationHasError', true);
+
+            return;
+        }
+
+        $this->resendActivationEmail();
+
+        $this->redirect(SITE_URL . $this->url->getQueryString() . '?activationHasBeenResent=true');
+    }
+
+    private function resendActivationEmail(): void
+    {
+        $activationUrl = SITE_URL . FrontendNavigation::getUrlForBlock($this->getModule(), 'Activate')
+                         . '/' . $this->profile->getSetting('activation_key');
+        $from = $this->get('fork.settings')->get('Core', 'mailer_from');
+        $replyTo = $this->get('fork.settings')->get('Core', 'mailer_reply_to');
+        $message = Message::newInstance(FL::getMessage('RegisterSubject'))
+            ->setFrom([$from['email'] => $from['name']])
+            ->setTo([$this->profile->getEmail() => $this->profile->getDisplayName()])
+            ->setReplyTo([$replyTo['email'] => $replyTo['name']])
+            ->parseHtml(
+                'Profiles/Layout/Templates/Mails/Register.html.twig',
+                ['activationUrl' => $activationUrl],
+                true
+            );
+        $this->get('mailer')->send($message);
     }
 }
