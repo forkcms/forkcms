1adc5b183bda1538a31b57d77a17b408010cd1e4
stijnvrolijk <stijn@sumocoders.be>
2017-10-12 16:02:05 +0200

    Create an event listener for the SitemapItemChanged event

---
+++ src/Backend/Modules/SitemapGenerator/EventListener/SitemapItemChangedListener.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Backend\Modules\SitemapGenerator\EventListener;
+
+use Backend\Modules\SitemapGenerator\Domain\SitemapEntry;
+use Backend\Modules\SitemapGenerator\Domain\SitemapEntryRepository;
+use Backend\Modules\SitemapGenerator\Domain\SitemapItemChanged;
+
+final class SitemapItemChangedListener
+{
+    /** @var SitemapEntryRepository */
+    private $sitemapEntryRepository;
+
+    public function __construct(SitemapEntryRepository $sitemapEntryRepository)
+    {
+        $this->sitemapEntryRepository = $sitemapEntryRepository;
+    }
+
+    public function onSitemapItemChanged(SitemapItemChanged $event)
+    {
+        $currentItem = $this->getCurrentItem($event);
+
+        if (!$currentItem instanceof SitemapEntry) {
+            $this->sitemapEntryRepository->add(SitemapEntry::fromEvent($event));
+
+            return;
+        }
+
+        $this->update($currentItem, $event);
+    }
+
+    private function update(SitemapEntry $currentItem, SitemapItemChanged $event): void
+    {
+        /** @var SitemapEntry[] $children */
+        $children = $this->sitemapEntryRepository->getChildren($currentItem);
+
+        foreach ($children as $child) {
+            $newUrl = str_replace($currentItem->getUrl(), $event->getUrl(), $child->getUrl());
+            $child->update(
+                $child->getSlug(),
+                $newUrl
+            );
+
+            $this->sitemapEntryRepository->update($child);
+        }
+
+        $currentItem->update(
+            $event->getSlug(),
+            $event->getUrl()
+        );
+
+        $this->sitemapEntryRepository->update($currentItem);
+    }
+
+    private function getCurrentItem(SitemapItemChanged $event): ?SitemapEntry
+    {
+        return $this->sitemapEntryRepository->findOneBy(
+            [
+                'module' => $event->getModule(),
+                'entity' => $event->getEntity(),
+                'otherId' => $event->getId(),
+            ]
+        );
+    }
+}
