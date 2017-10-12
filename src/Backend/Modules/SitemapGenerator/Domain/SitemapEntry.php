d62fafa6e07addb3963c2445846f7442f1a2682b
stijnvrolijk <stijn@sumocoders.be>
2017-10-12 14:05:05 +0200

    Create a SitemapEntry entity

---
+++ src/Backend/Modules/SitemapGenerator/Domain/SitemapEntry.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Backend\Modules\SitemapGenerator\Domain;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ */
+class SitemapEntry
+{
+    /**
+     * @var string
+     *
+     * @ORM\Id
+     * @ORM\Column(type="string")
+     */
+    private $module;
+
+    /**
+     * @var string
+     *
+     * @ORM\Id
+     * @ORM\Column(type="string")
+     */
+    private $entity;
+
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     */
+    private $otherId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $slug;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $url;
+
+    /**
+     * @var DateTime
+     *
+     * @ORM\Column(type="datetime")
+     */
+    private $createdOn;
+
+    /**
+     * @var DateTime
+     *
+     * @ORM\Column(type="datetime")
+     */
+    private $editedOn;
+
+    public function __construct(string $module, string $entity, int $otherId, string $slug, string $url)
+    {
+        $this->module = $module;
+        $this->entity = $entity;
+        $this->otherId = $otherId;
+        $this->slug = $slug;
+        $this->url = $url;
+    }
+
+    public function getModule(): string
+    {
+        return $this->module;
+    }
+
+    public function getEntity(): string
+    {
+        return $this->entity;
+    }
+
+    public function getOtherId(): int
+    {
+        return $this->otherId;
+    }
+
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    public function getCreatedOn(): DateTime
+    {
+        return $this->createdOn;
+    }
+
+    public function getEditedOn(): DateTime
+    {
+        return $this->editedOn;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function prePersist()
+    {
+        $this->createdOn = $this->editedOn = new DateTime();
+    }
+
+    /**
+     * @ORM\PreUpdate()
+     */
+    public function preUpdate()
+    {
+        $this->editedOn = new DateTime();
+    }
+}
--- src/Backend/Modules/SitemapGenerator/Installer/Installer.php
+++ src/Backend/Modules/SitemapGenerator/Installer/Installer.php
@@ -2,12 +2,15 @@
 
 namespace Backend\Modules\SitemapGenerator\Installer;
 
+use Backend\Core\Engine\Model;
 use Backend\Core\Installer\ModuleInstaller;
+use Backend\Modules\SitemapGenerator\Domain\SitemapEntry;
 
 class Installer extends ModuleInstaller
 {
     public function install(): void
     {
         $this->addModule('SitemapGenerator');
+        Model::get('fork.entity.create_schema')->forEntityClass(SitemapEntry::class);
     }
 }
