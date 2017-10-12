762126cdbbf9cf09b64282da56455a5774e804ba
stijnvrolijk <stijn@sumocoders.be>
2017-10-12 16:18:21 +0200

    Add some Doctrine configuration so it can find our entity

--- src/Backend/Modules/SitemapGenerator/DependencyInjection/SitemapGeneratorExtension.php
+++ src/Backend/Modules/SitemapGenerator/DependencyInjection/SitemapGeneratorExtension.php
@@ -4,14 +4,21 @@ namespace Backend\Modules\SitemapGenerator\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-class SitemapGeneratorExtension extends Extension
+class SitemapGeneratorExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('doctrine.yml');
     }
 }
