865710c77673fa765fb76f47ee2af1b745f31a18
Jeroen Desloovere <info@jeroendesloovere.be>
2017-10-18 09:51:03 +0200

    Updated ChangeFrequency

--- src/Backend/Modules/Sitemap/Domain/SitemapRow/ChangeFrequency.php
+++ src/Backend/Modules/Sitemap/Domain/SitemapRow/ChangeFrequency.php
@@ -4,80 +4,117 @@ namespace Backend\Modules\Sitemap\Domain\SitemapRow;
 
 final class ChangeFrequency
 {
-    /**
-     * @var string
-     */
+    protected const ALWAYS = 'always';
+    protected const HOURLY = 'hourly';
+    protected const DAILY = 'daily';
+    protected const WEEKLY = 'weekly';
+    protected const MONTHLY = 'monthly';
+    protected const YEARLY = 'yearly';
+    protected const NEVER = 'never';
+
+    public const POSSIBLE_VALUES = [
+        self::ALWAYS,
+        self::HOURLY,
+        self::DAILY,
+        self::WEEKLY,
+        self::MONTHLY,
+        self::YEARLY,
+        self::NEVER,
+    ];
+
+    /** @var string */
     private $value;
 
-    /**
-     * @param string $changeFrequency
-     */
     private function __construct($changeFrequency)
     {
+        if (!in_array($changeFrequency, self::POSSIBLE_VALUES)) {
+            throw new \Exception('The given changeFrequency is not allowed.');
+        }
+
         $this->value = $changeFrequency;
     }
 
-    /**
-     * @return string
-     */
     public function __toString(): string
     {
         return $this->value;
     }
 
-    /**
-     * @return ChangeFrequency
-     */
+    public function equals(ChangeFrequency $changeFrequency): bool
+    {
+        if (!($changeFrequency instanceof $this)) {
+            return false;
+        }
+
+        return $changeFrequency == $this;
+    }
+
     public static function always(): self
     {
-        return new self('always');
+        return new self(self::ALWAYS);
     }
 
-    /**
-     * @return ChangeFrequency
-     */
+    public function isAlways(): bool
+    {
+        return $this->equals(self::always());
+    }
+
     public static function hourly(): self
     {
-        return new self('hourly');
+        return new self(self::HOURLY);
     }
 
-    /**
-     * @return ChangeFrequency
-     */
+    public function isHourly(): bool
+    {
+        return $this->equals(self::hourly());
+    }
+
     public static function daily(): self
     {
-        return new self('daily');
+        return new self(self::DAILY);
     }
 
-    /**
-     * @return ChangeFrequency
-     */
+    public function isDaily(): bool
+    {
+        return $this->equals(self::daily());
+    }
+
     public static function weekly(): self
     {
-        return new self('weekly');
+        return new self(self::WEEKLY);
     }
 
-    /**
-     * @return ChangeFrequency
-     */
+    public function isWeekly(): bool
+    {
+        return $this->equals(self::weekly());
+    }
+
     public static function monthly(): self
     {
-        return new self('monthly');
+        return new self(self::MONTHLY);
     }
 
-    /**
-     * @return ChangeFrequency
-     */
+    public function isMonthly(): bool
+    {
+        return $this->equals(self::monthly());
+    }
+
     public static function yearly(): self
     {
-        return new self('yearly');
+        return new self(self::YEARLY);
     }
 
-    /**
-     * @return ChangeFrequency
-     */
+    public function isYearly(): bool
+    {
+        return $this->equals(self::yearly());
+    }
+
     public static function never(): self
     {
-        return new self('never');
+        return new self(self::NEVER);
+    }
+
+    public function isNever(): bool
+    {
+        return $this->equals(self::never());
     }
 }
