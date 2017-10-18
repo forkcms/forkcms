4284e5bc162ee698432ea137b25a8c30c148df75
Jelmer Prins <jelmer@sumocoders.be>
2017-10-18 09:22:11 +0200

    Clean up the edit comment action of the blog module

--- src/Backend/Modules/Blog/Actions/EditComment.php
+++ src/Backend/Modules/Blog/Actions/EditComment.php
@@ -8,56 +8,80 @@ use Backend\Core\Engine\Form as BackendForm;
 use Backend\Core\Language\Language as BL;
 use Backend\Modules\Blog\Engine\Model as BackendBlogModel;
 
-/**
- * This is the edit-action, it will display a form to edit an existing item
- */
 class EditComment extends BackendBaseActionEdit
 {
     public function execute(): void
     {
+        parent::execute();
+        $this->getData();
+        $this->handleForm();
+        $this->display();
+    }
+
+    private function getData(): void
+    {
         $this->id = $this->getRequest()->query->getInt('id');
 
         // does the item exist
-        if ($this->id !== 0 && BackendBlogModel::existsComment($this->id)) {
-            parent::execute();
-            $this->getData();
-            $this->loadForm();
-            $this->validateForm();
-            $this->parse();
-            $this->display();
-        } else {
-            // no item found, throw an exception, because somebody is fucking with our URL
+        if ($this->id === 0 || !BackendBlogModel::existsComment($this->id)) {
             $this->redirect(BackendModel::createUrlForAction('Index') . '&error=non-existing');
-        }
-    }
 
-    /**
-     * Get the data
-     * If a revision-id was specified in the URL we load the revision and not the actual data.
-     */
-    private function getData(): void
-    {
-        // get the record
+            return;
+        }
+
         $this->record = BackendBlogModel::getComment($this->id);
 
-        // no item found, throw an exceptions, because somebody is fucking with our URL
+        // Redirect to the index page if no comment was found
         if (empty($this->record)) {
             $this->redirect(BackendModel::createUrlForAction('Index') . '&error=non-existing');
         }
     }
 
-    private function loadForm(): void
+    private function handleForm()
     {
-        // create form
-        $this->form = new BackendForm('editComment');
+        $this->form = $this->buildForm();
 
-        // create elements
-        $this->form->addText('author', $this->record['author']);
-        $this->form->addText('email', $this->record['email']);
-        $this->form->addText('website', $this->record['website'], null);
-        $this->form->addTextarea('text', $this->record['text']);
+        if (!$this->form->isSubmitted() || !$this->validateForm()) {
+            $this->parse();
 
-        // assign URL
+            return;
+        }
+
+        $comment = [
+            'id' => $this->id,
+            'status' => $this->record['status'],
+            'author' => $this->form->getField('author')->getValue(),
+            'email' => $this->form->getField('email')->getValue(),
+            'website' => $this->form->getField('website')->isFilled()
+                ? $this->form->getField('website')->getValue() : null,
+            'text' => $this->form->getField('text')->getValue(),
+        ];
+
+        BackendBlogModel::updateComment($comment);
+
+        $this->redirect(
+            BackendModel::createUrlForAction('Comments') . '&report=edited-comment&id=' .
+            $comment['id'] . '&highlight=row-' . $comment['id'] . '#tab' .
+            \SpoonFilter::toCamelCase($comment['status'])
+        );
+    }
+
+    private function buildForm(): BackendForm
+    {
+        $form = new BackendForm('editComment');
+
+        $form->addText('author', $this->record['author']);
+        $form->addText('email', $this->record['email']);
+        $form->addText('website', $this->record['website'], null);
+        $form->addTextarea('text', $this->record['text']);
+
+        return $form;
+    }
+
+    protected function parse(): void
+    {
+        parent::parse();
+
         $this->template->assign(
             'itemURL',
             BackendModel::getUrlForBlock($this->getModule(), 'detail') . '/' .
@@ -66,43 +90,17 @@ class EditComment extends BackendBaseActionEdit
         $this->template->assign('itemTitle', $this->record['post_title']);
     }
 
-    private function validateForm(): void
+    private function validateForm(): bool
     {
-        if ($this->form->isSubmitted()) {
-            // cleanup the submitted fields, ignore fields that were added by hackers
-            $this->form->cleanupFields();
+        $this->form->cleanupFields();
 
-            // validate fields
-            $this->form->getField('author')->isFilled(BL::err('AuthorIsRequired'));
-            $this->form->getField('email')->isEmail(BL::err('EmailIsInvalid'));
-            $this->form->getField('text')->isFilled(BL::err('FieldIsRequired'));
-            if ($this->form->getField('website')->isFilled()) {
-                $this->form->getField('website')->isURL(BL::err('InvalidURL'));
-            }
-
-            // no errors?
-            if ($this->form->isCorrect()) {
-                // build item
-                $item = [
-                    'id' => $this->id,
-                    'status' => $this->record['status'],
-                    'author' => $this->form->getField('author')->getValue(),
-                    'email' => $this->form->getField('email')->getValue(),
-                    'website' => $this->form->getField('website')->isFilled()
-                        ? $this->form->getField('website')->getValue() : null,
-                    'text' => $this->form->getField('text')->getValue(),
-                ];
-
-                // insert the item
-                BackendBlogModel::updateComment($item);
-
-                // everything is saved, so redirect to the overview
-                $this->redirect(
-                    BackendModel::createUrlForAction('Comments') . '&report=edited-comment&id=' .
-                    $item['id'] . '&highlight=row-' . $item['id'] . '#tab' .
-                    \SpoonFilter::toCamelCase($item['status'])
-                );
-            }
+        $this->form->getField('author')->isFilled(BL::err('AuthorIsRequired'));
+        $this->form->getField('email')->isEmail(BL::err('EmailIsInvalid'));
+        $this->form->getField('text')->isFilled(BL::err('FieldIsRequired'));
+        if ($this->form->getField('website')->isFilled()) {
+            $this->form->getField('website')->isURL(BL::err('InvalidURL'));
         }
+
+        return $this->form->isCorrect();
     }
 }
