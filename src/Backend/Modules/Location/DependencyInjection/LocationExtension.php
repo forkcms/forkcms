30e8d3358a756d4c8125ad9a3e4a61e6a69ed0c3
stijnvrolijk <stijn@sumocoders.be>
2017-10-31 13:34:15 +0100

    Add some basic dependency injection to the location module

---
+++ src/Backend/Modules/Location/DependencyInjection/LocationExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Backend\Modules\Location\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class LocationExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
