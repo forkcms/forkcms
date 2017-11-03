e88d2d1498412ce96cbe3ecddda9f6082fa51484
Katrien Vanhaute <vanhautekatrien@gmail.com>
2017-11-03 13:56:06 +0100

    Add pagination for mobile

---
+++ src/Frontend/Themes/Bootstrap4/src/Js/Theme/Pagination.js
@@ -0,0 +1,29 @@
+export class Pagination {
+  events () {
+    $('[data-role="mobile-pagination"]').on('change', $.proxy(this.goToPage, this))
+  }
+
+  goToPage (event) {
+    let $current = $(event.currentTarget)
+    let nextPageId = $current.val()
+    let url = window.location.href.split('?')[0]
+
+    let parameters =  this.getParameters()
+
+    parameters["page"] = nextPageId
+    window.location.replace(url + '?' + decodeURIComponent($.param(parameters)))
+  }
+
+  getParameters () {
+    let queryString = window.location.href.split('?')[1]
+    let rawParameters = queryString.split('&')
+    let parameters = {}
+
+    $.each(rawParameters, (index, rawParameter) => {
+      let parameterPair = rawParameter.split('=')
+      parameters[parameterPair[0]] = parameterPair[1]
+    })
+
+    return parameters
+  }
+}
