d93d7c34459060bdcfc75a75d16d8be3e82562f1
Tijs Verkoyen <tijs@verkoyen.eu>
2017-11-09 17:29:12 +0100

    Comment entity
    Our Doctrine Entity that will replace the arrays we use.

---
+++ src/Backend/Modules/Blog/Domain/Comment.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace Backend\Modules\Blog\Domain;
+
+use Common\Locale;
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="blog_comments")
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ */
+class Comment
+{
+    /**
+     * @var int
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     */
+    private $postId;
+
+    /**
+     * @var Locale
+     * @ORM\Column(type="locale", name="language")
+     */
+    private $locale;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private $author;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private $email;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $website;
+
+    /**
+     * @var string
+     * @ORM\Column(type="text")
+     */
+    private $text;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", options={"default":"comment"})
+     */
+    private $type;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", options={"default":"moderation"})
+     */
+    private $status;
+
+    /**
+     * @var string
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $data;
+
+    /**
+     * @var DateTime
+     * @ORM\Column(type="datetime", name="created_on")
+     */
+    private $createdOn;
+
+    public function __construct(
+        int $postId,
+        Locale $locale,
+        string $author,
+        string $email,
+        string $website,
+        string $text,
+        string $type,
+        string $status,
+        string $data
+    ) {
+        $this->postId = $postId;
+        $this->locale = $locale;
+        $this->author = $author;
+        $this->email = $email;
+        $this->website = $website;
+        $this->text = $text;
+        $this->type = $type;
+        $this->status = $status;
+        $this->data = $data;
+    }
+
+    public function update(
+        int $postId,
+        Locale $locale,
+        string $author,
+        string $email,
+        string $website,
+        string $text,
+        string $type,
+        string $status,
+        string $data
+    ) {
+        $this->postId = $postId;
+        $this->locale = $locale;
+        $this->author = $author;
+        $this->email = $email;
+        $this->website = $website;
+        $this->text = $text;
+        $this->type = $type;
+        $this->status = $status;
+        $this->data = $data;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getPostId(): int
+    {
+        return $this->postId;
+    }
+
+    public function getLocale(): Locale
+    {
+        return $this->locale;
+    }
+
+    public function getAuthor(): string
+    {
+        return $this->author;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getWebsite(): string
+    {
+        return $this->website;
+    }
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function getData(): string
+    {
+        return $this->data;
+    }
+
+    public function getCreatedOn(): DateTime
+    {
+        return $this->createdOn;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function prePersist(): void
+    {
+        $this->createdOn = new DateTime();
+    }
+}
