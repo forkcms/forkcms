1944c1cf08120ecdd72f0bb089a754f7da02ce1b
Tijs Verkoyen <tijs@verkoyen.eu>
2017-11-14 16:15:19 +0100

    Simplified query that has to be build

--- src/Backend/Modules/Blog/Domain/Comment/CommentRepository.php
+++ src/Backend/Modules/Blog/Domain/Comment/CommentRepository.php
@@ -10,10 +10,8 @@ class CommentRepository extends EntityRepository
 {
     public function listCountPerStatus(Locale $locale): array
     {
-        $builder = $this->getEntityManager()
-                        ->createQueryBuilder()
+        $builder = $this->createQueryBuilder('c')
                         ->select('c.status, count(c.status) as number')
-                        ->from(Comment::class, 'c')
                         ->where("c.locale = :locale")
                         ->setParameter('locale', $locale)
                         ->groupBy('c.status');
