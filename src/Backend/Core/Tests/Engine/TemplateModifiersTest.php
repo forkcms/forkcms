443e2a7757683b2549938b8098e9b9b10e0ef866
Jeroen Desloovere <info@jeroendesloovere.be>
2017-12-01 10:52:18 +0100

    Removed method `testTruncate`, because it is obsolete now

--- src/Backend/Core/Tests/Engine/TemplateModifiersTest.php
+++ src/Backend/Core/Tests/Engine/TemplateModifiersTest.php
@@ -2,7 +2,6 @@
 
 namespace Backend\Core\Tests\Engine;
 
-use Backend\Core\Engine\Model as BackendModel;
 use Backend\Core\Engine\TemplateModifiers;
 use PHPUnit\Framework\TestCase;
 
@@ -23,59 +22,6 @@ class TemplateModifiersTest extends TestCase
         self::assertEquals(
             'Foo Bar',
             TemplateModifiers::stripNewlines("Foo\r\nBar")
-        );
-    }
-
-    public function testTruncate(): void
-    {
-        $containerMock = $this
-            ->getMockBuilder('Symfony\Component\DependencyInjection\ContainerInterface')
-            ->disableOriginalConstructor()
-            ->getMock()
-        ;
-
-        $containerMock->expects(self::any())
-            ->method('getParameter')
-            ->with('kernel.charset')
-            ->will(self::returnValue('UTF-8'))
-        ;
-
-        BackendModel::setContainer($containerMock);
-
-        self::assertEquals(
-            TemplateModifiers::truncate('foo bar baz qux', 3, false, true),
-            'foo'
-        );
-
-        self::assertEquals(
-            TemplateModifiers::truncate('foo bar baz qux', 4, false, true),
-            'foo'
-        );
-
-        self::assertEquals(
-            TemplateModifiers::truncate('foo bar baz qux', 8, false, true),
-            'foo bar'
-        );
-
-        self::assertEquals(
-            TemplateModifiers::truncate('foo bar baz qux', 100, false, true),
-            'foo bar baz qux'
-        );
-
-        // Hellip
-        self::assertEquals(
-            TemplateModifiers::truncate('foo bar baz qux', 5, true, true),
-            'foo…'
-        );
-
-        self::assertEquals(
-            TemplateModifiers::truncate('foo bar baz qux', 14, true, true),
-            'foo bar baz…'
-        );
-
-        self::assertEquals(
-            TemplateModifiers::truncate('foo bar baz qux', 15, true, true),
-            'foo bar baz qux'
         );
     }
 }
