dcc0f54ce69e137e12e7fa637ab67c46743d1ad0
Jeroen Desloovere <info@jeroendesloovere.be>
2017-12-07 13:41:26 +0100

    Fixed indentation

--- src/Backend/Modules/Analytics/Js/Analytics.js
+++ src/Backend/Modules/Analytics/Js/Analytics.js
@@ -24,20 +24,20 @@ jsBackend.analytics.charts =
         if (jsBackend.analytics.$chartPieChart.length > 0 || jsBackend.analytics.$chartDoubleMetricPerDay.length > 0)
         {
             Highcharts.setOptions(
+            {
+                colors: ['#2f77d1', '#021b45', '#ED561B', '#EDEF00', '#24CBE5', '#64E572', '#FF9655'],
+                title: { text: '' },
+                legend:
                 {
-                    colors: ['#2f77d1', '#021b45', '#ED561B', '#EDEF00', '#24CBE5', '#64E572', '#FF9655'],
-                    title: { text: '' },
-                    legend:
-                    {
-                        layout: 'vertical',
-                        borderWidth: 0,
-                        shadow: false,
-                        symbolPadding: 12,
-                        symbolWidth: 10,
-                        itemStyle: { cursor: 'pointer', color: '#000', lineHeight: '18px' },
-                        itemHoverStyle: { color: '#666' }
-                    }
-                });
+                    layout: 'vertical',
+                    borderWidth: 0,
+                    shadow: false,
+                    symbolPadding: 12,
+                    symbolWidth: 10,
+                    itemStyle: { cursor: 'pointer', color: '#000', lineHeight: '18px' },
+                    itemHoverStyle: { color: '#666' }
+                }
+            });
         }
     }
 };
@@ -64,44 +64,44 @@ jsBackend.analytics.chartPieChart =
             $this = $(this);
 
             pieChartData.push(
-                {
-                    'name': $this.children('span.label').html(),
-                    'y': parseInt($this.children('span.value').html()),
-                    'percentage': parseInt($this.children('span.percentage').html())
-                });
+            {
+                'name': $this.children('span.label').html(),
+                'y': parseInt($this.children('span.value').html()),
+                'percentage': parseInt($this.children('span.percentage').html())
+            });
         });
 
         var containerWidth = jsBackend.analytics.$chartPieChart.width();
 
         jsBackend.analytics.$chartPieChart.chart = new Highcharts.Chart(
+        {
+            chart: { renderTo: 'chartPieChart', height: 200, width: containerWidth, margin: [0, 160, 0, 0], backgroundColor:'transparent' },
+            credits: { enabled: false },
+            plotArea: { shadow: null, borderWidth: null, backgroundColor: null },
+            tooltip:
             {
-                chart: { renderTo: 'chartPieChart', height: 200, width: containerWidth, margin: [0, 160, 0, 0], backgroundColor:'transparent' },
-                credits: { enabled: false },
-                plotArea: { shadow: null, borderWidth: null, backgroundColor: null },
-                tooltip:
+                formatter: function()
                 {
-                    formatter: function()
+                    var percentage = String(this.point.percentage);
+                    return '<b>'+ this.point.name +'</b>: '+ this.y + ' (' + percentage.substring(0, $.inArray('.', percentage) + 3) + '%)';
+                },
+                borderWidth: 2, shadow: false
+            },
+            plotOptions:
+            {
+                pie:
+                {
+                    allowPointSelect: true,
+                    dataLabels:
                     {
-                        var percentage = String(this.point.percentage);
-                        return '<b>'+ this.point.name +'</b>: '+ this.y + ' (' + percentage.substring(0, $.inArray('.', percentage) + 3) + '%)';
+                        enabled: false
                     },
-                    borderWidth: 2, shadow: false
-                },
-                plotOptions:
-                {
-                    pie:
-                    {
-                        allowPointSelect: true,
-                        dataLabels:
-                        {
-                            enabled: false
-                        },
-                        showInLegend: true
-                    }
-                },
-                legend: { align: 'right' },
-                series: [ {type: 'pie', data: pieChartData } ]
-            });
+                    showInLegend: true
+                }
+            },
+            legend: { align: 'right' },
+            series: [ {type: 'pie', data: pieChartData } ]
+        });
     },
 
     // destroy chart
@@ -166,22 +166,22 @@ jsBackend.analytics.chartDoubleMetricPerDay =
         var containerWidth = $('#chartDoubleMetricPerDay').width();
 
         jsBackend.analytics.$chartDoubleMetricPerDay.chart = new Highcharts.Chart(
+        {
+            chart: { renderTo: 'chartDoubleMetricPerDay', height: 200, width: containerWidth, margin: [60, 0, 30, 40], defaultSeriesType: 'line', backgroundColor:'transparent' },
+            xAxis: { lineColor: '#CCC', lineWidth: 1, categories: xAxisCategories, color: '#000' },
+            yAxis: { min: 0, max: maxValue, tickInterval: tickInterval, title: { text: '' } },
+            credits: { enabled: false },
+            tooltip: { formatter: function() { return '<b>'+ this.series.name +'</b><br/>'+ xAxisValues[this.point.x] +': '+ this.y; } },
+            plotOptions:
             {
-                chart: { renderTo: 'chartDoubleMetricPerDay', height: 200, width: containerWidth, margin: [60, 0, 30, 40], defaultSeriesType: 'line', backgroundColor:'transparent' },
-                xAxis: { lineColor: '#CCC', lineWidth: 1, categories: xAxisCategories, color: '#000' },
-                yAxis: { min: 0, max: maxValue, tickInterval: tickInterval, title: { text: '' } },
-                credits: { enabled: false },
-                tooltip: { formatter: function() { return '<b>'+ this.series.name +'</b><br/>'+ xAxisValues[this.point.x] +': '+ this.y; } },
-                plotOptions:
-                {
-                    line: { marker: { enabled: false, states: { hover: { enabled: true, symbol: 'circle', radius: 5, lineWidth: 1 } } } },
-                    area: { marker: { enabled: false, states: { hover: { enabled: true, symbol: 'circle', radius: 5, lineWidth: 1 } } } },
-                    column: { pointPadding: 0.2, borderWidth: 0 },
-                    series: { fillOpacity: 0.3 }
-                },
-                series: [{name: metric1Name, data: metric1Data, type: 'area' }, { name: metric2Name, data: metric2Data }],
-                legend: { layout: 'horizontal', verticalAlign: 'top' }
-            });
+                line: { marker: { enabled: false, states: { hover: { enabled: true, symbol: 'circle', radius: 5, lineWidth: 1 } } } },
+                area: { marker: { enabled: false, states: { hover: { enabled: true, symbol: 'circle', radius: 5, lineWidth: 1 } } } },
+                column: { pointPadding: 0.2, borderWidth: 0 },
+                series: { fillOpacity: 0.3 }
+            },
+            series: [{name: metric1Name, data: metric1Data, type: 'area' }, { name: metric2Name, data: metric2Data }],
+            legend: { layout: 'horizontal', verticalAlign: 'top' }
+        });
     },
 
     // destroy chart
