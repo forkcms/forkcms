cd8ce2bf05b4297322f5aca5835fc2f84d0bb439
Jeroen Desloovere <info@jeroendesloovere.be>
2018-01-31 16:16:44 +0100

    Reverting Pages\Engine\Model copy method

--- src/Backend/Modules/Pages/Engine/Model.php
+++ src/Backend/Modules/Pages/Engine/Model.php
@@ -2,12 +2,17 @@
 
 namespace Backend\Modules\Pages\Engine;
 
+use Backend\Modules\ContentBlocks\Domain\ContentBlock\Command\CopyContentBlocksToOtherLocale;
+use Backend\Modules\Location\Command\CopyLocationWidgetsToOtherLocale;
+use SimpleBus\Message\Bus\MessageBus;
 use InvalidArgumentException;
 use Symfony\Component\Filesystem\Filesystem;
 use Backend\Core\Engine\Authentication as BackendAuthentication;
 use Backend\Core\Language\Language as BL;
 use Backend\Core\Engine\Model as BackendModel;
+use Backend\Core\Language\Locale;
 use Backend\Modules\Extensions\Engine\Model as BackendExtensionsModel;
+use Backend\Modules\Search\Engine\Model as BackendSearchModel;
 use Backend\Modules\Tags\Engine\Model as BackendTagsModel;
 use ForkCMS\App\ForkController;
 use Frontend\Core\Language\Language as FrontendLanguage;
@@ -79,6 +84,207 @@ class Model
     {
         $cacheBuilder = static::getCacheBuilder();
         $cacheBuilder->buildCache($language ?? BL::getWorkingLanguage());
+    }
+
+    public static function copy(string $fromLanguage, string $toLanguage): void
+    {
+        // get database
+        $database = BackendModel::getContainer()->get('database');
+
+        /** @var MessageBus $commanBus */
+        $commandBus = BackendModel::get('command_bus');
+
+        $toLocale = Locale::fromString($toLanguage);
+        $fromLocale = Locale::fromString($fromLanguage);
+
+        // copy contentBlocks and get copied contentBlockIds
+        $copyContentBlocks = new CopyContentBlocksToOtherLocale($toLocale, $fromLocale);
+        $commandBus->handle($copyContentBlocks);
+        $contentBlockIds = $copyContentBlocks->extraIdMap;
+
+        // define old block ids
+        $contentBlockOldIds = array_keys($contentBlockIds);
+
+        // copy location widgets and get copied widget ids
+        $copyLocationWidgets = new CopyLocationWidgetsToOtherLocale($toLocale, $fromLocale);
+        $commandBus->handle($copyLocationWidgets);
+        $locationWidgetIds = $copyLocationWidgets->extraIdMap;
+
+        // define old block ids
+        $locationWidgetOldIds = array_keys($locationWidgetIds);
+
+        // get all old pages
+        $ids = $database->getColumn(
+            'SELECT id
+             FROM pages AS i
+             WHERE i.language = ? AND i.status = ?',
+            [$toLanguage, 'active']
+        );
+
+        // any old pages
+        if (!empty($ids)) {
+            // delete existing pages
+            foreach ($ids as $id) {
+                // redefine
+                $id = (int) $id;
+
+                // get revision ids
+                $revisionIDs = (array) $database->getColumn(
+                    'SELECT i.revision_id
+                     FROM pages AS i
+                     WHERE i.id = ? AND i.language = ?',
+                    [$id, $toLanguage]
+                );
+
+                // get meta ids
+                $metaIDs = (array) $database->getColumn(
+                    'SELECT i.meta_id
+                     FROM pages AS i
+                     WHERE i.id = ? AND i.language = ?',
+                    [$id, $toLanguage]
+                );
+
+                // delete meta records
+                if (!empty($metaIDs)) {
+                    $database->delete('meta', 'id IN (' . implode(',', $metaIDs) . ')');
+                }
+
+                // delete blocks and their revisions
+                if (!empty($revisionIDs)) {
+                    $database->delete(
+                        'pages_blocks',
+                        'revision_id IN (' . implode(',', $revisionIDs) . ')'
+                    );
+                }
+
+                // delete page and the revisions
+                if (!empty($revisionIDs)) {
+                    $database->delete('pages', 'revision_id IN (' . implode(',', $revisionIDs) . ')');
+                }
+            }
+        }
+
+        // delete search indexes
+        $database->delete('search_index', 'module = ? AND language = ?', ['pages', $toLanguage]);
+
+        // get all active pages
+        $ids = BackendModel::getContainer()->get('database')->getColumn(
+            'SELECT id
+             FROM pages AS i
+             WHERE i.language = ? AND i.status = ?',
+            [$fromLanguage, 'active']
+        );
+
+        // loop
+        foreach ($ids as $id) {
+            // get data
+            $sourceData = self::get($id, null, $fromLanguage);
+
+            // get and build meta
+            $meta = $database->getRecord(
+                'SELECT *
+                 FROM meta
+                 WHERE id = ?',
+                [$sourceData['meta_id']]
+            );
+
+            // remove id
+            unset($meta['id']);
+
+            // init page
+            $page = [];
+
+            // build page
+            $page['id'] = $sourceData['id'];
+            $page['user_id'] = BackendAuthentication::getUser()->getUserId();
+            $page['parent_id'] = $sourceData['parent_id'];
+            $page['template_id'] = $sourceData['template_id'];
+            $page['meta_id'] = (int) $database->insert('meta', $meta);
+            $page['language'] = $toLanguage;
+            $page['type'] = $sourceData['type'];
+            $page['title'] = $sourceData['title'];
+            $page['navigation_title'] = $sourceData['navigation_title'];
+            $page['navigation_title_overwrite'] = $sourceData['navigation_title_overwrite'];
+            $page['hidden'] = $sourceData['hidden'];
+            $page['status'] = 'active';
+            $page['publish_on'] = BackendModel::getUTCDate();
+            $page['created_on'] = BackendModel::getUTCDate();
+            $page['edited_on'] = BackendModel::getUTCDate();
+            $page['allow_move'] = $sourceData['allow_move'];
+            $page['allow_children'] = $sourceData['allow_children'];
+            $page['allow_edit'] = $sourceData['allow_edit'];
+            $page['allow_delete'] = $sourceData['allow_delete'];
+            $page['sequence'] = $sourceData['sequence'];
+            $page['data'] = ($sourceData['data'] !== null) ? serialize($sourceData['data']) : null;
+
+            // insert page, store the id, we need it when building the blocks
+            $revisionId = self::insert($page);
+
+            $blocks = [];
+
+            // get the blocks
+            $sourceBlocks = self::getBlocks($id, null, $fromLanguage);
+
+            // loop blocks
+            foreach ($sourceBlocks as $sourceBlock) {
+                // build block
+                $block = $sourceBlock;
+                $block['revision_id'] = $revisionId;
+                $block['created_on'] = BackendModel::getUTCDate();
+                $block['edited_on'] = BackendModel::getUTCDate();
+
+                // Overwrite the extra_id of the old content block with the id of the new one
+                if (in_array($block['extra_id'], $contentBlockOldIds)) {
+                    $block['extra_id'] = $contentBlockIds[$block['extra_id']];
+                }
+
+                // Overwrite the extra_id of the old location widget with the id of the new one
+                if (in_array($block['extra_id'], $locationWidgetOldIds)) {
+                    $block['extra_id'] = $locationWidgetIds[$block['extra_id']];
+                }
+
+                // add block
+                $blocks[] = $block;
+            }
+
+            // insert the blocks
+            self::insertBlocks($blocks);
+
+            $text = '';
+
+            // build search-text
+            foreach ($blocks as $block) {
+                $text .= ' ' . $block['html'];
+            }
+
+            // add
+            BackendSearchModel::saveIndex(
+                'Pages',
+                (int) $page['id'],
+                ['title' => $page['title'], 'text' => $text],
+                $toLanguage
+            );
+
+            // get tags
+            $tags = BackendTagsModel::getTags('pages', $id, 'string', $fromLanguage);
+
+            // save tags
+            if ($tags != '') {
+                $saveWorkingLanguage = BL::getWorkingLanguage();
+
+                // If we don't set the working language to the target language,
+                // BackendTagsModel::getUrl() will use the current working
+                // language, possibly causing unnecessary '-2' suffixes in
+                // tags.url
+                BL::setWorkingLanguage($toLanguage);
+
+                BackendTagsModel::saveTags($page['id'], $tags, 'pages', $toLanguage);
+                BL::setWorkingLanguage($saveWorkingLanguage);
+            }
+        }
+
+        // build cache
+        self::buildCache($toLanguage);
     }
 
     public static function createHtml(
@@ -627,12 +833,12 @@ class Model
 
                 // insert link
                 $html .= '    <a href="' .
-                         BackendModel::createUrlForAction(
-                             'Edit',
-                             null,
-                             null,
-                             ['id' => $page['page_id']]
-                         ) . '"><ins>&#160;</ins>' . $page['navigation_title'] . '</a>' . "\n";
+                    BackendModel::createUrlForAction(
+                        'Edit',
+                        null,
+                        null,
+                        ['id' => $page['page_id']]
+                    ) . '"><ins>&#160;</ins>' . $page['navigation_title'] . '</a>' . "\n";
 
                 // get childs
                 $html .= self::getSubtree($navigation, $page['page_id']);
@@ -725,12 +931,12 @@ class Model
         // create homepage anchor from title
         $homePage = self::get(BackendModel::HOME_PAGE_ID);
         $html .= '            <a href="' .
-                 BackendModel::createUrlForAction(
-                     'Edit',
-                     null,
-                     null,
-                     ['id' => BackendModel::HOME_PAGE_ID]
-                 ) . '"><ins>&#160;</ins>' . $homePage['title'] . '</a>' . "\n";
+            BackendModel::createUrlForAction(
+                'Edit',
+                null,
+                null,
+                ['id' => BackendModel::HOME_PAGE_ID]
+            ) . '"><ins>&#160;</ins>' . $homePage['title'] . '</a>' . "\n";
 
         // add subpages
         $html .= self::getSubtree($navigation, BackendModel::HOME_PAGE_ID);
@@ -756,12 +962,12 @@ class Model
 
                     // insert link
                     $html .= '            <a href="' .
-                             BackendModel::createUrlForAction(
-                                 'Edit',
-                                 null,
-                                 null,
-                                 ['id' => $page['page_id']]
-                             ) . '"><ins>&#160;</ins>' . $page['navigation_title'] . '</a>' . "\n";
+                        BackendModel::createUrlForAction(
+                            'Edit',
+                            null,
+                            null,
+                            ['id' => $page['page_id']]
+                        ) . '"><ins>&#160;</ins>' . $page['navigation_title'] . '</a>' . "\n";
 
                     // insert subtree
                     $html .= self::getSubtree($navigation, $page['page_id']);
@@ -792,12 +998,12 @@ class Model
 
                 // insert link
                 $html .= '            <a href="' .
-                         BackendModel::createUrlForAction(
-                             'Edit',
-                             null,
-                             null,
-                             ['id' => $page['page_id']]
-                         ) . '"><ins>&#160;</ins>' . $page['navigation_title'] . '</a>' . "\n";
+                    BackendModel::createUrlForAction(
+                        'Edit',
+                        null,
+                        null,
+                        ['id' => $page['page_id']]
+                    ) . '"><ins>&#160;</ins>' . $page['navigation_title'] . '</a>' . "\n";
 
                 // insert subtree
                 $html .= self::getSubtree($navigation, $page['page_id']);
@@ -827,12 +1033,12 @@ class Model
 
                 // insert link
                 $html .= '            <a href="' .
-                         BackendModel::createUrlForAction(
-                             'Edit',
-                             null,
-                             null,
-                             ['id' => $page['page_id']]
-                         ) . '"><ins>&#160;</ins>' . $page['navigation_title'] . '</a>' . "\n";
+                    BackendModel::createUrlForAction(
+                        'Edit',
+                        null,
+                        null,
+                        ['id' => $page['page_id']]
+                    ) . '"><ins>&#160;</ins>' . $page['navigation_title'] . '</a>' . "\n";
 
                 // insert subtree
                 $html .= self::getSubtree($navigation, $page['page_id']);
@@ -1303,11 +1509,11 @@ class Model
         // calculate new sequence for items that should be moved inside
         if ($typeOfDrop === self::TYPE_OF_DROP_INSIDE) {
             $newSequence = (int) $database->getVar(
-                'SELECT MAX(i.sequence)
+                    'SELECT MAX(i.sequence)
                  FROM pages AS i
                  WHERE i.id = ? AND i.language = ? AND i.status = ?',
-                [$newParent, $language, 'active']
-            ) + 1;
+                    [$newParent, $language, 'active']
+                ) + 1;
 
             $database->update(
                 'pages',
