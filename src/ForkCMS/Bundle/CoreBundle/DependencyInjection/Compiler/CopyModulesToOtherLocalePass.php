6b24e3e0d83d490ba2c4a4288c0783796582bc2e
Jeroen Desloovere <info@jeroendesloovere.be>
2018-02-19 13:30:29 +0100

    Fix CopyModulesToOtherLocalePass

--- src/ForkCMS/Bundle/CoreBundle/DependencyInjection/Compiler/CopyModulesToOtherLocalePass.php
+++ src/ForkCMS/Bundle/CoreBundle/DependencyInjection/Compiler/CopyModulesToOtherLocalePass.php
@@ -12,11 +12,11 @@ class CopyModulesToOtherLocalePass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         // always first check if the primary service is defined
-        if (!$container->has('fork.copy_modules_to_other_locale')) {
+        if (!$container->has('fork.manager.copy_modules_to_other_locale')) {
             return;
         }
 
-        $definition = $container->findDefinition('fork.copy_modules_to_other_locale');
+        $definition = $container->findDefinition('fork.manager.copy_modules_to_other_locale');
 
         // find all service IDs with the fork.copy_module_to_other_locale tag
         $taggedServices = $container->findTaggedServiceIds('fork.copy_module_to_other_locale');
