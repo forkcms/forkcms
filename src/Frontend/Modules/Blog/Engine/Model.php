2c0e1c041f83f6b27f45e1da1ad0af4f38fb43f0
Jelmer Prins <jelmer@sumocoders.be>
2018-02-22 11:13:19 +0100

    Add the tags to the blogpost in the frontend in the model instead of the action
    That way it will also be available in the widgets

--- src/Frontend/Modules/Blog/Engine/Model.php
+++ src/Frontend/Modules/Blog/Engine/Model.php
@@ -762,6 +762,14 @@ class Model implements FrontendTagsInterface
             }
         }
 
+        if (isset($blogPost['id'])) {
+            $blogPost['tags'] = FrontendTagsModel::getForItem(
+                'Blog',
+                $blogPost['id'],
+                isset($blogPost['language']) ? Locale::fromString($blogPost['language']) : null
+            );
+        }
+
         return $blogPost;
     }
 
