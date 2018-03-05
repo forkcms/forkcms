0654b3b075e74fbbaa5a41f68836de1f48d08ac5
StijnVrolijk <stijn.o.vrolijk@gmail.com>
2018-03-05 10:28:03 +0100

    Test updating a profile

--- src/Backend/Modules/Profiles/Tests/Engine/ModelTest.php
+++ src/Backend/Modules/Profiles/Tests/Engine/ModelTest.php
@@ -67,9 +67,29 @@ final class ModelTest extends WebTestCase
         $this->assertTrue(Model::existsDisplayName($this->getDisplayName()));
     }
 
+    public function testUpdatingProfile(): void
+    {
+        $profileId = $this->addProfile();
+        $this->assertEquals(1, $this->updateProfile());
+
+        $updatedProfileData = $this->getUpdatedProfileData();
+        $updatedProfile = Model::get($profileId);
+
+        $this->assertEquals($profileId, $updatedProfile['id']);
+        $this->assertEquals($updatedProfileData['email'], $updatedProfile['email']);
+        $this->assertEquals($updatedProfileData['status'], $updatedProfile['status']);
+        $this->assertEquals($updatedProfileData['display_name'], $updatedProfile['display_name']);
+        $this->assertEquals($updatedProfileData['url'], $updatedProfile['url']);
+    }
+
     public function addProfile(): int
     {
         return Model::insert($this->getProfileData());
+    }
+
+    public function updateProfile(): int
+    {
+        return Model::update(1, $this->getUpdatedProfileData());
     }
 
     public function getPassword(): string
@@ -92,6 +112,19 @@ final class ModelTest extends WebTestCase
             'url' => 'fork-cms',
             'registered_on' => '2018-03-05 09:45:12',
             'last_login' => '1970-01-01 00:00:00',
+        ];
+    }
+
+    public function getUpdatedProfileData(): array
+    {
+        return [
+            'email' => 'test2@fork-cms.com',
+            'password' => '$2y$10$1Ev9QQNYZBjdU1ELKjKNqelcV.j2l3CgtVkHl0aMvbNpg1g73S5lC',
+            'status' => 'archived',
+            'display_name' => 'Fork CMS 2',
+            'url' => 'fork-cms-2',
+            'registered_on' => '2018-03-05 10:22:34',
+            'last_login' => '2018-03-05 10:16:19',
         ];
     }
 }
