18b278c862045112474f9bef36a81eb8679facdc
StijnVrolijk <stijn.o.vrolijk@gmail.com>
2018-03-07 09:57:03 +0100

    Use data fixtures instead of adding data for each test

--- src/Frontend/Modules/Profiles/Tests/Engine/AuthenticationTest.php
+++ src/Frontend/Modules/Profiles/Tests/Engine/AuthenticationTest.php
@@ -3,9 +3,9 @@
 namespace Frontend\Modules\Profiles\Tests\Engine;
 
 use Common\WebTestCase;
-use DateTime;
 use Frontend\Core\Engine\Model as FrontendModel;
 use Frontend\Modules\Profiles\Engine\Authentication;
+use Frontend\Modules\Profiles\Tests\DataFixtures\LoadProfiles;
 use SpoonDatabase;
 
 final class AuthenticationTest extends WebTestCase
@@ -22,34 +22,13 @@ final class AuthenticationTest extends WebTestCase
         }
 
         $client = self::createClient();
-        $this->loadFixtures($client);
+        $this->loadFixtures($client, [LoadProfiles::class]);
 
         $this->database = FrontendModel::get('database');
     }
 
     public function testOldSessionCleanUp()
     {
-        $dateWithinAMonthAgo = new DateTime('-1 week');
-        $dateOverAMonthAgo = new DateTime('-2 months');
-
-        $this->database->insert(
-            'profiles_sessions',
-            [
-                [
-                    'session_id' => '0123456789',
-                    'profile_id' => 1,
-                    'secret_key' => 'NotSoSecretNowIsIt',
-                    'date' => $dateWithinAMonthAgo->format('Y-m-d H:i:s'),
-                ],
-                [
-                    'session_id' => '1234567890',
-                    'profile_id' => 1,
-                    'secret_key' => 'WeNeedToTalk',
-                    'date' => $dateOverAMonthAgo->format('Y-m-d H:i:s'),
-                ],
-            ]
-        );
-
         $this->assertEquals('2', $this->database->getVar('SELECT COUNT(session_id) FROM profiles_sessions'));
 
         Authentication::cleanupOldSessions();
