0aa8b89294a8c7d0b7582cd61fc0e7417601351c
StijnVrolijk <stijn.o.vrolijk@gmail.com>
2018-03-08 16:53:41 +0100

    Test the logout method

--- src/Frontend/Modules/Profiles/Tests/Engine/AuthenticationTest.php
+++ src/Frontend/Modules/Profiles/Tests/Engine/AuthenticationTest.php
@@ -7,11 +7,17 @@ use Frontend\Core\Engine\Model as FrontendModel;
 use Frontend\Modules\Profiles\Engine\Authentication;
 use Frontend\Modules\Profiles\Tests\DataFixtures\LoadProfiles;
 use SpoonDatabase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 final class AuthenticationTest extends WebTestCase
 {
     /** @var SpoonDatabase */
     private $database;
+
+    /** @var Session */
+    private $session;
 
     public function setUp(): void
     {
@@ -25,6 +31,15 @@ final class AuthenticationTest extends WebTestCase
         $this->loadFixtures($client, [LoadProfiles::class]);
 
         $this->database = FrontendModel::get('database');
+        $this->session = FrontendModel::getSession();
+
+        // Create a request stack for cookie stuff
+        $requestStack = new RequestStack();
+        $request = new Request();
+        $request->setSession($this->session);
+        $request->cookies->set('frontend_profile_secret_key', 'NotSoSecret');
+        $requestStack->push($request);
+        FrontendModel::getContainer()->set('request_stack', $requestStack);
     }
 
     public function testOldSessionCleanUp()
@@ -122,5 +137,57 @@ final class AuthenticationTest extends WebTestCase
         $newLastLogin = $this->database->getVar('SELECT last_login FROM profiles WHERE id = 1');
 
         $this->assertLessThan($newLastLogin, $initalLastLogin);
+    }
+
+    public function testLogoutDeletesSessionFromDatabase(): void
+    {
+        $this->database->insert(
+            'profiles_sessions',
+            [
+                'session_id' => $this->session->getId(),
+                'profile_id' => 1,
+                'secret_key' => 'Fork is da bomb',
+                'date' => '1970-01-01 00:00:00',
+            ]
+        );
+
+        $this->assertTrue(
+            (bool) $this->database->getVar(
+                'SELECT 1 FROM profiles_sessions WHERE session_id = ?',
+                $this->session->getId()
+            )
+        );
+
+        Authentication::logout();
+
+        $this->assertFalse(
+            (bool) $this->database->getVar(
+                'SELECT 1 FROM profiles_sessions WHERE session_id = ?',
+                $this->session->getId()
+            )
+        );
+    }
+
+    public function testLogoutSetsLoggedInSessionToFalse(): void
+    {
+        $this->session->set('frontend_profile_logged_in', true);
+        $this->assertTrue($this->session->get('frontend_profile_logged_in'));
+
+        Authentication::logout();
+
+        $this->assertFalse($this->session->get('frontend_profile_logged_in'));
+    }
+
+    public function testLogoutDeletesSecretKeyCookie(): void
+    {
+        $cookie = FrontendModel::getContainer()->get('fork.cookie');
+
+        $this->assertTrue($cookie->has('frontend_profile_secret_key'));
+        $this->assertEquals('NotSoSecret', $cookie->get('frontend_profile_secret_key'));
+
+        Authentication::logout();
+
+        $this->assertFalse($cookie->has('frontend_profile_secret_key'));
+        $this->assertNotEquals('NotSoSecret', $cookie->get('frontend_profile_secret_key'));
     }
 }
