189657df81af49d09ca38c23cbca93d4226f8c4a
Jeroen Desloovere <info@jeroendesloovere.be>
2018-04-04 17:03:32 +0200

    Add functional test for Thumbnails

---
+++ tests/Utility/ThumbnailsTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace ForkCMS\Tests\Utility\Thumbnails;
+
+use ForkCMS\Utility\Thumbnails;
+use PHPUnit\Framework\TestCase;
+
+class ThumbnailsTest extends TestCase
+{
+    private const LANDSCAPE = '1000x500';
+    private const PORTRAIT = '500x1000';
+    private const SOURCE = 'source';
+    private const SQUARE = '500x500';
+
+    /** @var array  */
+    private $folders = [];
+
+    /** @var string  */
+    private $filename;
+
+    /** @var string  */
+    private $rootPath;
+
+    /** @var Thumbnails */
+    private $thumbnails;
+
+    private function createFolders()
+    {
+        foreach ($this->folders as $folder) {
+            mkdir($folder);
+        }
+    }
+
+    private function removeFolders()
+    {
+        foreach ($this->folders as $folder) {
+            rmdir($folder);
+        }
+    }
+
+    private function removeFiles()
+    {
+        unlink($this->rootPath . '/' . self::SOURCE . '/' . $this->filename);
+    }
+
+    /**
+     * Put your temp image into your filesystem.
+     * (Not good as unit tests must work with any system,
+     * but my research about mocking resources gave me nothing)
+     */
+    protected function setUp()
+    {
+        // Init variables
+        $sitePathWWW = dirname(__FILE__) . '/..';
+        $this->rootPath = realpath($sitePathWWW . '/assets');
+        $this->filename = 'test_image.jpg';
+        $this->thumbnails = new Thumbnails($sitePathWWW);
+
+        $this->folders = [
+            $this->rootPath . '/' . self::SOURCE,
+            $this->rootPath . '/' . self::SQUARE,
+            $this->rootPath . '/' . self::PORTRAIT,
+            $this->rootPath . '/' . self::LANDSCAPE,
+        ];
+
+        $this->createFolders();
+
+        // Copy our file to /source folder
+        copy(
+            $this->rootPath . '/' . $this->filename,
+            $this->rootPath . '/' . self::SOURCE . '/' . $this->filename
+        );
+    }
+
+    protected function tearDown()
+    {
+        $this->removeFiles();
+        $this->removeFolders();
+    }
+
+    /**
+     * We have one big test method, because setUp and tearDown are executed for every test method.
+     */
+    public function testClassMethods()
+    {
+        // Test thumbnail folders
+        $this->assertEquals(
+            [
+                [
+                    'dirname' => self::LANDSCAPE,
+                    'path' => $this->rootPath . '/' . self::LANDSCAPE,
+                    'url' => '/assets',
+                    'width' => (int) explode('x', self::LANDSCAPE)[0],
+                    'height' => (int) explode('x', self::LANDSCAPE)[1],
+                ],
+                [
+                    'dirname' => self::PORTRAIT,
+                    'path' => $this->rootPath . '/' . self::PORTRAIT,
+                    'url' => '/assets',
+                    'width' => (int) explode('x', self::PORTRAIT)[0],
+                    'height' => (int) explode('x', self::PORTRAIT)[1],
+                ],
+                [
+                    'dirname' => self::SQUARE,
+                    'path' => $this->rootPath . '/' . self::SQUARE,
+                    'url' => '/assets',
+                    'width' => (int) explode('x', self::SQUARE)[0],
+                    'height' => (int) explode('x', self::SQUARE)[0],
+                ]
+            ],
+            $this->thumbnails->getFolders($this->rootPath)
+        );
+
+        // Test thumbnail generating
+        $this->thumbnails->generate($this->rootPath, $this->rootPath . '/' . self::SOURCE . '/' . $this->filename);
+        $this->assertTrue(is_file($this->rootPath . '/' . self::SQUARE . '/' . $this->filename));
+
+        // Test thumbnail deleting
+        $this->thumbnails->delete($this->rootPath, $this->filename);
+        $this->assertFalse(file_exists($this->rootPath . '/' . self::LANDSCAPE . '/' . $this->filename));
+        $this->assertFalse(file_exists($this->rootPath . '/' . self::PORTRAIT . '/' . $this->filename));
+        $this->assertFalse(file_exists($this->rootPath . '/' . self::SQUARE . '/' . $this->filename));
+        $this->assertTrue(file_exists($this->rootPath . '/' . self::SOURCE . '/' . $this->filename));
+    }
+}
