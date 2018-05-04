e7df6fdf27dfa5385559c96564210c0eb9b97623
Jeroen Desloovere <info@jeroendesloovere.be>
2018-05-04 16:56:56 +0200

    Add CurlPost to Google Recaptcha
    Add CurlPost to Google Recaptcha
    
    We add this because otherwise "allow_url_fopen" must be enabled, otherwise a "invalid-json" error will be thrown by Google.
    Using CurlPost, it does not listen to "allow_url_fopen" and it always works.

--- src/Frontend/Modules/FormBuilder/Widgets/Form.php
+++ src/Frontend/Modules/FormBuilder/Widgets/Form.php
@@ -12,6 +12,7 @@ use Frontend\Modules\FormBuilder\Engine\Model as FrontendFormBuilderModel;
 use Frontend\Modules\FormBuilder\FormBuilderEvents;
 use Frontend\Modules\FormBuilder\Event\FormBuilderSubmittedEvent;
 use ReCaptcha\ReCaptcha;
+use ReCaptcha\RequestMethod\CurlPost;
 use SpoonFormAttributes;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
@@ -411,7 +412,7 @@ class Form extends FrontendBaseWidget
                     $this->form->addError(FL::err('RecaptchaInvalid'));
                 }
 
-                $recaptcha = new ReCaptcha($secret);
+                $recaptcha = new ReCaptcha($secret, new CurlPost());
 
                 $response = $recaptcha->verify($response);
 
