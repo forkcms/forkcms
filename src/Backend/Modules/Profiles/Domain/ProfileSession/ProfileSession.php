eee32be67a9be46a378d14b23d30d08e695877b4
Jonas De Keukelaere <jonas@sumocoders.be>
2018-05-17 11:43:41 +0100

    Add Profile Session entity

---
+++ src/Backend/Modules/Profiles/Domain/ProfileSession/ProfileSession.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Backend\Modules\Profiles\Domain\ProfileSession;
+
+use Backend\Modules\Profiles\Domain\Profile\Profile;
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ */
+class ProfileSession
+{
+    /**
+     * @var string
+     *
+     * @ORM\Id
+     * @ORM\Column(type="string", length=50)
+     */
+    private $sessionId;
+
+    /**
+     * @var Profile
+     *
+     * @ORM\Id
+     * @ORM\ManyToOne(targetEntity="Backend\Modules\Profiles\Domain\Profile\Profile")
+     */
+    private $profile;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $secretKey;
+
+    /**
+     * @var DateTime
+     *
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    public function __construct(string $sessionId, Profile $profile, string $secretKey)
+    {
+        $this->sessionId = $sessionId;
+        $this->profile = $profile;
+        $this->secretKey = $secretKey;
+    }
+
+    public function getSessionId(): string
+    {
+        return $this->sessionId;
+    }
+
+    public function getProfile(): Profile
+    {
+        return $this->profile;
+    }
+
+    public function getSecretKey(): string
+    {
+        return $this->secretKey;
+    }
+
+    public function getDate(): DateTime
+    {
+        return $this->date;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function prePersist(): void
+    {
+        $this->date = new DateTime();
+    }
+}
