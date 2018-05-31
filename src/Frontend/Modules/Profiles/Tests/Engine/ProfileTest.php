0ce05e041470cf559497a07c8d0ec80569e58e1a
Jonas De Keukelaere <jonas@sumocoders.be>
2018-05-31 09:13:13 +0100

    Fix frontend profile tests

--- src/Frontend/Modules/Profiles/Tests/Engine/ProfileTest.php
+++ src/Frontend/Modules/Profiles/Tests/Engine/ProfileTest.php
@@ -2,9 +2,10 @@
 
 namespace Frontend\Modules\Profiles\Tests\Engine;
 
+use Backend\Modules\Profiles\Domain\Profile\Profile;
+use Backend\Modules\Profiles\Domain\Profile\Status;
 use Frontend\Modules\Profiles\Engine\Model;
 use Common\WebTestCase;
-use Frontend\Modules\Profiles\Engine\Profile;
 
 final class ProfileTest extends WebTestCase
 {
@@ -26,58 +27,16 @@ final class ProfileTest extends WebTestCase
 
     public function testCreatingProfile(): void
     {
-        $profile = new Profile();
+        $profile = new Profile(
+            'info@fork-cms.com',
+            'forkcms',
+            Status::active(),
+            'Fork CMS',
+            'fork-cms'
+        );
 
-        $profile->setDisplayName('Fork CMS');
         $this->assertEquals('Fork CMS', $profile->getDisplayName());
-
-        $profile->setEmail('info@fork-cms.com');
         $this->assertEquals('info@fork-cms.com', $profile->getEmail());
-
-        $profile->setRegisteredOn(1234567890);
-        $this->assertEquals(1234567890, $profile->getRegisteredOn());
-
-        $profile->setStatus('random_status');
-        $this->assertEquals('random_status', $profile->getStatus());
-
-        $profile->setUrl('fork-cms');
-        $this->assertEquals('fork-cms', $profile->getUrl());
-
-        // @TODO These settings setters don't work because the profile doesn't have an ID, this should be fixed
-        /*$profile->setSettings(
-            [
-                'my_first_setting' => 'My first value',
-                'my_second_setting' => 'My second value',
-            ]
-        );
-        $this->assertEquals(
-            [
-                'my_first_setting' => 'My first value',
-                'my_second_setting' => 'My second value',
-            ],
-            $profile->getSettings()
-        );
-        $this->assertEquals('My first value', $profile->getSetting('my_first_setting'));
-
-        $profile->setSetting('my_second_setting', 'My updated value');
-        $this->assertEquals('My updated value', $profile->getSetting('my_second_setting'));
-
-        $profileArray = $profile->toArray();
-        $this->assertArrayHasKey('display_name', $profileArray);
-        $this->assertEquals('Fork CMS', $profileArray['display_name']);
-        $this->assertArrayHasKey('registered_on', $profileArray);
-        $this->assertEquals(1234567890, $profileArray['registered_on']);*/
-    }
-
-    public function testLoadingOfProfile()
-    {
-        $profileId = $this->addProfile();
-
-        $profile = new Profile($profileId);
-
-        $this->assertEquals('Fork CMS', $profile->getDisplayName());
-        $this->assertEquals('test@fork-cms.com', $profile->getEmail());
-        $this->assertEquals(1520243112, $profile->getRegisteredOn());
         $this->assertEquals('active', $profile->getStatus());
         $this->assertEquals('fork-cms', $profile->getUrl());
 
@@ -87,12 +46,59 @@ final class ProfileTest extends WebTestCase
                 'my_second_setting' => 'My second value',
             ]
         );
+
+        $settings = [];
+        foreach ($profile->getSettings()->toArray() as $profileSetting) {
+            $settings[$profileSetting->getName()] = $profileSetting->getValue();
+        }
         $this->assertEquals(
             [
                 'my_first_setting' => 'My first value',
                 'my_second_setting' => 'My second value',
             ],
-            $profile->getSettings()
+            $settings
+        );
+        $this->assertEquals('My first value', $profile->getSetting('my_first_setting'));
+
+        $profile->setSetting('my_second_setting', 'My updated value');
+        $this->assertEquals('My updated value', $profile->getSetting('my_second_setting'));
+
+        // @TODO The toArray will not work because the profile doesn't have an ID, this should be fixed?
+        /*$profileArray = $profile->toArray();
+        $this->assertArrayHasKey('display_name', $profileArray);
+        $this->assertEquals('Fork CMS', $profileArray['display_name']);
+        $this->assertArrayHasKey('registered_on', $profileArray);
+        $this->assertEquals(1234567890, $profileArray['registered_on']);*/
+    }
+
+    public function testLoadingOfProfile(): void
+    {
+        $profileId = $this->addProfile();
+
+        $profile = Model::get($profileId);
+
+        $this->assertEquals('Fork CMS', $profile->getDisplayName());
+        $this->assertEquals('test@fork-cms.com', $profile->getEmail());
+        $this->assertEquals('active', $profile->getStatus());
+        $this->assertEquals('fork-cms', $profile->getUrl());
+
+        $profile->setSettings(
+            [
+                'my_first_setting' => 'My first value',
+                'my_second_setting' => 'My second value',
+            ]
+        );
+
+        $settings = [];
+        foreach ($profile->getSettings()->toArray() as $profileSetting) {
+            $settings[$profileSetting->getName()] = $profileSetting->getValue();
+        }
+        $this->assertEquals(
+            [
+                'my_first_setting' => 'My first value',
+                'my_second_setting' => 'My second value',
+            ],
+            $settings
         );
         $this->assertEquals('My first value', $profile->getSetting('my_first_setting'));
 
@@ -102,8 +108,6 @@ final class ProfileTest extends WebTestCase
         $profileArray = $profile->toArray();
         $this->assertArrayHasKey('display_name', $profileArray);
         $this->assertEquals('Fork CMS', $profileArray['display_name']);
-        $this->assertArrayHasKey('registered_on', $profileArray);
-        $this->assertEquals(1520243112, $profileArray['registered_on']);
     }
 
     public function addProfile(): int
