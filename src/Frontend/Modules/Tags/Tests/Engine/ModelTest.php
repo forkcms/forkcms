8dd4204df84ab706f41cd049b18bd5310ba2632d
Jelmer Prins <jelmer@sumocoders.be>
2018-07-17 09:47:35 +0200

    Test that an exception is thrown when the interface isn't implemented

--- src/Frontend/Modules/Tags/Tests/Engine/ModelTest.php
+++ src/Frontend/Modules/Tags/Tests/Engine/ModelTest.php
@@ -2,8 +2,10 @@
 
 namespace Frontend\Modules\Tags\Tests\Engine;
 
+use Frontend\Core\Engine\Exception as FrontendException;
 use Frontend\Core\Engine\Model as FrontendModel;
-use Frontend\Modules\Tags\Engine\Model;
+use Frontend\Modules\Search\Engine\Model as SearchModel;
+use Frontend\Modules\Tags\Engine\Model as TagsModel;
 use Common\WebTestCase;
 
 final class ModelTest extends WebTestCase
@@ -19,4 +21,18 @@ final class ModelTest extends WebTestCase
         $client = self::createClient();
         $this->loadFixtures($client);
     }
+
+    public function testCallFromInterfaceOnModuleThatDoesNotImplementIt(): void
+    {
+        $module = 'Search';
+        $this->expectException(FrontendException::class);
+        $this->expectExceptionMessage(
+            'To use the tags module you need
+            to implement the FrontendTagsInterface
+            in the model of your module
+            (' . $module . ').'
+        );
+        TagsModel::callFromInterface($module, SearchModel::class, 'getIdForTags', null);
+    }
+
 }
