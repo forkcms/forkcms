b27b7c6c7215f147abc58177e9404b4d73dcecde
Jelmer Prins <jelmer@sumocoders.be>
2018-07-17 14:07:35 +0200

    Add test data for tags in the backend

--- src/Backend/Modules/Tags/Tests/Engine/ModelTest.php
+++ src/Backend/Modules/Tags/Tests/Engine/ModelTest.php
@@ -18,6 +18,25 @@ final class ModelTest extends WebTestCase
 
         $client = self::createClient();
         $this->loadFixtures($client);
+
+
+        BackendModel::get('database')->execute(
+            'INSERT INTO `modules_tags` (`module`, `tag_id`, `other_id`)
+            VALUES
+                (\'Pages\', 1, 1),
+                (\'Pages\', 2, 2),
+                (\'Pages\', 2, 3),
+                (\'Pages\', 2, 404),
+                (\'Pages\', 2, 405),
+                (\'Pages\', 2, 406),
+                (\'Faq\', 1, 1)'
+        );
+        BackendModel::get('database')->execute(
+            'INSERT INTO `tags` (`id`, `language`, `tag`, `number`, `url`)
+            VALUES
+                (1, \'en\', \'test\', 1, \'test\'),
+                (2, \'en\', \'most used\', 5, \'most-used\')'
+        );
     }
 
     public function testGetStartsWith(): void
