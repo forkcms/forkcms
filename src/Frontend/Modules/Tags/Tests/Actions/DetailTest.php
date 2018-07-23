80035687b85d8d10dc38ba1f8c64d4fc4bdea714
Jelmer Prins <jelmer@sumocoders.be>
2018-07-23 16:43:38 +0200

    Test tags detail page

---
+++ src/Frontend/Modules/Tags/Tests/Actions/DetailTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Frontend\Modules\Tags\Actions;
+
+use Backend\Modules\Tags\DataFixtures\LoadTagsModulesTags;
+use Backend\Modules\Tags\DataFixtures\LoadTagsTags;
+use Common\WebTestCase;
+
+class DetailTest extends WebTestCase
+{
+    public function testTagsHaveDetailPage(): void
+    {
+        $client = static::createClient();
+        $this->loadFixtures(
+            $client,
+            [
+                LoadTagsTags::class,
+                LoadTagsModulesTags::class,
+            ]
+        );
+
+        $crawler = $client->request('GET', '/en/tags');
+        self::assertEquals(
+            200,
+            $client->getResponse()->getStatusCode()
+        );
+
+        $link = $crawler->selectLink('most used')->link();
+        $crawler = $client->click($link);
+
+        self::assertEquals(
+            200,
+            $client->getResponse()->getStatusCode()
+        );
+        self::assertStringEndsWith(
+            '/en/tags/detail/most-used',
+            $client->getHistory()->current()->getUri()
+        );
+        self::assertStringStartsWith(
+            'Tags',
+            $crawler->filter('title')->text()
+        );
+        self::assertContains('<a href="/en/tags" title="To tags overview">', $crawler->html());
+        self::assertContains('<h2>Pages</h2>', $crawler->html());
+        self::assertContains('<a href="/en/sitemap" rel="tag">', $crawler->html());
+    }
+
+    public function testNonExistingFaqGives404(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/en/faq/detail/non-existing');
+        $this->assertIs404($client);
+    }
+}
