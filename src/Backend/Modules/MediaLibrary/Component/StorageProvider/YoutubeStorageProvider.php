4418e248b636170b67c3e97e6e94bbf7cf6194a2
Jelmer Prins <jelmer@sumocoders.be>
2019-05-21 09:57:58 +0200

    Return the thumbnail for youtube videos

--- src/Backend/Modules/MediaLibrary/Component/StorageProvider/YoutubeStorageProvider.php
+++ src/Backend/Modules/MediaLibrary/Component/StorageProvider/YoutubeStorageProvider.php
@@ -10,4 +10,9 @@ class YoutubeStorageProvider extends MovieStorageProvider
     {
         return '<iframe width="560" height="315" src="' . $this->includeUrl . $mediaItem->getUrl() . '" frameborder="0" allowfullscreen></iframe>';
     }
+
+    public function getWebPath(MediaItem $mediaItem): string
+    {
+        return 'https://img.youtube.com/vi/' . $mediaItem->getUrl() . '/maxresdefault.jpg';
+    }
 }
