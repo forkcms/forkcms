e20b232b16dd242ea194a36c11d49893d327553c
Katrien Vanhaute <vanhautekatrien@gmail.com>
2019-06-24 16:10:02 +0200

    Fix layout blog wordpress import

--- src/Backend/Core/Engine/Form.php
+++ src/Backend/Core/Engine/Form.php
@@ -111,7 +111,7 @@ class Form extends \Common\Core\Form
     public function addFile($name, $class = null, $classError = null): SpoonFormFile
     {
         $name = (string) $name;
-        $class = (string) ($class ?? 'fork-form-file');
+        $class = (string) ($class ?? 'custom-file-input');
         $classError = (string) ($classError ?? 'error form-control-danger is-invalid');
 
         // add element
