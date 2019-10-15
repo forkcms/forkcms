d01bf0ac21d8e5881a59ff6b04817422d8d1eaf8
Bruno Vitorino <bruno@sumocoders.be>
2019-10-15 10:51:37 +0200

    Add GET controller

--- src/Backend/Modules/ContentBlocks/Api/ApiController.php
+++ src/Backend/Modules/ContentBlocks/Api/ApiController.php
@@ -2,9 +2,11 @@
 
 namespace Backend\Modules\ContentBlocks\Api;
 
+use Backend\Modules\ContentBlocks\Domain\ContentBlock\ContentBlock;
 use Backend\Modules\ContentBlocks\Domain\ContentBlock\ContentBlockRepository;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 final class ApiController
 {
@@ -20,11 +22,27 @@ final class ApiController
         $this->serializer = $serializer;
     }
 
-    public function getContentBlocksAction()
+    public function getContentblocksAction(): JsonResponse
     {
         return JsonResponse::create(
             json_decode(
                 $this->serializer->serialize($this->blockRepository->findAll(), 'json'),
+                true
+            )
+        );
+    }
+
+    public function getContentblockAction($contentBlock): JsonResponse
+    {
+        $contentBlock = $this->blockRepository->find($contentBlock);
+
+        if (!$contentBlock instanceof ContentBlock) {
+            throw new NotFoundHttpException();
+        }
+
+        return JsonResponse::create(
+            json_decode(
+                $this->serializer->serialize($contentBlock, 'json'),
                 true
             )
         );
