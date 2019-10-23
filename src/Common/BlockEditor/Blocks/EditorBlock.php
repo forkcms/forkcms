1adeea5b3c15d78fc88b54858e1b13611e9033e2
Jelmer Prins <jelmer@sumocoders.be>
2019-10-23 18:26:59 +0200

    Create abstract class for the EditorBlocks

---
+++ src/Common/BlockEditor/Blocks/EditorBlock.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Common\BlockEditor\Blocks;
+
+use JsonSerializable;
+
+abstract class EditorBlock implements JsonSerializable
+{
+    /** @var array */
+    protected $data;
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    abstract public static function getName(): string;
+
+    /**
+     * @return array The config must contain the key "class" with the JS class for the editor
+     */
+    abstract public static function getConfig(): array;
+
+    /**
+     * @see https://github.com/editor-js/editorjs-php#configuration-file
+     */
+    abstract public static function getValidation(): array;
+
+    public function jsonSerialize(): array
+    {
+        return $this->data;
+    }
+}
