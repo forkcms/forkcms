1f1d4f29849590106fdd332a70b2077e3745b7cf
Jelmer Prins <jelmer@sumocoders.be>
2019-10-24 14:09:17 +0200

    Initial implementation for a header block

---
+++ src/Common/BlockEditor/Blocks/Header.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Common\BlockEditor\Blocks;
+
+final class Header extends EditorBlock
+{
+    public function getConfig(): array
+    {
+        return [
+            'shortcut' => 'CMD+SHIFT+H',
+            'class' => 'BlockEditor.blocks.Header',
+        ];
+    }
+
+    public function getValidation(): array
+    {
+        return [
+            'text' => [
+                'type' => 'string',
+                'required' => true,
+                'allowedTags' => 'b,i,a[href]',
+            ],
+            'level' => [
+                'type' => 'int',
+                'canBeOnly' => [1, 2, 3, 4, 5, 6],
+            ],
+        ];
+    }
+
+    public function parse(array $data): string
+    {
+        return '<h' . $data['level'] . '>' . $data['text'] . '</h' . $data['level'] . '>';
+    }
+
+    public function getJavaScriptUrl(): ?string
+    {
+        return '/js/editor.js';
+    }
+}
