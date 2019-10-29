f867f56cd06cc60575717094d5bed2106e13ab2f
Bruno Vitorino <bruno@sumocoders.be>
2019-10-29 14:37:04 +0100

    Add DTO's for page

---
+++ src/Backend/Modules/Pages/Domain/Page/Command/UpdatePage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Backend\Modules\Pages\Domain\Page\Command;
+
+use Backend\Modules\Pages\Domain\Page\Page;
+use Backend\Modules\Pages\Domain\Page\PageDataTransferObject;
+
+final class UpdatePage extends PageDataTransferObject
+{
+}
