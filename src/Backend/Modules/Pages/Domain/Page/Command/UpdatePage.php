420fea34abf69723ed2e925999771bdc61c641b0
Bruno Vitorino <bruno@sumocoders.be>
2019-10-29 14:56:39 +0100

    Remove use statement

--- src/Backend/Modules/Pages/Domain/Page/Command/UpdatePage.php
+++ src/Backend/Modules/Pages/Domain/Page/Command/UpdatePage.php
@@ -2,7 +2,6 @@
 
 namespace Backend\Modules\Pages\Domain\Page\Command;
 
-use Backend\Modules\Pages\Domain\Page\Page;
 use Backend\Modules\Pages\Domain\Page\PageDataTransferObject;
 
 final class UpdatePage extends PageDataTransferObject
