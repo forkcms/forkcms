06b991c977e0833bc435cc46d94f21b891161a53
Bruno Vitorino <bruno@sumocoders.be>
2019-11-07 16:31:26 +0100

    Fix table name

--- src/Frontend/Core/Engine/Model.php
+++ src/Frontend/Core/Engine/Model.php
@@ -206,7 +206,7 @@ class Model extends \Common\Core\Model
             'SELECT pe.id AS extra_id, pb.html, pb.position,
              pe.module AS extra_module, pe.type AS extra_type, pe.action AS extra_action, pe.data AS extra_data
              FROM PagesPageBlock AS pb
-             INNER JOIN pages AS p ON p.revision_id = pb.revision_id
+             INNER JOIN PagesPage AS p ON p.revision_id = pb.revision_id
              LEFT OUTER JOIN PagesModuleExtra AS pe ON pb.extra_id = pe.id AND pe.hidden = ?
              WHERE ' . $where . '
              ORDER BY pb.position, pb.sequence',
