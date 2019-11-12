5de43c02959d4d52f8f3f5efb6c2277cf5af56ff
Bruno Vitorino <bruno@sumocoders.be>
2019-11-12 14:49:46 +0100

    Use repository method to retrieve page

--- src/Backend/Modules/Pages/Api/ApiController.php
+++ src/Backend/Modules/Pages/Api/ApiController.php
@@ -4,7 +4,6 @@ namespace Backend\Modules\Pages\Api;
 
 use Backend\Modules\Pages\Domain\Page\PageRepository;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use JMS\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -13,13 +12,10 @@ final class ApiController
     /** @var PageRepository */
     private $pageRepository;
 
-    /** @var SerializerInterface */
-    private $serializer;
-
-    public function __construct(PageRepository $pageRepository, SerializerInterface $serializer)
-    {
+    public function __construct(
+        PageRepository $pageRepository
+    ) {
         $this->pageRepository = $pageRepository;
-        $this->serializer = $serializer;
     }
 
     /**
@@ -27,20 +23,12 @@ final class ApiController
      */
     public function getPageAction(string $language, int $id): JsonResponse
     {
-        $latest = $this->pageRepository->getLatest($id, $language);
+        $latest = $this->pageRepository->getLatestForApi($id, $language);
 
-        if ($language === null) {
+        if ($latest === null) {
             throw new NotFoundHttpException();
         }
 
-        return JsonResponse::create(
-            json_decode(
-                $this->serializer->serialize(
-                    $latest,
-                    'json'
-                ),
-                true
-            )
-        );
+        return JsonResponse::create($latest);
     }
 }
