1012e187b50022cf50e20ae0743989aeea608bbc
Bruno Vitorino <bruno@sumocoders.be>
2019-11-19 14:35:21 +0100

    dd PHPDoc for beter code completion

--- src/Backend/Modules/ContentBlocks/Domain/ContentBlock/ContentBlockRepository.php
+++ src/Backend/Modules/ContentBlocks/Domain/ContentBlock/ContentBlockRepository.php
@@ -6,6 +6,12 @@ use Backend\Modules\ContentBlocks\Domain\ContentBlock\Exception\ContentBlockNotF
 use Common\Locale;
 use Doctrine\ORM\EntityRepository;
 
+/**
+ * @method ContentBlock|null find($id, $lockMode = null, $lockVersion = null)
+ * @method ContentBlock|null findOneBy(array $criteria, array $orderBy = null)
+ * @method ContentBlock[]    findAll()
+ * @method ContentBlock[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
 class ContentBlockRepository extends EntityRepository
 {
     public function add(ContentBlock $contentBlock): void
