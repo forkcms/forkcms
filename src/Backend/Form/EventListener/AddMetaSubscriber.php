0393386eb8cbf4312253dd6089b9badc5d728da3
Jelmer Prins <jelmer@sumocoders.be>
2019-11-26 10:01:44 +0100

    Add support for the pages module to the meta subscriber

--- src/Backend/Form/EventListener/AddMetaSubscriber.php
+++ src/Backend/Form/EventListener/AddMetaSubscriber.php
@@ -4,6 +4,7 @@ namespace Backend\Form\EventListener;
 
 use Backend\Core\Engine\Model;
 use Backend\Form\Type\MetaType;
+use Backend\Modules\Pages\Engine\Model as BackendPagesModel;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -75,7 +76,7 @@ class AddMetaSubscriber implements EventSubscriberInterface
             MetaType::class,
             [
                 'base_field_name' => $this->baseFieldName,
-                'detail_url' => Model::getUrlForBlock($this->moduleForUrl, $this->actionForUrl),
+                'detail_url' => $this->getDetailUrl(),
                 'generate_url_callback_class' => $this->generateUrlCallbackClass,
                 'generate_url_callback_method' => $this->generateUrlCallbackMethod,
                 'generate_url_callback_parameters' => $this->buildCallbackParameters($event),
@@ -83,6 +84,17 @@ class AddMetaSubscriber implements EventSubscriberInterface
         );
     }
 
+    private function getDetailUrl(): string
+    {
+        if ($this->moduleForUrl === 'Pages' && $this->actionForUrl === 'Page') {
+            return rtrim(
+                BackendPagesModel::getFullUrl(Model::getRequest()->query->getInt('parent', Model::HOME_PAGE_ID)),
+                '/'
+            );
+        }
+
+        return Model::getUrlForBlock($this->moduleForUrl, $this->actionForUrl);
+    }
     private function buildCallbackParameters(FormEvent $event): array
     {
         $parameters = [];
