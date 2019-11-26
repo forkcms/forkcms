c8c78eb23510f2e9cd53a40bf188a8daace46da4
Jelmer Prins <jelmer@sumocoders.be>
2019-11-26 11:06:08 +0100

    Add basic meta tab

--- src/Backend/Modules/Pages/Domain/Page/Form/PageType.php
+++ src/Backend/Modules/Pages/Domain/Page/Form/PageType.php
@@ -2,7 +2,10 @@
 
 namespace Backend\Modules\Pages\Domain\Page\Form;
 
+use Backend\Form\EventListener\AddMetaSubscriber;
+use Backend\Form\Type\MetaType;
 use Backend\Modules\Pages\Domain\Page\PageDataTransferObject;
+use Backend\Modules\Pages\Domain\Page\PageRepository;
 use Common\Form\TitleType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -12,12 +15,18 @@ final class PageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add(
-            'title',
-            TitleType::class,
-            [
-                'label' => 'lbl.Title',
-            ]
+        $builder->add('title', TitleType::class);
+        $builder->addEventSubscriber(
+            new AddMetaSubscriber(
+                'Pages', // Virtual to make sure the correct url is used
+                'Page', // Virtual to make sure the correct url is used
+                PageRepository::class,
+                'getUrl',
+                [
+                    'getData.getLocale',
+                    'getData.getId',
+                ]
+            )
         );
     }
 
