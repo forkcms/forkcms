6a65bbe89426770ccbc3f8625ecabbdc2f85dfa8
Jelmer Prins <jelmer@sumocoders.be>
2019-11-27 10:36:53 +0100

    Add switch type

---
+++ src/Common/Form/SwitchType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Common\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SwitchType extends CheckboxType
+{
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        parent::configureOptions($resolver);
+        $resolver->setDefault('label_attr', ['class' => 'custom-control-label']);
+    }
+
+    public function getParent(): string
+    {
+        return CheckboxType::class;
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'checkbox_switch';
+    }
+}
