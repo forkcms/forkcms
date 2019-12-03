d25b72a54c29215330340047ac824edf89919c2e
Jelmer Prins <jelmer@sumocoders.be>
2019-12-03 14:20:45 +0100

    Fix type casting

--- src/Backend/Modules/Pages/Domain/Page/Form/PageContentType.php
+++ src/Backend/Modules/Pages/Domain/Page/Form/PageContentType.php
@@ -10,7 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-final class PageSettingsType extends AbstractType
+final class PageContentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
