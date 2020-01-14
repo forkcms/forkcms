92cd84430baf868cde02e7bee40a7b4de79b33a5
Jelmer Prins <jelmer@sumocoders.be>
2020-01-14 18:50:09 +0100

    New page to add pages

--- src/Backend/Modules/Pages/Actions/PageAdd.php
+++ src/Backend/Modules/Pages/Actions/PageAdd.php
@@ -7,8 +7,12 @@ use Backend\Core\Language\Locale;
 use Backend\Modules\Pages\Domain\Page\Command\CreatePage;
 use Backend\Modules\Pages\Domain\Page\Form\PageType;
 use Backend\Core\Engine\Model as BackendModel;
+use Backend\Modules\Pages\Domain\Page\Page;
+use Backend\Modules\Pages\Domain\Page\PageRepository;
+use Backend\Modules\Pages\Domain\Page\Status;
 use Backend\Modules\Pages\Engine\Model as BackendPagesModel;
 use Common\ModulesSettings;
+use SimpleBus\Message\Bus\MessageBus;
 use Symfony\Component\Form\Form;
 use Symfony\Component\HttpKernel\KernelInterface;
 
@@ -17,11 +21,19 @@ final class PageAdd extends Action
     /** @var ModulesSettings */
     private $settings;
 
+    /** @var PageRepository */
+    private $pageRepository;
+
+    /** @var MessageBus */
+    private $commandBus;
+
     public function setKernel(KernelInterface $kernel = null): void
     {
         parent::setKernel($kernel);
 
         $this->settings = $this->getContainer()->get('fork.settings');
+        $this->pageRepository = $this->getContainer()->get(PageRepository::class);
+        $this->commandBus = $this->getContainer()->get('command_bus');
     }
 
     public function execute(): void
@@ -45,9 +57,8 @@ final class PageAdd extends Action
     private function handleForm(Form $form): void
     {
         $createPage = $form->getData();
-dump($createPage);
-die;
-        $this->get('command_bus')->handle($createPage);
+
+        $this->commandBus->handle($createPage);
 
         $this->redirect(
             $this->getBackLink(
@@ -65,7 +76,8 @@ die;
             PageType::class,
             new CreatePage(
                 Locale::workingLocale(),
-                $this->settings->get($this->getModule(), 'default_template', 1)
+                $this->settings->get($this->getModule(), 'default_template', 1),
+                $this->getParent()
             )
         );
 
@@ -88,5 +100,21 @@ die;
     {
         $this->header->addJS('/js/vendors/jstree.js', null, false, true, true);
         $this->template->assign('tree', BackendPagesModel::getTreeHTML());
+    }
+
+    private function getParent(): ?Page
+    {
+        $parentPage = $this->pageRepository->findOneBy(
+            [
+                'id' => $this->getRequest()->query->getInt('parent'),
+                'status' => Status::active(),
+            ]
+        );
+
+        if ($parentPage instanceof Page && $parentPage->isAllowChildren()) {
+            return $parentPage;
+        }
+
+        return null;
     }
 }
