ba6293f1579bc2d44bc53cfed353120f466bfd61
Jelmer Prins <jelmer@sumocoders.be>
2020-01-22 15:38:25 +0100

    Remove extra whitespace

--- src/Frontend/Modules/Profiles/Tests/Engine/ModelTest.php
+++ src/Frontend/Modules/Profiles/Tests/Engine/ModelTest.php
@@ -127,7 +127,7 @@ final class ModelTest extends WebTestCase
         $this->assertNotContains('Someone else\'s setting\'s value', $settings);
     }
 
-    public  function testDeletingSetting(): void
+    public function testDeletingSetting(): void
     {
         Model::setSetting(1, 'my_setting', 'My setting\'s value');
         $this->assertEquals('My setting\'s value', Model::getSetting(1, 'my_setting'));
