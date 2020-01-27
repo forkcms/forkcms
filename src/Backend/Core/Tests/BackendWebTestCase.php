666b773dc88e5ae347bd16f0033db0f23612ca4a
Jelmer Prins <jelmer@sumocoders.be>
2020-01-27 14:44:48 +0100

    Add assertion to check if you need to be authenticated to access a page in the backend

--- src/Backend/Core/Tests/BackendWebTestCase.php
+++ src/Backend/Core/Tests/BackendWebTestCase.php
@@ -3,6 +3,7 @@
 namespace Backend\Core\Tests;
 
 use Common\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Client;
 
 abstract class BackendWebTestCase extends WebTestCase
 {
@@ -13,5 +14,20 @@ abstract class BackendWebTestCase extends WebTestCase
         if (!defined('APPLICATION')) {
             define('APPLICATION', 'Backend');
         }
+    }
+
+    protected function assertAuthenticationIsNeeded(Client $client, string $url, string $method = 'GET'): void
+    {
+        // make sure we aren't logged in with the client
+        $this->logout($client);
+
+        $client->setMaxRedirects(1);
+        $client->request($method, $url);
+
+        // we should get redirected to authentication with a reference to the wanted page
+        self::assertStringEndsWith(
+            '/private/en/authentication?querystring=' . rawurlencode($url),
+            $client->getHistory()->current()->getUri()
+        );
     }
 }
