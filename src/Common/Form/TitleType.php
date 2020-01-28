a1e6555dac5ae3bfd498a6e8ea3d08a66f687c6b
Jelmer Prins <jelmer@sumocoders.be>
2020-01-28 11:12:38 +0100

    Use the normal way of extending a form type from symfony

--- src/Common/Form/TitleType.php
+++ src/Common/Form/TitleType.php
@@ -2,10 +2,11 @@
 
 namespace Common\Form;
 
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class TitleType extends TextType
+class TitleType extends AbstractType
 {
     public function configureOptions(OptionsResolver $resolver): void
     {
@@ -20,6 +21,11 @@ class TitleType extends TextType
                 ]
             ]
         );
+    }
+
+    public function getParent(): string
+    {
+        return TextType::class;
     }
 
     public function getBlockPrefix(): string
