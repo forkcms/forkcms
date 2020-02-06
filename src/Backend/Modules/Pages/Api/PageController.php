c93fa5d0e664344f0c1855a699c28e0f26b88abf
Jelmer Prins <jelmer@sumocoders.be>
2020-02-06 17:11:16 +0100

    Return the assigned parameters from the widget or action

--- src/Backend/Modules/Pages/Api/PageController.php
+++ src/Backend/Modules/Pages/Api/PageController.php
@@ -3,20 +3,46 @@
 namespace Backend\Modules\Pages\Api;
 
 use Backend\Core\Language\Locale;
+use Backend\Modules\Pages\Domain\ModuleExtra\ModuleExtra;
+use Backend\Modules\Pages\Domain\ModuleExtra\ModuleExtraType;
 use Backend\Modules\Pages\Domain\Page\PageRepository;
+use Backend\Modules\Pages\Domain\PageBlock\PageBlockRepository;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Frontend\Core\Engine\Breadcrumb;
+use Frontend\Core\Engine\Footer;
+use Frontend\Core\Engine\Navigation;
+use Frontend\Core\Engine\Url;
+use Frontend\Core\Header\Header;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 final class PageController
 {
     /** @var PageRepository */
     private $pageRepository;
 
+    /** @var PageBlockRepository */
+    private $pageBlockRepository;
+
+    /** @var KernelInterface */
+    private $kernel;
+
+    /** @var RequestStack */
+    private $requestStack;
+
     public function __construct(
-        PageRepository $pageRepository
+        PageRepository $pageRepository,
+        PageBlockRepository $pageBlockRepository,
+        KernelInterface $kernel,
+        RequestStack $requestStack
     ) {
         $this->pageRepository = $pageRepository;
+        $this->pageBlockRepository = $pageBlockRepository;
+        $this->kernel = $kernel;
+        $this->requestStack = $requestStack;
     }
 
     /**
@@ -41,5 +67,48 @@ final class PageController
         $subPages = $this->pageRepository->getSubPagesForApi($id, Locale::fromString($locale));
 
         return JsonResponse::create($subPages);
+    }
+
+    /**
+     * @Rest\Get("/pages/{locale}/{pageId}/page-block/{pageBlockId}")
+     */
+    public function getPageBlockAction(string $locale, int $pageId, int $pageBlockId): JsonResponse
+    {
+        $locale = Locale::fromString($locale);
+        $latest = $this->pageRepository->getLatestForApi($pageId, $locale);
+
+        if ($latest === null) {
+            return JsonResponse::create(null, JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        $pageBlock = $latest['extras'][$pageBlockId] ?? [];
+
+        if (empty($pageBlock) || $pageBlock['hidden']) {
+            return JsonResponse::create(null, JsonResponse::HTTP_NOT_FOUND);
+        }
+
+
+        $mockRequest = Request::create(rtrim(Navigation::getUrl($latest['id'], $locale), '/'));
+        $this->requestStack->push($mockRequest);
+
+        // we need this hurray
+        new Url($this->kernel);
+        new Header($this->kernel);
+        new Breadcrumb($this->kernel);
+        new Footer($this->kernel);
+
+        $moduleExtra = ModuleExtra::createModuleExtra(
+            $this->kernel,
+            new ModuleExtraType($pageBlock['type']),
+            $pageBlock['module'],
+            $pageBlock['action'],
+            $pageBlock['data']
+        );
+        $moduleExtra->execute();
+        $extraVariables = $moduleExtra->getTemplate()->getAssignedVariables();
+
+        $this->requestStack->pop();
+
+        return JsonResponse::create($extraVariables);
     }
 }
