f1c9a3b18e955110f7fa3f89d18c96ae32bf48a3
Tijs Verkoyen <tijs@verkoyen.eu>
2020-02-17 16:57:20 +0100

    Use gtag.js
    gtag.js is the current way to add Google Analytics to a site.
    
    See https://support.google.com/analytics/answer/7538414

--- src/Frontend/Core/Header/GoogleAnalytics.php
+++ src/Frontend/Core/Header/GoogleAnalytics.php
@@ -54,34 +54,32 @@ final class GoogleAnalytics
         return $this->modulesSettings->get('Core', 'show_cookie_bar', false) && !$this->cookie->hasAllowedCookies();
     }
 
-    private function getGoogleAnalyticsEvent(): string
-    {
-        if ($this->shouldAnonymize()) {
-            return 'ga(\'send\', \'pageview\', {\'anonymizeIp\': true});';
-        }
-
-        return 'ga(\'send\', \'pageview\');';
-    }
-
     public function __toString(): string
     {
         if (!$this->shouldAddGoogleAnalyticsHtml()) {
             return '';
         }
 
-        $webPropertyId = $this->modulesSettings->get('Analytics', 'web_property_id', null);
+        $code = [
+            '<!-- Global site tag (gtag.js) - Google Analytics -->',
+            '<script async src="https://www.googletagmanager.com/gtag/js?id=%1$s"></script>',
+            '<script>',
+            '  window.dataLayer = window.dataLayer || [];',
+            '  function gtag(){dataLayer.push(arguments);}',
+            '  gtag(\'js\', new Date());',
+        ];
 
-        $trackingCode = '<script>
-                          (function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){
-                          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                          })(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');
-                          ga(\'create\', \'' . $webPropertyId . '\', \'' . $this->httpHost
-                        /**string */. '\');
-                        ';
-        $trackingCode .= $this->getGoogleAnalyticsEvent();
-        $trackingCode .= '</script>';
+        if ($this->shouldAnonymize()) {
+            $code[] = '  gtag(\'config\', \'%1$s\', { \'anonymize_ip\': true });';
+        } else {
+            $code[] = '  gtag(\'config\', \'%1$s\');';
+        }
 
-        return $trackingCode;
+        $code[] = '</script>';
+
+        return sprintf(
+            implode("\n", $code) . "\n",
+            $this->modulesSettings->get('Analytics', 'web_property_id', null)
+        );
     }
 }
