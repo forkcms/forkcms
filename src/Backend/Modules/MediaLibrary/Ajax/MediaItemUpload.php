cf656aab993e2aee306a53dfd129d80afb19bfc0
Jelmer Prins <jelmer@pageon.be>
2020-07-04 17:34:25 +0200

    Remove old documentation

--- src/Backend/Modules/MediaLibrary/Ajax/MediaItemUpload.php
+++ src/Backend/Modules/MediaLibrary/Ajax/MediaItemUpload.php
@@ -72,34 +72,6 @@ class MediaItemUpload extends BackendBaseAJAXAction
         $this->commandBus = $this->get('command_bus');
     }
 
-    /**
-     * PHP Server-Side Example for Fine Uploader (traditional endpoint handler).
-     * Maintained by Widen Enterprises.
-     *
-     * This example:
-     *  - handles chunked and non-chunked requests
-     *  - supports the concurrent chunking feature
-     *  - assumes all upload requests are multipart encoded
-     *  - handles delete requests
-     *  - handles cross-origin environments
-     *
-     * Follow these steps to get up and running with Fine Uploader in a PHP environment:
-     *
-     * 1. Setup your client-side code, as documented on http://docs.fineuploader.com.
-     *
-     * 2. Copy this file and handler.php to your server.
-     *
-     * 3. Ensure your php.ini file contains appropriate values for
-     *    max_input_time, upload_max_filesize and post_max_size.
-     *
-     * 4. Ensure your "chunks" and "files" folders exist and are writable.
-     *    "chunks" is only needed if you have enabled the chunking feature client-side.
-     *
-     * 5. If you have chunking enabled in Fine Uploader, you MUST set a value for the `chunking.success.endpoint` option.
-     *    This will be called by Fine Uploader when all chunks for a file have been successfully uploaded, triggering the
-     *    PHP server to combine all parts into one file. This is particularly useful for the concurrent chunking feature,
-     *    but is now required in all cases if you are making use of this PHP example.
-     */
     public function execute(): void
     {
         parent::execute();
