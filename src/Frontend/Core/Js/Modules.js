645dd934666383a6e018c25f47187dcb465bae58
Katrien Vanhaute <vanhautekatrien@gmail.com>
2020-07-23 15:19:46 +0200

    Add profiles class

--- src/Frontend/Core/Js/Modules.js
+++ src/Frontend/Core/Js/Modules.js
@@ -1,9 +1,11 @@
 import { Faq } from '../../Modules/Faq/Js/Faq'
 import { Location } from '../../Modules/Location/Js/Location'
+import { Profiles } from '../../Modules/Profiles/Js/Profiles'
 
 export class Modules {
   initModules () {
     this.faq = new Faq()
     this.location = new Location()
+    this.profiles = new Profiles()
   }
 }
