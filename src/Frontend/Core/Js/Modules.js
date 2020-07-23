f4ad8751a686aab0f18fffb2cd051921e38adeeb
Katrien Vanhaute <vanhautekatrien@gmail.com>
2020-07-23 15:03:22 +0200

    Add location class

--- src/Frontend/Core/Js/Modules.js
+++ src/Frontend/Core/Js/Modules.js
@@ -1,7 +1,9 @@
 import { Faq } from '../../Modules/Faq/Js/Faq'
+import { Location } from '../../Modules/Location/Js/Location'
 
 export class Modules {
   initModules () {
     this.faq = new Faq()
+    this.location = new Location()
   }
 }
