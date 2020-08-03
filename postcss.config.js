3ff745711412cb2eac0748d3308804ca5352934c
Katrien Vanhaute <vanhautekatrien@gmail.com>
2020-08-03 15:43:19 +0200

    Do not specify belgium

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
     plugins: {
         'postcss-import': {},
         'postcss-cssnext': {
-            browsers: ['last 2 versions', '> 5% in BE'],
+            browsers: ['last 2 versions'],
         },
         'postcss-clean': {
             level: 2,
