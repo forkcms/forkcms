8ac54e2b861fcd829f90389b8d4246f56a307d3d
Jelmer Prins <jelmer@pageon.be>
2020-11-13 00:38:05 +0100

    Fix twig deprecations

--- src/Frontend/Core/Engine/FormFieldErrorNode.php
+++ src/Frontend/Core/Engine/FormFieldErrorNode.php
@@ -2,28 +2,28 @@
 
 namespace Frontend\Core\Engine;
 
+use Twig\Compiler;
+use Twig\Node\Node;
+
 /**
  * Twig note for writing out the compiled version of a form field error.
  */
-class FormFieldErrorNode extends \Twig_Node
+class FormFieldErrorNode extends Node
 {
     /**
+     * Name of the template var holding the form this field error belongs to.
+     *
      * @var string
      */
     private $form;
 
     /**
+     * Name of the field of which we need to render the error.
+     *
      * @var string
      */
     private $field;
 
-    /**
-     * @param string $form Name of the template var holding the form this field
-     *                     error belongs to.
-     * @param string $field Name of the field of which we need to render the error.
-     * @param int $lineNumber Line number in the template source file.
-     * @param string $tag the name of the template tag.
-     */
     public function __construct(string $form, string $field, int $lineNumber, string $tag)
     {
         parent::__construct([], [], $lineNumber, $tag);
@@ -31,10 +31,7 @@ class FormFieldErrorNode extends \Twig_Node
         $this->field = $field;
     }
 
-    /**
-     * @param \Twig_Compiler $compiler
-     */
-    public function compile(\Twig_Compiler $compiler): void
+    public function compile(Compiler $compiler): void
     {
         $writeErrorMessage = 'echo '
             . "\$context['form_{$this->form}']->getField('{$this->field}')->getErrors() "
