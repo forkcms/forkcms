2d6a2fe3d082d3d83d9fb821e0e122a1562961a5
Bjorn Van Acker <bjorn@sumocoders.be>
2020-11-25 16:32:32 +0100

    Use radio buttons for selecting templates

--- src/Backend/Modules/Pages/Domain/Page/Form/PageContentType.php
+++ src/Backend/Modules/Pages/Domain/Page/Form/PageContentType.php
@@ -54,6 +54,7 @@ final class PageContentType extends AbstractType
                 'choice_attr' => function (int $templateId): array {
                     return [
                         'data-config' => $this->templates[$templateId]['json'],
+                        'data-html' => $this->templates[$templateId]['html'],
                     ];
                 },
                 'attr' => [
@@ -61,6 +62,7 @@ final class PageContentType extends AbstractType
                     'autocomplete' => 'off',
                 ],
                 'label' => 'lbl.Template',
+                'expanded' => true,
             ]
         );
 
