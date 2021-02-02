d983e80a8027bfb4bf9d89441d9864cf01501d90
Katrien Vanhaute <vanhautekatrien@gmail.com>
2021-02-02 10:36:39 +0100

    Rewrite markup button with new method make and getter css

--- src/Backend/Core/Js/BlockEditor/Blocks/Button.js
+++ src/Backend/Core/Js/BlockEditor/Blocks/Button.js
@@ -1,3 +1,5 @@
+import { StringUtil } from '../../Components/StringUtil'
+
 class Button {
   constructor ({data}) {
     this.data = {
@@ -19,6 +21,16 @@ class Button {
     return {
       title: 'Button',
       icon: '<svg width="17" height="15" viewBox="0 0 336 276" xmlns="http://www.w3.org/2000/svg"><path d="M291 150V79c0-19-15-34-34-34H79c-19 0-34 15-34 34v42l67-44 81 72 56-29 42 30zm0 52l-43-30-56 30-81-67-66 39v23c0 19 15 34 34 34h178c17 0 31-13 34-29zM79 0h178c44 0 79 35 79 79v118c0 44-35 79-79 79H79c-44 0-79-35-79-79V79C0 35 35 0 79 0z"/></svg>'
+    }
+  }
+
+  get CSS () {
+    return {
+      wrapper: 'ce-wrapper',
+      wrapperLabel: 'ce-wrapper-label',
+      input: 'ce-input',
+      label: 'ce-label',
+      spacingBellow: 'mb-2'
     }
   }
 
@@ -50,53 +62,77 @@ class Button {
   }
 
   render () {
-    this.wrapper = document.createElement('div')
-    this.inputTextWrapper = document.createElement('div')
-    this.inputUrlWrapper = document.createElement('div')
-    this.selectColorWrapper = document.createElement('div')
+    const wrapper = this._make('div', this.CSS.wrapper)
+    const wrapperContent = this._make('div')
+    const wrapperTitle = this._make('label', this.CSS.wrapperLabel,
+      {
+        innerHTML: StringUtil.ucfirst(window.backend.locale.lbl('Button'))
+      }
+    )
 
-    this.wrapper.classList.add('button-wrapper')
-    this.inputTextWrapper.classList.add('form-group')
-    this.inputUrlWrapper.classList.add('form-group')
-    this.selectColorWrapper.classList.add('form-group')
+    const labelText = this._make('label', this.CSS.label,
+      {
+        innerHTML: StringUtil.ucfirst(window.backend.locale.lbl('Text'))
+      }
+    )
+    const inputText = this._make('input', [this.CSS.input, this.CSS.spacingBellow],
+      {
+        value: this.data && this.data.text ? this.data.text : ''
+      },
+      {
+        'data-input-text': ''
+      }
+    )
 
-    const inputUrl = document.createElement('input')
-    const inputText = document.createElement('input')
-    const selectColor = document.createElement('select')
+    const labelUrl = this._make('label', this.CSS.label,
+      {
+        innerHTML: StringUtil.ucfirst(window.backend.locale.lbl('Link'))
+      }
+    )
+    const inputUrl = this._make('input', [this.CSS.input, this.CSS.spacingBellow],
+      {
+        value: this.data && this.data.url ? this.data.url : ''
+      },
+      {
+        'data-input-url': ''
+      }
+    )
 
-    this.inputTextWrapper.appendChild(inputText)
-    this.inputUrlWrapper.appendChild(inputUrl)
-    this.selectColorWrapper.appendChild(selectColor)
+    const labelColor = this._make('label', this.CSS.label,
+      {
+        innerHTML: StringUtil.ucfirst(window.backend.locale.lbl('Color'))
+      }
+    )
+    const selectColor = this._make('select', this.CSS.input, {}, {
+      'data-select-color': ''
+    })
+    const opt1 = this._make('option', null,
+      {
+        value: 'primary',
+        innerHTML: 'primary'
+      }
+    )
+    const opt2 = this._make('option', null,
+      {
+        value: 'secondary',
+        innerHTML: 'secondary'
+      }
+    )
 
-    this.wrapper.appendChild(this.inputUrlWrapper)
-    this.wrapper.appendChild(this.inputTextWrapper)
-    this.wrapper.appendChild(this.selectColorWrapper)
-
-    inputUrl.placeholder = 'Button url here'
-    inputUrl.value = this.data && this.data.url ? this.data.url : ''
-    inputUrl.setAttribute('data-input-url', null)
-    inputUrl.classList.add('form-control')
-
-    inputText.placeholder = 'Button text here'
-    inputText.value = this.data && this.data.text ? this.data.text : ''
-    inputText.setAttribute('data-input-text', null)
-    inputText.classList.add('form-control')
-
-    selectColor.setAttribute('data-select-color', null)
-    selectColor.classList.add('form-control')
-
-    const opt1 = document.createElement('option')
-    const opt2 = document.createElement('option')
     selectColor.appendChild(opt1)
     selectColor.appendChild(opt2)
 
-    opt1.appendChild(document.createTextNode('primary'))
-    opt2.appendChild(document.createTextNode('secondary'))
+    wrapperContent.appendChild(labelText)
+    wrapperContent.appendChild(inputText)
+    wrapperContent.appendChild(labelUrl)
+    wrapperContent.appendChild(inputUrl)
+    wrapperContent.appendChild(labelColor)
+    wrapperContent.appendChild(selectColor)
 
-    opt1.value = 'primary'
-    opt2.value = 'secondary'
+    wrapper.appendChild(wrapperTitle)
+    wrapper.appendChild(wrapperContent)
 
-    return this.wrapper
+    return wrapper
   }
 
   save (blockContent) {
@@ -108,8 +144,36 @@ class Button {
     return Object.assign(this.data, {
       url: inputUrl.value,
       text: inputText.value,
-      style: selectColor.value,
+      style: selectColor.value
     })
+  }
+
+  /**
+   * Helper for making Elements with attributes
+   *
+   * @param  {string} tagName           - new Element tag name
+   * @param  {Array|string} classNames  - list or name of CSS classname(s)
+   * @param  {object} attributes        - any attributes
+   * @returns {Element}
+   */
+  _make (tagName, classNames = null, attributes = {}, customAttributes = {}) {
+    const el = document.createElement(tagName)
+
+    if (Array.isArray(classNames)) {
+      el.classList.add(...classNames)
+    } else if (classNames) {
+      el.classList.add(classNames)
+    }
+
+    for (const attrName in attributes) {
+      el[attrName] = attributes[attrName]
+    }
+
+    for (const attrName in customAttributes) {
+      el.setAttribute(attrName, customAttributes[attrName])
+    }
+
+    return el
   }
 }
 
