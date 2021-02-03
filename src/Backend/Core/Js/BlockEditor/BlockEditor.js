8b1db884b1ee5022c50a86ccb7fe935dad80d7de
Katrien Vanhaute <vanhautekatrien@gmail.com>
2021-02-03 08:07:58 +0100

    Use our own quote block so we can manage it more, like the layout

--- src/Backend/Core/Js/BlockEditor/BlockEditor.js
+++ src/Backend/Core/Js/BlockEditor/BlockEditor.js
@@ -3,10 +3,10 @@ import Embed from '@editorjs/embed'
 import Header from '@editorjs/header'
 import List from '@editorjs/list'
 import Paragraph from '@editorjs/paragraph'
-import Quote from '@editorjs/quote'
 import Underline from '@editorjs/underline'
 import MediaLibraryImage from './Blocks/MediaLibraryImage'
 import Button from './Blocks/Button'
+import Quote from './Blocks/Quote'
 
 export class BlockEditor {
   constructor () {
@@ -95,7 +95,7 @@ window.BlockEditor.blocks.Header = Header
 window.BlockEditor.blocks.Embed = Embed
 window.BlockEditor.blocks.List = List
 window.BlockEditor.blocks.Paragraph = Paragraph
-window.BlockEditor.blocks.Quote = Quote
 window.BlockEditor.blocks.Underline = Underline
 window.BlockEditor.blocks.MediaLibraryImage = MediaLibraryImage
 window.BlockEditor.blocks.Button = Button
+window.BlockEditor.blocks.Quote = Quote
