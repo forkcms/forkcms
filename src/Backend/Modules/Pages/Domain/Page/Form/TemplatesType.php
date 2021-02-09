96de0a2aa13b77c71ec7fde16de7cc20a03ac44d
Bjorn Van Acker <bjorn@sumocoders.be>
2021-02-09 14:25:07 +0100

    Get template data when building the view

--- src/Backend/Modules/Pages/Domain/Page/Form/TemplatesType.php
+++ src/Backend/Modules/Pages/Domain/Page/Form/TemplatesType.php
@@ -4,25 +4,14 @@ namespace Backend\Modules\Pages\Domain\Page\Form;
 
 use Backend\Modules\Extensions\Engine\Model as BackendExtensionsModel;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TemplatesType extends ChoiceType
 {
-    /** @var array */
-    private $templates;
-
-    public function __construct()
-    {
-        parent::__construct(null);
-        $this->templates = BackendExtensionsModel::getTemplates();
-    }
-
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        $view->vars['templates'] = $this->templates;
+        $view->vars['templates'] = BackendExtensionsModel::getTemplates();
         return parent::buildView($view, $form, $options);
     }
 }
