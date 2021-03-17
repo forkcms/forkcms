be470681797141fbfaf4aab147c9922be2bff96b
Bjorn Van Acker <bjorn@sumocoders.be>
2021-03-17 11:31:31 +0100

    Add ovverrideMapSettings to test_db.sql

--- tests/data/test_db.sql
+++ tests/data/test_db.sql
@@ -1117,6 +1117,7 @@ CREATE TABLE `location` (
   `showInOverview` tinyint(1) NOT NULL DEFAULT 1,
   `createdOn` datetime NOT NULL COMMENT '(DC2Type:datetime)',
   `editedOn` datetime NOT NULL COMMENT '(DC2Type:datetime)',
+  `overrideMapSettings` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
