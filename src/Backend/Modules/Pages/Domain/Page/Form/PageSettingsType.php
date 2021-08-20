c9e3632561acf9be4cc75c3f80a984951a73a436
Sander De la Marche <sander@sumocoders.be>
2021-08-20 16:10:39 +0200

    Remove old use statement

--- src/Backend/Modules/Pages/Domain/Page/Form/PageSettingsType.php
+++ src/Backend/Modules/Pages/Domain/Page/Form/PageSettingsType.php
@@ -8,7 +8,6 @@ use Common\Form\DatePickerType;
 use Common\Form\SwitchType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
