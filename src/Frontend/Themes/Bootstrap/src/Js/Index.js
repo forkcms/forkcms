79ee43f16e2c24acdfcb88f72467f5492546390b
Daphne Slootmans <daphne@sumocoders.be>
2022-10-05 10:58:27 +0200

    Re-order module imports in Index.js to comply with code style standards

--- src/Frontend/Themes/Bootstrap/src/Js/Index.js
+++ src/Frontend/Themes/Bootstrap/src/Js/Index.js
@@ -1,19 +1,17 @@
-/* External library imports */
+// /* External library imports */
 import * as bootstrap from 'bootstrap5/dist/js/bootstrap.js'
-window.bootstrap = bootstrap
 import '@fortawesome/fontawesome-free/js/all'
 // import '@fancyapps/fancybox/dist/jquery.fancybox'
-
-/* Utilities imports */
+// /* Utilities imports */
 import { ScrollTo } from './Utilities/ScrollTo'
 import { Resize } from './Utilities/Resize'
-/* import {Fancybox} from './Utilities/Fancybox' */
-
-/* Theme imports */
-/* eg. import tooltip from './Theme/Tooltip' */
+// /* import {Fancybox} from './Utilities/Fancybox' */
+// /* Theme imports */
+// /* eg. import tooltip from './Theme/Tooltip' */
 import { Pagination } from './Theme/Pagination'
 
 /* Renders */
+window.bootstrap = bootstrap
 window.scrollto = new ScrollTo()
 window.resizeFunction = new Resize()
 window.pagination = new Pagination()
