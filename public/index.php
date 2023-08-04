073e610835354636e0228a9617f26d7514ff7b74
Jelmer Prins <jelmer@pageon.be>
2023-08-04 11:03:16 +0200

    Public files belong in the public directory

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+use ForkCMS\Core\Domain\Kernel\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+require dirname(__DIR__).'/src/bootstrap.php';
+
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
