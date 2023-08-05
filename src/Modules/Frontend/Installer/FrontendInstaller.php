ef7a79a32ca1af4577d495d9632704122495a2b2
Jelmer Prins <jelmer@pageon.be>
2023-08-06 01:22:47 +0200

    Update frontend installer

--- src/Modules/Frontend/Installer/FrontendInstaller.php
+++ src/Modules/Frontend/Installer/FrontendInstaller.php
@@ -3,8 +3,10 @@
 namespace ForkCMS\Modules\Frontend\Installer;
 
 use ForkCMS\Modules\Extensions\Domain\Module\ModuleInstaller;
+use ForkCMS\Modules\Frontend\Backend\Actions\ModuleSettings;
 use ForkCMS\Modules\Frontend\Domain\Block\Block;
 use ForkCMS\Modules\Frontend\Domain\Meta\Meta;
+use ForkCMS\Modules\Internationalisation\Domain\Translation\TranslationKey;
 
 final class FrontendInstaller extends ModuleInstaller
 {
@@ -18,5 +20,15 @@ final class FrontendInstaller extends ModuleInstaller
     public function install(): void
     {
         $this->importTranslations(__DIR__ . '/../assets/installer/translations.xml');
+        $this->createBackendPages();
+    }
+
+    private function createBackendPages(): void
+    {
+        $this->getOrCreateBackendNavigationItem(
+            TranslationKey::label('Frontend'),
+            ModuleSettings::getActionSlug(),
+            $this->getModuleSettingsNavigationItem()
+        );
     }
 }
