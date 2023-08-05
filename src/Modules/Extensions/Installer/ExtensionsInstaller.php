19e5df6bc8724ff9e27fabd9c4f28b589c891eb4
Jelmer Prins <jelmer@pageon.be>
2023-08-06 01:11:58 +0200

    Update extensions module installer

--- src/Modules/Extensions/Installer/ExtensionsInstaller.php
+++ src/Modules/Extensions/Installer/ExtensionsInstaller.php
@@ -2,6 +2,20 @@
 
 namespace ForkCMS\Modules\Extensions\Installer;
 
+use ForkCMS\Modules\Extensions\Backend\Actions\ModuleDetail;
+use ForkCMS\Modules\Extensions\Backend\Actions\ModuleIndex;
+use ForkCMS\Modules\Extensions\Backend\Actions\ModuleInstall;
+use ForkCMS\Modules\Extensions\Backend\Actions\ModuleUpload;
+use ForkCMS\Modules\Extensions\Backend\Actions\ThemeActivate;
+use ForkCMS\Modules\Extensions\Backend\Actions\ThemeDetail;
+use ForkCMS\Modules\Extensions\Backend\Actions\ThemeIndex;
+use ForkCMS\Modules\Extensions\Backend\Actions\ThemeInstall;
+use ForkCMS\Modules\Extensions\Backend\Actions\ThemeTemplateAdd;
+use ForkCMS\Modules\Extensions\Backend\Actions\ThemeTemplateDelete;
+use ForkCMS\Modules\Extensions\Backend\Actions\ThemeTemplateEdit;
+use ForkCMS\Modules\Extensions\Backend\Actions\ThemeTemplateExport;
+use ForkCMS\Modules\Extensions\Backend\Actions\ThemeTemplateIndex;
+use ForkCMS\Modules\Extensions\Backend\Actions\ThemeUpload;
 use ForkCMS\Modules\Extensions\Domain\Module\Module;
 use ForkCMS\Modules\Extensions\Domain\Module\ModuleInstaller;
 use ForkCMS\Modules\Extensions\Domain\Theme\Command\ActivateTheme;
@@ -9,6 +23,7 @@ use ForkCMS\Modules\Extensions\Domain\Theme\Command\InstallTheme;
 use ForkCMS\Modules\Extensions\Domain\Theme\Theme;
 use ForkCMS\Modules\Extensions\Domain\Theme\ThemeRepository;
 use ForkCMS\Modules\Extensions\Domain\ThemeTemplate\ThemeTemplate;
+use ForkCMS\Modules\Internationalisation\Domain\Translation\TranslationKey;
 
 final class ExtensionsInstaller extends ModuleInstaller
 {
@@ -26,7 +41,50 @@ final class ExtensionsInstaller extends ModuleInstaller
     public function install(): void
     {
         $this->importTranslations(__DIR__ . '/../assets/installer/translations.xml');
+        $this->createBackendPages();
         $this->installDefaultTheme();
+    }
+
+    private function createBackendPages(): void
+    {
+        $this->getOrCreateBackendNavigationItem(
+            TranslationKey::label('Modules'),
+            ModuleIndex::getActionSlug(),
+            $this->getModuleSettingsNavigationItem(),
+            [
+                ModuleDetail::getActionSlug(),
+                ModuleInstall::getActionSlug(),
+                ModuleUpload::getActionSlug(),
+            ],
+            0
+        );
+        $themeSettings = $this->getOrCreateBackendNavigationItem(
+            TranslationKey::label('Themes'),
+            ThemeIndex::getActionSlug(),
+            $this->getSettingsNavigationItem(),
+        );
+        $this->getOrCreateBackendNavigationItem(
+            TranslationKey::label('ThemeSelection'),
+            ThemeIndex::getActionSlug(),
+            $themeSettings,
+            [
+                ThemeDetail::getActionSlug(),
+                ThemeInstall::getActionSlug(),
+                ThemeActivate::getActionSlug(),
+                ThemeUpload::getActionSlug(),
+            ]
+        );
+        $this->getOrCreateBackendNavigationItem(
+            TranslationKey::label('ThemeTemplates'),
+            ThemeTemplateIndex::getActionSlug(),
+            $themeSettings,
+            [
+                ThemeTemplateAdd::getActionSlug(),
+                ThemeTemplateEdit::getActionSlug(),
+                ThemeTemplateDelete::getActionSlug(),
+                ThemeTemplateExport::getActionSlug(),
+            ]
+        );
     }
 
     private function installDefaultTheme(): void
