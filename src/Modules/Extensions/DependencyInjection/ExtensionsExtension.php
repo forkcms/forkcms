1b5048aa6680a3251a11e9b7799b69a0433d3492
Jelmer Prins <jelmer@pageon.be>
2023-08-05 15:16:28 +0200

    Integrate the modules with liip imagine

--- src/Modules/Extensions/DependencyInjection/ExtensionsExtension.php
+++ src/Modules/Extensions/DependencyInjection/ExtensionsExtension.php
@@ -3,6 +3,8 @@
 namespace ForkCMS\Modules\Extensions\DependencyInjection;
 
 use ForkCMS\Core\Domain\DependencyInjection\ForkModuleExtension;
+use ForkCMS\Modules\Extensions\Domain\Module\InstalledModules;
+use ForkCMS\Modules\Extensions\Domain\Theme\ThemeRepository;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class ExtensionsExtension extends ForkModuleExtension
@@ -15,5 +17,27 @@ class ExtensionsExtension extends ForkModuleExtension
     public function prepend(ContainerBuilder $container): void
     {
         $this->getLoader($container)->load('doctrine.yaml');
+        $this->configureLiipImagine($container);
+    }
+
+    private function configureLiipImagine(ContainerBuilder $container): void
+    {
+        $modulesDirectory = $container->getParameter('kernel.project_dir') . '/src/Modules/';
+        $dataRoots = [];
+        foreach (InstalledModules::fromContainer($container)() as $moduleName) {
+            $dataRoots[$moduleName . 'Module'] = $modulesDirectory . $moduleName . '/assets/public';
+        }
+        foreach (ThemeRepository::getThemePaths() as $themeName => $path) {
+            $dataRoots[$themeName . 'Theme'] = $path . '/assets/public';
+        }
+        $container->prependExtensionConfig('liip_imagine', [
+            'loaders' => [
+                'default' => [
+                    'filesystem' => [
+                        'data_root' => ['%kernel.project_dir%/public'] + array_filter($dataRoots, 'is_dir'),
+                    ],
+                ],
+            ],
+        ]);
     }
 }
