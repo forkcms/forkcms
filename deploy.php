257a5fe58d4dc222996f6f9b7d4d220f4baef29e
Bjorn Van Acker <bjorn@sumocoders.be>
2023-09-14 20:08:23 +0200

    Use the new deployer-sumo-forkcms bundle

--- deploy.php
+++ deploy.php
@@ -8,7 +8,7 @@ use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 
 require 'recipe/symfony.php';
 require 'contrib/cachetool.php';
-require __DIR__ . '/vendor/tijsverkoyen/deployer-sumo/sumo.php';
+require __DIR__ . '/vendor/sumocoders/deployer-sumo-forkcms/sumo.php';
 
 // Define some variables
 set('client', '');
@@ -83,143 +83,6 @@ set('bin/composer', function () {
 
 set('keep_releases', 3);
 
-// Upload the assets
-desc('Uploads the assets');
-task(
-    'sumo:assets:upload',
-    function () {
-        upload('public/assets', '{{release_path}}/public');
-    }
-);
-
-desc('Ask if the .env.local is filled in correctly');
-task('sumo:check:env', function () {
-    askConfirmation('Is the .env.local filled in correctly?');
-});
-
-$databaseUtility = new Database();
-
-desc('Replace the local database with the remote database');
-task(
-    'sumo:db:get',
-    function () use ($databaseUtility) {
-        $remoteHost = Configuration::fromRemote()->get('FORK_DATABASE_HOST');
-        $remotePort = Configuration::fromRemote()->get('FORK_DATABASE_PORT');
-        $remoteName = Configuration::fromRemote()->get('FORK_DATABASE_NAME');
-        $remoteUser = Configuration::fromRemote()->get('FORK_DATABASE_USER');
-        $remotePassword = Configuration::fromRemote()->get('FORK_DATABASE_PASSWORD');
-
-        $localHost = Configuration::fromLocal()->get('FORK_DATABASE_HOST');
-        $localPort = Configuration::fromLocal()->get('FORK_DATABASE_PORT');
-        $localName = Configuration::fromLocal()->get('FORK_DATABASE_NAME');
-        $localUser = Configuration::fromLocal()->get('FORK_DATABASE_USER');
-        $localPassword = Configuration::fromLocal()->get('FORK_DATABASE_PASSWORD');
-
-        $remoteDatabaseUrl = parse_url("mysql://{$remoteUser}:{$remotePassword}@{$remoteHost}:{$remotePort}/{$remoteName}?serverVersion=5.7&charset=utf8mb4");
-        $localDatabaseUrl = parse_url("mysql://{$localUser}:{$localPassword}@{$localHost}:{$localPort}/{$localName}?serverVersion=5.7&charset=utf8mb4");
-
-        run(
-            sprintf(
-                'mysqldump --lock-tables=false --set-charset %1$s %2$s > {{deploy_path}}/db_download.tmp.sql',
-                $databaseUtility->getConnectionOptions($remoteDatabaseUrl),
-                $databaseUtility->getNameFromConnectionOptions($remoteDatabaseUrl)
-            )
-        );
-        download(
-            '{{deploy_path}}/db_download.tmp.sql',
-            './db_download.tmp.sql'
-        );
-        run('rm {{deploy_path}}/db_download.tmp.sql');
-
-        runLocally(
-            sprintf(
-                'mysql %1$s %2$s < ./db_download.tmp.sql',
-                $databaseUtility->getConnectionOptions($localDatabaseUrl),
-                $databaseUtility->getNameFromConnectionOptions($localDatabaseUrl)
-            )
-        );
-        runLocally('rm ./db_download.tmp.sql');
-    }
-);
-
-desc('Replace the remote database with the local database');
-task(
-    'sumo:db:put',
-    function () use ($databaseUtility) {
-        $remoteHost = Configuration::fromRemote()->get('FORK_DATABASE_HOST');
-        $remotePort = Configuration::fromRemote()->get('FORK_DATABASE_PORT');
-        $remoteName = Configuration::fromRemote()->get('FORK_DATABASE_NAME');
-        $remoteUser = Configuration::fromRemote()->get('FORK_DATABASE_USER');
-        $remotePassword = Configuration::fromRemote()->get('FORK_DATABASE_PASSWORD');
-
-        $localHost = Configuration::fromLocal()->get('FORK_DATABASE_HOST');
-        $localPort = Configuration::fromLocal()->get('FORK_DATABASE_PORT');
-        $localName = Configuration::fromLocal()->get('FORK_DATABASE_NAME');
-        $localUser = Configuration::fromLocal()->get('FORK_DATABASE_USER');
-        $localPassword = Configuration::fromLocal()->get('FORK_DATABASE_PASSWORD');
-
-        $remoteDatabaseUrl = parse_url("mysql://{$remoteUser}:{$remotePassword}@{$remoteHost}:{$remotePort}/{$remoteName}?serverVersion=5.7&charset=utf8mb4");
-        $localDatabaseUrl = parse_url("mysql://{$localUser}:{$localPassword}@{$localHost}:{$localPort}/{$localName}?serverVersion=5.7&charset=utf8mb4");
-
-        // create a backup
-        // @todo make separate backup dir
-        run(
-            sprintf(
-                'mysqldump --lock-tables=false --set-charset %1$s %2$s > {{deploy_path}}/backup_%3$s.sql',
-                $databaseUtility->getConnectionOptions($remoteDatabaseUrl),
-                $databaseUtility->getNameFromConnectionOptions($remoteDatabaseUrl),
-                date('YmdHi')
-            )
-        );
-
-        runLocally(
-            sprintf(
-                'mysqldump --column-statistics=0 --lock-tables=false --set-charset %1$s %2$s > ./db_upload.tmp.sql',
-                $databaseUtility->getConnectionOptions($localDatabaseUrl),
-                $databaseUtility->getNameFromConnectionOptions($localDatabaseUrl)
-            )
-        );
-
-        upload('./db_upload.tmp.sql', '{{deploy_path}}/db_upload.tmp.sql');
-        runLocally('rm ./db_upload.tmp.sql');
-
-        run(
-            sprintf(
-                'mysql %1$s %2$s < {{deploy_path}}/db_upload.tmp.sql',
-                $databaseUtility->getConnectionOptions($remoteDatabaseUrl),
-                $databaseUtility->getNameFromConnectionOptions($remoteDatabaseUrl)
-            )
-        );
-        run('rm {{deploy_path}}/db_upload.tmp.sql');
-    }
-);
-
-desc('Cleanup the codebase');
-task('sumo:files:cleanup', function () {
-    run('rm -rf {{release_path}}/.github');
-    run('rm -rf {{release_path}}/.git');
-    run('rm -rf {{release_path}}/.gitattributes');
-    run('rm -rf {{release_path}}/Dockerfile');
-    run('rm -rf {{release_path}}/docker-compose.yml');
-    run('rm -rf {{release_path}}/.scrutinizer.yml');
-    run('rm -rf {{release_path}}/.codecov.yml');
-    run('rm -rf {{release_path}}/php.ini');
-    run('rm -rf {{release_path}}/phpunit.xml.dist');
-    run('rm -rf {{release_path}}/phpstan.neon');
-    run('rm -rf {{release_path}}/UPGRADE***');
-    run('rm -rf {{release_path}}/var/docs');
-    run('rm -rf {{release_path}}/tests');
-    run('rm -rf {{release_path}}/var/docker');
-    run('rm -rf {{release_path}}/.gitlab-ci');
-    run('rm -rf {{release_path}}/.phpcs.xml.dist');
-    run('rm -rf {{release_path}}/.gitlab-ci.yml');
-    run('rm -rf {{release_path}}/.stylelintignore');
-    run('rm -rf {{release_path}}/.stylelintrc');
-    run('rm -rf {{release_path}}/.editorconfig');
-    run('rm -rf {{release_path}}/.dockerignore');
-    run('rm -rf {{release_path}}/CHANGELOG.nd');
-});
-
 /**********************
  * Flow configuration *
  **********************/
@@ -227,9 +90,5 @@ task('sumo:files:cleanup', function () {
 after('deploy:symlink', 'cachetool:clear:opcache');
 // Unlock the deploy when it failed
 after('deploy:failed', 'deploy:unlock');
-// Check config before migrating database
-before('database:migrate', 'sumo:check:env');
 // Migrate database before symlink new release.
 before('deploy:symlink', 'database:migrate');
-// Remove unneeded files
-before('deploy:symlink', 'sumo:files:cleanup');
