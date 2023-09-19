f28dcca3154af210796750466db630f00fb01a70
Davy De Vuysdere <davy@sumocoders.be>
2023-09-19 15:12:56 +0200

    typo in CreateContentBlock

--- src/Modules/ContentBlocks/Domain/ContentBlock/Command/CreateContentBlockHandler.php
+++ src/Modules/ContentBlocks/Domain/ContentBlock/Command/CreateContentBlockHandler.php
@@ -9,7 +9,6 @@ use ForkCMS\Modules\ContentBlocks\Frontend\Widgets\Detail as DetailWidget;
 use ForkCMS\Modules\Frontend\Domain\Block\Block;
 use ForkCMS\Modules\Frontend\Domain\Block\BlockRepository;
 use ForkCMS\Modules\Frontend\Domain\Block\ModuleBlock;
-use ForkCMS\Modules\Internationalisation\Domain\Locale\Locale;
 use ForkCMS\Modules\Internationalisation\Domain\Translation\TranslationKey;
 
 final class CreateContentBlockHandler implements CommandHandlerInterface
@@ -22,24 +21,16 @@ final class CreateContentBlockHandler implements CommandHandlerInterface
 
     public function __invoke(CreateContentBlock $createContentBlock): void
     {
-        $createContentBlock->widget = $this->createWidget($createContentBlock->title, $createContentBlock->locale);
+        $createContentBlock->widget = new Block(
+            ModuleBlock::fromFQCN(DetailWidget::class),
+            TranslationKey::label('ContentBlock'),
+            locale: $createContentBlock->locale
+        );
+
         $createContentBlock->id = $this->contentBlockRepository->getNextIdForLocale($createContentBlock->locale);
 
         $contentBlock = ContentBlock::fromDataTransferObject($createContentBlock);
         $this->contentBlockRepository->save($contentBlock);
         $createContentBlock->setEntity($contentBlock);
-    }
-
-    private function createWidget(string $title, Locale $locale): Block
-    {
-        $block = new Block(
-            ModuleBlock::fromFQCN(DetailWidget::class),
-            TranslationKey::label('ContentBlock'),
-            locale: $locale
-        );
-        $block->getSettings()->set('label', $title);
-        $this->blockRepository->save($block);
-
-        return $block;
     }
 }
