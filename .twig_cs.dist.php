486f5768a8a17fad2f3655dddda19fb1bdab0ebc
Bjorn Van Acker <bjorn@sumocoders.be>
2023-09-19 15:56:31 +0200

    Add the templates directory to the twigcs config

--- .twig_cs.dist.php
+++ .twig_cs.dist.php
@@ -8,6 +8,9 @@ return Twigcs\Config\Config::create()
     ->addFinder(
         Twigcs\Finder\TemplateFinder::create()->in(__DIR__ . '/src')
     )
+    ->addFinder(
+        Twigcs\Finder\TemplateFinder::create()->in(__DIR__ . '/templates')
+    )
     ->setName('my-config')
     ->setSeverity('warning')
     ->setReporter('json')
