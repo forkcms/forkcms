9b49905674b4838567fe7ede4bcaeb26b47f0aa1
Davy De Vuysdere <davy@sumocoders.be>
2023-09-19 10:56:48 +0200

    enum values in uppercase

--- src/Modules/ContentBlocks/Domain/ContentBlock/Status.php
+++ src/Modules/ContentBlocks/Domain/ContentBlock/Status.php
@@ -6,64 +6,6 @@ use InvalidArgumentException;
 
 enum Status: string
 {
-    case Archived = 'archived';
-    case Active = 'active';
-
-    /*
-    private string $status;
-
-    private function __construct(string $status)
-    {
-        $this->setStatus($status);
-    }
-
-    public static function getPossibleStatuses(): array
-    {
-        return [
-            self::STATUS_ARCHIVED,
-            self::STATUS_ACTIVE,
-        ];
-    }
-
-    public static function fromString($status): self
-    {
-        return new self($status);
-    }
-
-    private function setStatus(string $status): self
-    {
-        if (!in_array($status, self::getPossibleStatuses(), true)) {
-            throw new InvalidArgumentException('Invalid status');
-        }
-
-        $this->status = $status;
-
-        return $this;
-    }
-
-    public function __toString(): string
-    {
-        return $this->status;
-    }
-
-    public function isActive(): bool
-    {
-        return $this->status === self::STATUS_ACTIVE;
-    }
-
-    public function isArchived(): bool
-    {
-        return $this->status === self::STATUS_ARCHIVED;
-    }
-
-    public static function active(): self
-    {
-        return new self(self::STATUS_ACTIVE);
-    }
-
-    public static function archived(): self
-    {
-        return new self(self::STATUS_ARCHIVED);
-    }
-    */
+    case Archived = 'ARCHIVED';
+    case Active = 'ACTIVE';
 }
