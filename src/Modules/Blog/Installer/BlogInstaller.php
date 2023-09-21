9264889da848d7c044fd3fb8f1a68055111940f0
Davy De Vuysdere <davy@sumocoders.be>
2023-09-21 14:27:21 +0200

    backend navigation -> put under modules

--- src/Modules/Blog/Installer/BlogInstaller.php
+++ src/Modules/Blog/Installer/BlogInstaller.php
@@ -4,6 +4,9 @@ namespace ForkCMS\Modules\Blog\Installer;
 
 use ForkCMS\Modules\Blog\Backend\Actions\BlogIndex;
 use ForkCMS\Modules\Blog\Backend\Actions\BlogPostAdd;
+use ForkCMS\Modules\Blog\Backend\Actions\CategoryAdd;
+use ForkCMS\Modules\Blog\Backend\Actions\CategoryEdit;
+use ForkCMS\Modules\Blog\Backend\Actions\CategoryIndex;
 use ForkCMS\Modules\Blog\Domain\BlogPost\BlogPost;
 use ForkCMS\Modules\Blog\Domain\Category\Category;
 use ForkCMS\Modules\Blog\Domain\Comment\Comment;
@@ -28,13 +31,31 @@ final class BlogInstaller extends ModuleInstaller
 
     private function createBackendPages(): void
     {
-        $this->getOrCreateBackendNavigationItem(
+        $modulesNavigationItem = $this->getModulesNavigationItem();
+        $blogNavigationitem = $this->getOrCreateBackendNavigationItem(
             label: TranslationKey::label('Blog'),
+            parent: $modulesNavigationItem
+        );
+
+        $this->getOrCreateBackendNavigationItem(
+            label: TranslationKey::label('Articles'),
             slug: BlogIndex::getActionSlug(),
+            parent: $blogNavigationitem,
             selectedFor: [
                 BlogPostAdd::getActionSlug(),
             ],
             sequence: 1,
+        );
+
+        $this->getOrCreateBackendNavigationItem(
+            label: TranslationKey::label('Categories'),
+            slug: CategoryIndex::getActionSlug(),
+            parent: $blogNavigationitem,
+            selectedFor: [
+                CategoryAdd::getActionSlug(),
+                CategoryEdit::getActionSlug(),
+            ],
+            sequence: 2,
         );
     }
 }
