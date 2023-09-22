59b9920a1947fa803d9892bee123dc3a5597b735
Daphne Slootmans <daphne@sumocoders.be>
2023-09-22 15:21:16 +0200

    Initialise toast present on page load and add progress bar for autohide

---
+++ src/Modules/Backend/assets/Backend/webpack/js/Components/InitToasts.js
@@ -0,0 +1,15 @@
+export class InitBsToasts {
+  constructor () {
+    this.initToasts()
+  }
+
+  // init toasts present on page load
+  initToasts () {
+    const messageNodes = document.querySelector('[data-messaging-wrapper]').querySelectorAll('.toast')
+    const messageNodesList = Array.from(messageNodes)
+    messageNodesList.forEach(message => {
+      const toast = window.bootstrap.Toast.getOrCreateInstance(message)
+      if (message.classList.contains('to-show')) toast.show()
+    })
+  }
+}
