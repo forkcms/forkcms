e58df99951a575affdab8280538bb9d7c94f0e09
Bjorn Van Acker <bjorn@sumocoders.be>
2023-09-27 11:10:14 +0200

    Clear cache after changes in 2fa

--- src/Modules/Backend/Backend/Actions/ModuleSettings.php
+++ src/Modules/Backend/Backend/Actions/ModuleSettings.php
@@ -2,8 +2,16 @@
 
 namespace ForkCMS\Modules\Backend\Backend\Actions;
 
+use ForkCMS\Core\Domain\Header\FlashMessage\FlashMessage;
+use ForkCMS\Core\Domain\Kernel\Command\ClearContainerCache;
 use ForkCMS\Modules\Backend\Domain\Action\AbstractFormActionController;
 use ForkCMS\Modules\Backend\Domain\ModuleSettings\ModuleSettingsType;
+use ForkCMS\Modules\Extensions\Domain\Module\Command\ChangeModuleSettings;
+use ForkCMS\Modules\Extensions\Domain\Module\Module;
+use ForkCMS\Modules\Extensions\Domain\Module\ModuleName;
+use RuntimeException;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -16,9 +24,24 @@ final class ModuleSettings extends AbstractFormActionController
 
     protected function getFormResponse(Request $request): ?Response
     {
-        return $this->handleModuleSettingsForm(
-            $request,
-            ModuleSettingsType::class,
+        $moduleRepository = $this->getRepository(Module::class);
+        $moduleName = $this->getModuleName();
+
+        return $this->handleForm(
+            request: $request,
+            formType: ModuleSettingsType::class,
+            formData: new ChangeModuleSettings(
+                $moduleRepository->find(ModuleName::core()) ?? throw new RuntimeException('Core module not found'),
+                $moduleRepository->find($moduleName) ?? throw new RuntimeException($moduleName . ' module not found'),
+                []
+            ),
+            flashMessage: FlashMessage::success('SettingsSaved'),
+            validCallback: function (FormInterface $form): ?Response {
+                $this->commandBus->dispatch($form->getData());
+                $this->commandBus->dispatch(new ClearContainerCache());
+
+                return new RedirectResponse(self::getActionSlug()->generateRoute($this->router));
+            }
         );
     }
 }
