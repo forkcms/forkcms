fd23b5c56d616de23613003aa483fd1a4f5d6d57
Jelmer Prins <jelmer@pageon.be>
2023-09-28 18:17:23 +0200

    Add package to speed up functional testing

--- var/docker/db/scripts/00_init_test_db.sh
+++ var/docker/db/scripts/00_init_test_db.sh
@@ -8,9 +8,6 @@ MYSQL_DATABASE_TEST="${MYSQL_DATABASE}_test"
 echo "Creating database ${MYSQL_DATABASE_TEST}..."
 mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "create database ${MYSQL_DATABASE_TEST};"
 
-echo "Importing fixtures into database ${MYSQL_DATABASE_TEST}..."
-mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" ${MYSQL_DATABASE_TEST} < ../test_db.sql
-
 echo "Granting all privileges for user $MYSQL_USER on database ${MYSQL_DATABASE_TEST}..."
 mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL ON \`${MYSQL_DATABASE_TEST}\`.* TO '$MYSQL_USER'@'%' ;"
 
