4fa40ab510eae08c0c5a83fa0f9a1d079f663216
Jelmer Prins <jelmer@pageon.be>
2023-09-30 02:46:47 +0200

    Add tests for adding users

---
+++ src/Modules/Backend/tests/Backend/Actions/UserAddTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace ForkCMS\Modules\Backend\tests\Backend\Actions;
+
+use ForkCMS\Modules\Backend\tests\BackendWebTestCase;
+
+final class UserAddTest extends BackendWebTestCase
+{
+    protected const TEST_URL = '/private/en/backend/user-add';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testPageLoads(): void
+    {
+        self::loginBackendUser();
+        self::assertPageLoadedCorrectly(
+            self::TEST_URL,
+            'Add | users | settings | Fork CMS | Fork CMS',
+            [
+                'Display name',
+                'E-mail',
+                'Super admin (grant access to everything)',
+                'Enable CMS access for this account.',
+                'Short date format',
+            ]
+        );
+        self::assertHasLink('Cancel', '/private/en/backend/user-index');
+    }
+
+    public function testEmptyFormShowsValidationErrors(): void
+    {
+        self::loadPage();
+
+        $form = self::getCrawler()->selectButton('Add')->form();
+        self::getClient()->submit($form);
+        self::assertMinCount(
+            3,
+            self::getCrawler()->filter('#content form[name="user"] .form-control.is-invalid'),
+            'Not all required fields are marked as invalid.'
+        );
+    }
+
+    public function testValidData(): void
+    {
+        self::loadPage();
+
+        $form = self::getCrawler()->selectButton('Add')->form();
+        self::getClient()->submit(
+            $form,
+            [
+                'user[user][ec05aaca240e74a0604d93f9e5a7caef][displayName]' => 'Jelmer Prins',
+                'user[user][ec05aaca240e74a0604d93f9e5a7caef][email]' => 'jelmer.prins',
+                'user[user][ec05aaca240e74a0604d93f9e5a7caef][plainTextPassword][first]' => 'I<3ForkCMS',
+                'user[user][ec05aaca240e74a0604d93f9e5a7caef][plainTextPassword][second]' => 'I<3ForkCMS',
+            ]
+        );
+        self::assertResponseHasContent('The password is too short.', 'Please provide a valid e-mail address.');
+    }
+
+    public function testUniqueness(): void
+    {
+        $user = self::loginBackendUser();
+        self::loadPage(loginBackendUser: false);
+
+        $form = self::getCrawler()->selectButton('Add')->form();
+        self::getClient()->submit(
+            $form,
+            [
+                'user[user][ec05aaca240e74a0604d93f9e5a7caef][displayName]' => $user->getDisplayName(),
+                'user[user][ec05aaca240e74a0604d93f9e5a7caef][email]' =>  $user->getEmail(),
+                'user[user][ec05aaca240e74a0604d93f9e5a7caef][plainTextPassword][first]' => 'IAbsolutely<3ForkCMS',
+                'user[user][ec05aaca240e74a0604d93f9e5a7caef][plainTextPassword][second]' => 'IAbsolutely<3ForkCMS',
+            ]
+        );
+        self::assertResponseHasContent('This e-mailaddress is in use.', 'This display name is in use.');
+    }
+
+    public function testSubmittedFormRedirectsToIndex(): void
+    {
+        self::loadPage();
+
+        $form = self::getCrawler()->selectButton('Add')->form();
+        self::getClient()->submit(
+            $form,
+            [
+                'user[user][ec05aaca240e74a0604d93f9e5a7caef][displayName]' => 'Jelmer Prins',
+                'user[user][ec05aaca240e74a0604d93f9e5a7caef][email]' => 'jelmer.prins@fork-cms.com',
+                'user[user][ec05aaca240e74a0604d93f9e5a7caef][plainTextPassword][first]' => 'IAbsolutely<3ForkCMS',
+                'user[user][ec05aaca240e74a0604d93f9e5a7caef][plainTextPassword][second]' => 'IAbsolutely<3ForkCMS',
+            ]
+        );
+        self::getClient()->followRedirect();
+        self::assertCurrentUrlEndsWith('/private/en/backend/user-index');
+        self::assertDataGridHasLink('jelmer.prins@fork-cms.com');
+        self::assertResponseHasContent('The user "Jelmer Prins" was added.');
+    }
+}
