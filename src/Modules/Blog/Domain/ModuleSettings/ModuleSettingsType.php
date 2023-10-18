e6a76b01b6c445f8e071d50ebcb97fea83f8ce43
Davy De Vuysdere <davy@sumocoders.be>
2023-10-18 15:33:37 +0200

    Blog: settings

--- src/Modules/Blog/Domain/ModuleSettings/ModuleSettingsType.php
+++ src/Modules/Blog/Domain/ModuleSettings/ModuleSettingsType.php
@@ -3,18 +3,32 @@
 namespace ForkCMS\Modules\Blog\Domain\ModuleSettings;
 
 use ForkCMS\Core\Domain\Form\FieldsetType;
+use ForkCMS\Modules\Backend\Domain\User\User;
 use ForkCMS\Modules\Extensions\Domain\Module\Command\ChangeModuleSettings;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ModuleSettingsType extends AbstractType
 {
+    public function __construct(
+        private readonly Security $security,
+        private readonly TranslatorInterface $translator
+    ) {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $settings = $builder->getData();
+        /** @var User $user */
+        $user = $this->security->getUser();
+        $workingLocale = $this->translator->getLocale();
 
         $builder
             ->add(
@@ -59,6 +73,127 @@ class ModuleSettingsType extends AbstractType
                             )
                         ;
                     },
+                ]
+            )
+            ->add(
+                'comments',
+                FieldsetType::class,
+                [
+                    'label' => 'lbl.Comments',
+                    'fields' => static function (FormBuilderInterface $builder): void {
+                        $builder
+                            ->add(
+                                'allow_comments',
+                                CheckboxType::class,
+                                [
+                                    'required' => false,
+                                    'label' => 'lbl.AllowComments',
+                                ]
+                            )
+                            ->add(
+                                'moderation',
+                                CheckboxType::class,
+                                [
+                                    'required' => false,
+                                    'label' => 'lbl.EnableModeration',
+                                ]
+                            )
+                        ;
+                    }
+                ]
+            )
+        ;
+
+        if ($user->isSuperAdmin()) {
+            $builder->add(
+                'image',
+                FieldsetType::class,
+                [
+                    'label' => 'lbl.Image',
+                    'fields' => static function (FormBuilderInterface $builder): void {
+                        $builder->add(
+                            'show_image_form',
+                            CheckboxType::class,
+                            [
+                                'label' => 'msg.ShowImageForm',
+                                'required' => false,
+                            ]
+                        );
+                    }
+                ]
+            );
+        }
+
+        $builder
+            ->add(
+                'notifications',
+                FieldsetType::class,
+                [
+                    'label' => 'lbl.Notifications',
+                    'fields' => static function (FormBuilderInterface $builder): void {
+                        $builder
+                            ->add(
+                                'notify_by_email_on_new_comment_to_moderate',
+                                CheckboxType::class,
+                                [
+                                    'required' => false,
+                                    'label' => 'msg.NotifyByEmailOnNewCommentToModerate',
+                                ]
+                            )
+                            ->add(
+                                'notify_by_email_on_new_comment',
+                                CheckboxType::class,
+                                [
+                                    'required' => false,
+                                    'label' => 'msg.NotifyByEmailOnNewComment',
+                                ]
+                            )
+                        ;
+                    }
+                ]
+            )
+            ->add(
+                'rssFeed',
+                FieldsetType::class,
+                [
+                    'label' => 'lbl.RSSFeed',
+                    'fields' => static function (FormBuilderInterface $builder) use ($settings, $workingLocale): void {
+                        $builder
+                            ->add(
+                                'rss_title_' . $workingLocale,
+                                TextType::class,
+                                [
+                                    'label' => 'lbl.Title',
+                                    'required' => true,
+                                    'data' => $settings->__get('rss_title_' . $workingLocale) ?? 'RSS',
+                                    'help' => 'msg.HelpRSSTitle',
+                                ]
+                            )
+                            ->add(
+                                'rss_description_' . $workingLocale,
+                                TextareaType::class,
+                                [
+                                    'label' => 'lbl.Description',
+                                    'required' => false,
+                                    'data' => $settings->__get('rss_description_' . $workingLocale) ?? '',
+                                    'help' => 'msg.HelpRSSDescription',
+                                    'attr' => [
+                                        'rows' => 5,
+                                    ]
+                                ]
+                            )
+                            ->add(
+                                'rss_meta_' . $workingLocale,
+                                CheckboxType::class,
+                                [
+                                    'label' => 'lbl.Meta',
+                                    'help' => 'msg.HelpMeta',
+                                    'required' => false,
+                                    'data' => $settings->__get('rss_meta_' . $workingLocale) ?? true,
+                                ]
+                            )
+                        ;
+                    }
                 ]
             )
         ;
