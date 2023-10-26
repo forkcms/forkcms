0260257ac19e17e3ca5303f0b592003e3aab33a0
Davy De Vuysdere <davy@sumocoders.be>
2023-10-26 10:26:50 +0200

    workaround / quickfix getRouteForBlock for frontend actions/widgets

--- src/Modules/Pages/Domain/Page/PageRouter.php
+++ src/Modules/Pages/Domain/Page/PageRouter.php
@@ -29,16 +29,29 @@ final class PageRouter implements BlockRouterInterface
         }
         $locale = $locale ?? Locale::current();
 
+        // TODO this is quick fix, create issue on github or discuss with Jelmer
+        if (str_contains($moduleBlock->getFQCN(), '\\Actions\\')) {
+            $moduleName = 'action__' . $moduleBlock->getName();
+        } elseif (str_contains($moduleBlock->getFQCN(), '\\Widget\\')) {
+            $moduleName = 'widget__' . $moduleBlock->getName();
+        } else {
+            $moduleName = $moduleBlock->getName();
+        }
+
         $page = $this->pageRepository->createQueryBuilder('p')
             ->innerJoin('p.revisions', 'r', Join::WITH, 'r.isDraft = :draft AND r.locale = :locale')
             ->innerJoin('r.blocks', 'pb')
             ->innerJoin('pb.block', 'fb', Join::WITH, 'fb.block.module = :module AND fb.block.name = :name')
             ->setParameter('module', $moduleBlock->getModule())
-            ->setParameter('name', $moduleBlock->getName())
+            ->setParameter('name', $moduleName)
             ->setParameter('locale', $locale->value)
             ->setParameter('draft', false)
             ->getQuery()
-            ->getSingleResult();
+            ->getOneOrNullResult();
+
+        if ($page === null) {
+            return $this->getRouteForPageId(Page::PAGE_ID_404, $locale);
+        }
 
         return $this->getRouteForPage($page, $locale, $parameters, $referenceType);
     }
