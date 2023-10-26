28950ff4af60426cf2fabb0c5083773a41dbe660
Davy De Vuysdere <davy@sumocoders.be>
2023-10-26 12:11:09 +0200

    rudimentary getUrlForBlock method

--- src/Core/Domain/Twig/RoutingExtension.php
+++ src/Core/Domain/Twig/RoutingExtension.php
@@ -2,13 +2,18 @@
 
 namespace ForkCMS\Core\Domain\Twig;
 
+use Doctrine\ORM\Query\Expr\Join;
 use ForkCMS\Modules\Backend\Domain\Action\ActionName;
 use ForkCMS\Modules\Backend\Domain\Action\ActionSlug;
 use ForkCMS\Modules\Extensions\Domain\Module\ModuleName;
 use ForkCMS\Modules\Internationalisation\Domain\Locale\Locale;
+use ForkCMS\Modules\Pages\Domain\Page\Page;
+use ForkCMS\Modules\Pages\Domain\Page\PageRepository;
+use ForkCMS\Modules\Pages\Domain\Page\PageRouter;
 use Symfony\Bridge\Twig\Extension\RoutingExtension as TwigBridgeRoutingExtension;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -17,7 +22,9 @@ final class RoutingExtension extends AbstractExtension
     public function __construct(
         private UrlGeneratorInterface $generator,
         private TwigBridgeRoutingExtension $twigBridgeRoutingExcension,
-        private RequestStack $requestStack
+        private RequestStack $requestStack,
+        private readonly PageRepository $pageRepository,
+        private readonly PageRouter $pageRouter
     ) {
     }
 
@@ -33,6 +40,10 @@ final class RoutingExtension extends AbstractExtension
                 'action_path',
                 [$this, 'getPath'],
                 ['is_safe_callback' => [$this->twigBridgeRoutingExcension, 'isUrlGenerationSafe']]
+            ),
+            new TwigFunction(
+                'get_url_for_block',
+                [$this, 'getUrlForBlock']
             ),
         ];
     }
@@ -87,5 +98,29 @@ final class RoutingExtension extends AbstractExtension
             ModuleName::fromString($moduleName),
             ActionName::fromString($actionName)
         );
+    }
+
+    public function getUrlForBlock(
+        string $moduleName,
+        string $actionName,
+        ?string $type = 'action',
+        array $parameters = [],
+    ): string {
+        $page = $this->pageRepository->createQueryBuilder('p')
+            ->innerJoin('p.revisions', 'r', Join::WITH, 'r.isDraft = :draft AND r.locale = :locale')
+            ->innerJoin('r.blocks', 'pb')
+            ->innerJoin('pb.block', 'fb', Join::WITH, 'fb.block.module = :module AND fb.block.name = :name')
+            ->setParameter('module', $moduleName)
+            ->setParameter('name', $type . '__' . $actionName)
+            ->setParameter('locale', Locale::current()->value)
+            ->setParameter('draft', false)
+            ->getQuery()
+            ->getOneOrNullResult();
+
+        if ($page === null) {
+            return $this->pageRouter->getRouteForPageId(Page::PAGE_ID_404, Locale::current());
+        }
+
+        return $this->pageRouter->getRouteForPage($page, Locale::current(), $parameters);
     }
 }
