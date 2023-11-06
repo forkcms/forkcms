300641be1a23f56f47bbd6d91e3eb590f82d8576
Bjorn Van Acker <bjorn@sumocoders.be>
2023-11-06 15:45:24 +0100

    Initial setup for 2fa

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Pageon\DoctrineDataGridBundle\PageonDoctrineDataGridBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true, 'test_install' => true],
+    Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
 ];
--- src/Modules/Backend/Backend/Actions/UserEdit.php
+++ src/Modules/Backend/Backend/Actions/UserEdit.php
@@ -2,12 +2,19 @@
 
 namespace ForkCMS\Modules\Backend\Backend\Actions;
 
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\Writer\PngWriter;
 use ForkCMS\Core\Domain\Header\Breadcrumb\Breadcrumb;
 use ForkCMS\Core\Domain\Header\FlashMessage\FlashMessage;
 use ForkCMS\Modules\Backend\Domain\Action\AbstractFormActionController;
+use ForkCMS\Modules\Backend\Domain\Action\ActionServices;
 use ForkCMS\Modules\Backend\Domain\User\Command\ChangeUser;
 use ForkCMS\Modules\Backend\Domain\User\User;
 use ForkCMS\Modules\Backend\Domain\User\UserType;
+use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\Google\GoogleAuthenticatorInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,6 +25,13 @@ use Symfony\Component\HttpFoundation\Response;
  */
 final class UserEdit extends AbstractFormActionController
 {
+    public function __construct(
+        ActionServices $services,
+        private readonly GoogleAuthenticatorInterface $googleAuthenticator,
+    ) {
+        parent::__construct($services);
+    }
+
     protected function getFormResponse(Request $request): ?Response
     {
         $user = $this->getEntityFromRequest($request, User::class);
@@ -32,11 +46,29 @@ final class UserEdit extends AbstractFormActionController
         return $this->handleForm(
             request: $request,
             formType: UserType::class,
-            formData: new ChangeUser($user),
+            formData: new ChangeUser($user, $this->displayQrCode($user)),
             redirectResponse: new RedirectResponse(UserIndex::getActionSlug()->generateRoute($this->router)),
             flashMessageCallback: static function (FormInterface $form): FlashMessage {
                 return FlashMessage::success('UserEdited', ['%user%' => $form->getData()->displayName]);
             }
         );
+    }
+
+    private function displayQrCode(User $user): ?string
+    {
+        if ($user->getGoogleAuthenticatorSecret() === null) {
+            return null;
+        }
+
+        return Builder::create()
+            ->writer(new PngWriter())
+            ->data($this->googleAuthenticator->getQRContent($user))
+            ->encoding(new Encoding('UTF-8'))
+            ->errorCorrectionLevel(new ErrorCorrectionLevelHigh())
+            ->size(200)
+            ->margin(0)
+            ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
+            ->build()
+            ->getDataUri();
     }
 }
