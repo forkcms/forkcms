ef939323f1be648e16630d88ccd86cb7d9e907f6
Bjorn Van Acker <bjorn@sumocoders.be>
2023-11-06 15:46:02 +0100

    Add trusted devices to 2fa login

---
+++ src/Modules/Backend/Controller/TwoFactorAuthenticationController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace ForkCMS\Modules\Backend\Controller;
+
+use ForkCMS\Modules\Extensions\Domain\Module\ModuleName;
+use ForkCMS\Modules\Extensions\Domain\Module\ModuleSettings;
+use ForkCMS\Modules\Internationalisation\Domain\Translation\TranslationKey;
+use Scheb\TwoFactorBundle\Controller\FormController;
+use Scheb\TwoFactorBundle\Security\Authentication\Token\TwoFactorTokenInterface;
+use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\TwoFactorProviderRegistry;
+use Scheb\TwoFactorBundle\Security\TwoFactor\Trusted\TrustedDeviceManagerInterface;
+use Scheb\TwoFactorBundle\Security\TwoFactor\TwoFactorFirewallContext;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Http\Logout\LogoutUrlGenerator;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class TwoFactorAuthenticationController extends FormController
+{
+    public function __construct(
+        TokenStorageInterface $tokenStorage,
+        TwoFactorProviderRegistry $providerRegistry,
+        TwoFactorFirewallContext $twoFactorFirewallContext,
+        LogoutUrlGenerator $logoutUrlGenerator,
+        ?TrustedDeviceManagerInterface $trustedDeviceManager,
+        bool $trustedFeatureEnabled,
+        private readonly TranslatorInterface $translator,
+        private readonly ModuleSettings $moduleSettings,
+    ) {
+        parent::__construct(
+            $tokenStorage,
+            $providerRegistry,
+            $twoFactorFirewallContext,
+            $logoutUrlGenerator,
+            $trustedDeviceManager,
+            $trustedFeatureEnabled
+        );
+    }
+
+    #[Route('/2fa', name: '2fa_login')]
+    public function form(Request $request): Response
+    {
+        return parent::form($request);
+    }
+
+    protected function getTemplateVars(Request $request, TwoFactorTokenInterface $token): array
+    {
+        $templateVars = parent::getTemplateVars($request, $token);
+
+        return array_merge(
+            $templateVars,
+            [
+                'error' => null,
+                'page_title' => $this->translator->trans(TranslationKey::label('TwoFactorAuthentication')),
+                'SITE_TITLE' => $this->moduleSettings->get(
+                    ModuleName::fromString('Frontend'),
+                    'site_title_' . $request->getLocale(),
+                    $_ENV['SITE_DEFAULT_TITLE']
+                ),
+                'SITE_URL' => $_ENV['SITE_PROTOCOL'] . '://' . $_ENV['SITE_DOMAIN'],
+                'jsFiles' => [],
+                'cssFiles' => [],
+                'jsData' => '',
+            ]
+        );
+    }
+}
