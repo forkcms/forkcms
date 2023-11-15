e0777c82d9d53b698a2d67c8fa9cc52e36bd2758
Bjorn Van Acker <bjorn@sumocoders.be>
2023-11-15 13:33:59 +0100

    Reset crudIndexAction

--- src/Core/templates/Maker/templates/Backend/Actions/Add.html.twig.tpl.php
+++ src/Core/templates/Maker/templates/Backend/Actions/Add.html.twig.tpl.php
@@ -1,3 +1,3 @@
 <?php /** @var string $entity */ ?>
 {% extends '@Backend/base/crud/add.html.twig' %}
-{% set crud_index_action = '<?= $entity ?>Index' %}
+{% set crudIndexAction = '<?= $entity ?>Index' %}
--- src/Core/templates/Maker/templates/Backend/Actions/Edit.html.twig.tpl.php
+++ src/Core/templates/Maker/templates/Backend/Actions/Edit.html.twig.tpl.php
@@ -1,3 +1,3 @@
 <?php /** @var string $entity */ ?>
 {% extends '@Backend/base/crud/edit.html.twig' %}
-{% set crud_index_action = '<?= $entity ?>rIndex' %}
+{% set crudIndexAction = '<?= $entity ?>rIndex' %}
