ec04f30a3c0d2c1efdfa870b650ff09af5848300
Bjorn Van Acker <bjorn@sumocoders.be>
2023-11-15 13:38:41 +0100

    Reset crudActionName

--- src/Core/templates/Maker/templates/Backend/Actions/Index.html.twig.tpl.php
+++ src/Core/templates/Maker/templates/Backend/Actions/Index.html.twig.tpl.php
@@ -1,3 +1,3 @@
 <?php /** @var string $entity */ ?>
 {% extends '@Backend/base/crud/index.html.twig' %}
-{% set crud_action_name = '<?= $entity ?>Add' %}
+{% set crudActionName = '<?= $entity ?>Add' %}
