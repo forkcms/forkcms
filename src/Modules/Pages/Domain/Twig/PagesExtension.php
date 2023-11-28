e889bf94b984da871a882505e244a2b56f1a6274
Bjorn Van Acker <bjorn@sumocoders.be>
2023-11-28 15:21:56 +0100

    Use is_sub_navigation

--- src/Modules/Pages/Domain/Twig/PagesExtension.php
+++ src/Modules/Pages/Domain/Twig/PagesExtension.php
@@ -67,7 +67,7 @@ final class PagesExtension extends AbstractExtension
                 'nextDepth' => $depth - 1,
                 'excludedIds' => $excludedIds,
                 'template' => $template,
-                'isSubNavigation' => $isSubNavigation,
+                'is_sub_navigation' => $isSubNavigation,
             ]
         );
     }
