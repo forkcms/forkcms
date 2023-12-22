7ef40cf16c2a0d1a06c8a80d2f36f5c7fe4b8a9c
Bjorn Van Acker <bjorn@sumocoders.be>
2023-12-22 13:37:27 +0100

    Use clear cache event on module settings change

--- src/Modules/Extensions/Domain/Module/Command/ChangeModuleSettingsHandler.php
+++ src/Modules/Extensions/Domain/Module/Command/ChangeModuleSettingsHandler.php
@@ -2,22 +2,22 @@
 
 namespace ForkCMS\Modules\Extensions\Domain\Module\Command;
 
-use ForkCMS\Core\Domain\Kernel\Command\ClearContainerCache;
+use ForkCMS\Core\Domain\Kernel\Event\ClearCacheEvent;
 use ForkCMS\Core\Domain\MessageHandler\CommandHandlerInterface;
 use ForkCMS\Modules\Extensions\Domain\Module\ModuleRepository;
-use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 final class ChangeModuleSettingsHandler implements CommandHandlerInterface
 {
     public function __construct(
         private readonly ModuleRepository $moduleRepository,
-        private readonly MessageBusInterface $commandBus,
+        private readonly EventDispatcher $eventDispatcher,
     ) {
     }
 
     public function __invoke(ChangeModuleSettings $changeModuleSettings): void
     {
         $this->moduleRepository->save($changeModuleSettings->module);
-        $this->commandBus->dispatch(new ClearContainerCache());
+        $this->eventDispatcher->dispatch(new ClearCacheEvent());
     }
 }
